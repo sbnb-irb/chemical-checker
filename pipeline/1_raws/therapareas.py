6412a34de02059c00a219970c28800dd82ecc783
Oriol Guitart Pla <oriol.guitart@irbbarcelona.org>
2018-07-17 10:59:09 +0200

    Add new thereapeutic raw script plus fixing cellbioass

--- pipeline/1_raws/therapareas.py
+++ pipeline/1_raws/therapareas.py
@@ -1,8 +1,4 @@
-'''
-# Therapeutic Areas (ATC)
-
-From DrugBank and KEGG Drugs.
-'''
+#!/miniconda/bin/python
 
 # Imports
 
@@ -10,15 +6,19 @@ import collections
 import pybel
 import subprocess
 import os, sys
-sys.path.append(os.path.join(sys.path[0], "../../dbutils/"))
+sys.path.append(os.path.join(sys.path[0],"../../src/utils"))
+sys.path.append(os.path.join(sys.path[0],"../config"))
+from checkerUtils import logSystem, execAndCheck, draw
 import Psql
 
+import checkerconfig
 
 # Variables
 
 kegg_molrepo = "XXX"
 drugbank_molrepo = "XXX"
 br_file = "XXX" # db/br08303.keg
+dbname = ''
 
 table = "therapareas"
 
@@ -31,11 +31,13 @@ def parse_kegg(inchikey_atc = None):
 
     # Read molrepo
     kegg_inchikey = {}
+    inchikey_inchi = {}
     f = open(molrepo_file, "r")
     for l in f:
         l = l.rstrip("\n").split("\t")
         if not l[2]: continue
         kegg_inchikey[l[0]] = l[2]
+        inchikey_inchi[l[2]] = l[3]
 
     # Read drug KEGG branch
     with open(br_file, "r") as f:
@@ -47,10 +49,10 @@ def parse_kegg(inchikey_atc = None):
                 if drug not in kegg_inchikey: continue
                 inchikey_atc[kegg_inchikey[drug]].update([atc])
 
-    return inchikey_atc
+    return inchikey_atc,inchikey_inchi
 
 
-def parse_drugbank(inhchikey_atc = None):
+def parse_drugbank(inchikey_inchi,inhchikey_atc = None):
 
     if not inchikey_atc:
         inchikey_atc = collections.defaultdict(set)
@@ -62,6 +64,7 @@ def parse_drugbank(inhchikey_atc = None):
         l = l.rstrip("\n").split("\t")
         if not l[2]: continue
         dbid_inchikey[l[0]] = l[2]
+        inchikey_inchi[l[2]] = l[3]
 
     # Read DrugBank
 
@@ -113,28 +116,54 @@ def break_atcs(inchikey_atc):
     return inchikey_raw
 
 
-def insert_to_database(inchikey_raw):
+def insert_to_database(inchikey_raw,inchikey_inchi):
 
     inchikey_raw = dict((k,",".join(sorted(v))) for k,v in inchikey_raw.iteritems())
+    
+    todos = Psql.insert_structures(inchikey_inchi, dbname)
+    for ik in todos:
+        draw(ik,inchikey_inchi[ik])
 
-    Psql.insert_raw(table, inchikey_raw)
+    Psql.insert_raw(table, inchikey_raw,dbname)
 
 
 # Main
 
 def main():
+    
+    import argparse
+    
+    if len(sys.argv) != 2:
+        sys.exit(1)
+  
+    configFilename = sys.argv[1]
 
-    print "Parsing KEGG..."
-    inchikey_atc = parse_kegg()
+    checkercfg = checkerconfig.checkerConf( configFilename)  
+    global dbname
+    
+    dbname = checkerconfig.dbname + "_" + checkercfg.getVariable("General",'release')
+    global br_file,kegg_molrepo,drugbank_molrepo
+    
+    downloadsdir = checkercfg.getDirectory( "downloads" )
+    br_file = os.path.join(downloadsdir,checkerconfig.kegg_atcs_download)
+    kegg_molrepo = os.path.join(checkercfg.getDirectory( "molRepo" ),"kegg.tsv")
+    drugbank_molrepo = os.path.join(checkercfg.getDirectory( "molRepo" ),"drugbank.tsv")
+    logsFiledir = checkercfg.getDirectory( "logs" )
 
-    print "Parsing DrugBank..."
-    inchikey_atc = parse_drugbank(inchikey_atc)
+    log = logSystem(sys.stdout)
 
-    print "Breaking ATCs..."
+
+    log.info( "Parsing KEGG...")
+    inchikey_atc,inchikey_inchi = parse_kegg()
+
+    log.info( "Parsing DrugBank...")
+    inchikey_atc = parse_drugbank(inchikey_inchi,inchikey_atc)
+
+    log.info( "Breaking ATCs...")
     inchikey_raw = break_atcs(inchikey_atc)
 
-    print "Inserting to database..."
-    insert_to_database(inchikey_raw)
+    log.info( "Inserting to database...")
+    insert_to_database(inchikey_raw,inchikey_inchi)
 
 
 if __name__ == "__main__":
