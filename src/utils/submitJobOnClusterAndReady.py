7339b455a7361ffa350de25995eb3ed899d8f460
Oriol Guitart Pla <oriol.guitart@irbbarcelona.org>
2018-08-27 13:05:10 +0200

    Fix issues in clustering. Change rthe way the job logs are checked after qsub sync is finished

--- src/utils/submitJobOnClusterAndReady.py
+++ src/utils/submitJobOnClusterAndReady.py
@@ -32,53 +32,31 @@ def main():
   
   # And we start it
   cmdStr = checkerconfig.SUBMITJOB % { 'JOB_NAME':jobName }
-  wrapperCmd = "cd "+directory+"; "+cmdStr+" > "+logFilename
+  wrapperCmd = "cd "+directory+"; "+cmdStr+" &> "+logFilename
   if MASTERNODE != "":
     wrapperCmd = "ssh "+MASTERNODE+" '%s'" % wrapperCmd
-  execAndCheck(wrapperCmd,log)
+  try:
+    execAndCheck(wrapperCmd,log)
+  except:
+    log.error( "ERROR running this script in cluster: " + cmdStr  )
 
   # Then we check the log file
   logFile = open( logFilename )
-  bOks = {}
+  bExitOk = True
   for line in logFile:
-    if line.find( "Your job-array ") != -1:
-      #Your job-array 104037.1-2360:1 ("celegans_pdbsearch") has been submitted
-      jobSpec = line.rstrip("\n\r").split()[2]
-      jobId = jobSpec.split(".")[0]
-      taskStart = int(jobSpec.split(".")[1].split("-")[0])
-      taskEnd = int(jobSpec.split(".")[1].split("-")[1].split(":")[0])
-      for i in range(taskStart,taskEnd+1):
-        taskId = jobId+"."+str(i)
-        bOks[taskId] = False
-    elif line.find( "Your job ") != -1:
-      #Your job 104043 ("prova") has been submitted
-      jobId = line.rstrip("\n\r").split()[2]
-      bOks[jobId] = False
-    elif line[:3] == "Job":
-      jobId = line.rstrip("\n\r").split()[1]
-      if line.find( "exited with exit code" ) != -1:
+    
+    if line.find( "exited with exit code" ) != -1:
         #print line
         #print jobId
         exitCode = int(line.rstrip("\n\r.").split()[6])
         #print exitCode
-        if jobId.find(".") == -1:
-          for k in bOks:
-            if exitCode == 0:
-              bOks[k] = True
-        else:
-          if exitCode == 0:
-            bOks[jobId] = True
-      else:
-        bOks[jobId] = False
+        
+        if exitCode != 0:
+            bExitOk = False
+            break
+
   logFile.close()
   
-  bExitOk = True
-  for k in bOks:
-    #print k
-    #print bOks[k]
-    if not bOks[k]:
-      bExitOk = False
-      break
   
   # Once we are done we put a file <job_name>.ready in the excution directory
   if bExitOk:
