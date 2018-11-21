1de01de9accc9ebc7b7e64c7e09a0fd52296609f
Oriol Guitart Pla <oriol.guitart@irbbarcelona.org>
2018-11-21 15:55:12 +0100

    Change similarity external to Miquel's modifications. Also change inchikeys to keys

--- src/similarity/similarity_internal.py
+++ src/similarity/similarity_internal.py
@@ -3,24 +3,19 @@
 
 # Imports
 
-import subprocess
-from scipy.spatial.distance import cosine
 from scipy.spatial.distance import cdist
-import sys, os
-import json
+import sys
+import os
 import h5py
 sys.path.append(os.path.join(sys.path[0], "../utils/"))
-sys.path.append(os.path.join(sys.path[0],"../../pipeline/config"))
+sys.path.append(os.path.join(sys.path[0], "../../pipeline/config"))
 from checkerUtils import inchikey2webrepo
-from Psql import  querylite
-import checkerconfig
 import numpy as np
 import bisect
 import time
 
 # Compare molecules
 
-#coordinate, inchikey, infile, bgfile, outfile, vname, VERSION = sys.argv[1].split('---')
 coordinate, from_j, to_j, infile, bgfile, outfile, vname, VERSION, batch_ref = sys.argv[1].split('---')
 
 from_j, to_j = int(from_j), int(to_j)
@@ -40,6 +35,7 @@ with h5py.File(bgfile, "r") as hf:
     distances = hf["distance"][:]
     max_idx = len(distances) - 1
 
+
 def get_integer(d):
     return bisect.bisect_left(distances, d)
 
@@ -51,14 +47,14 @@ get_integer = np.vectorize(get_integer)
 time0 = time.time()
 
 with h5py.File(infile, "r") as hf:
-    N        = len(hf["inchikeys"][:])
-    my_V     = hf["V"][from_j:to_j]
-    my_iks   = hf["inchikeys"][from_j:to_j]
+    N = len(hf["keys"][:])
+    my_V = hf["V"][from_j:to_j]
+    my_iks = hf["keys"][from_j:to_j]
     INTEGERS = np.zeros((N, my_V.shape[0]), np.int8)
     from_i = 0
     while from_i < N:
         to_i = from_i + chunksize
-        INTEGERS[from_i:to_i, :] = get_integer(np.round(cdist(hf["V"][from_i:to_i], my_V, metric = "cosine"), numerical_precision))
+        INTEGERS[from_i:to_i, :] = get_integer(np.round(cdist(hf["V"][from_i:to_i], my_V, metric="cosine"), numerical_precision))
         from_i += chunksize
 
 INTEGERS[INTEGERS > max_idx] = max_idx
@@ -80,19 +76,13 @@ for j, ik in enumerate(my_iks):
         with h5py.File("%s/%s" % (PATH, outfile), "a") as hf:
             dname = coordinate + "_obs"
             dversion = dname + "_vrs"
-            if dname in hf.keys(): del hf[dname]
-            hf.create_dataset(dname, data = INTEGERS[:,j])
-            if dversion in hf.keys(): del hf[dversion]
-            hf.create_dataset(dversion, data = [VERSION])
-
-
+            if dname in hf.keys():
+                del hf[dname]
+            hf.create_dataset(dname, data=INTEGERS[:, j])
+            if dversion in hf.keys():
+                del hf[dversion]
+            hf.create_dataset(dversion, data=[VERSION])
 
     except:
 
         print "Error when saving %s: %s/%s" % (ik, PATH, outfile)
-
-
-# Inserting the current version in the database
-
-#cmd = "INSERT INTO distance_versions (inchikey, coord, vname, version) VALUES ('%s', '%s', '%s', '%s') ON CONFLICT (inchikey, coord, vname) DO UPDATE SET version = '%s'" % (inchikey, coordinate + "_obs", vname, VERSION, VERSION)
-#querylite(cmd, checkerconfig.SQLITE_DATABASE_RELEASES)
