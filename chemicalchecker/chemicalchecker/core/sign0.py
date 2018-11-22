9b3b8ab03ac9574f5789f9d5b7a8d0d56969bd48
Martino Bertoni <martino.bertoni@irbbarcelona.org>
2018-11-22 16:04:39 +0100

    added initial core module

---
+++ chemicalchecker/chemicalchecker/core/sign0.py
@@ -0,0 +1,29 @@
+from signature_base import BaseSignature
+
+
+class SignatureZero(BaseSignature):
+    """A Signature bla bla."""
+
+    def __init__(self, config_file=None):
+        """From the recipe we derive all the cleaning logic."""
+        BaseSignature.__init__(self, config_file)
+        print 'SignatureZero'
+        print self.config.PATH.CC_ROOT
+
+    def fit(self):
+        """Takes an input and learns to produce an output."""
+
+    def predict(self):
+        """Uses the fitted models to go from input to output."""
+
+    def validate(self):
+        """Performs a validation across external data as MoA and ATC codes."""
+
+    def __iter__(self):
+        """Batch iteration, if necessary."""
+
+    def __getattr__(self):
+        """Return the vector corresponding to the key. 
+
+        Works fast with bisect, but should return None if the key is not in
+        keys (ideally, keep a set to do this).."""
