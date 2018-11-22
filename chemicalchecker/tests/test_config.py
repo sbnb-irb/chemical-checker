9b92a5e90cad7ab44c029541350bea8bc6959d39
Martino Bertoni <martino.bertoni@irbbarcelona.org>
2018-11-22 16:05:38 +0100

    unit test for config

---
+++ chemicalchecker/tests/test_config.py
@@ -0,0 +1,38 @@
+import os
+import unittest
+
+from chemicalchecker.util import Config
+
+
+class TestConfig(unittest.TestCase):
+
+    def setUp(self):
+        # path for test data
+        test_dir = os.path.dirname(os.path.realpath(__file__))
+        self.data_dir = os.path.join(test_dir, 'data')
+
+    def test_init(self):
+        filename = os.path.join(self.data_dir, 'config.json')
+        self.assertTrue(os.path.isfile(filename))
+        config = Config(filename)
+        self.assertTrue(hasattr(config, 'PATH'))
+        self.assertTrue(hasattr(config.PATH, 'CC_ROOT'))
+        self.assertEqual(config.PATH.CC_ROOT, '/aloy/web_checker/')
+
+    def test_init_default(self):
+        filename = os.path.join(self.data_dir, 'config.json')
+        self.assertTrue(os.path.isfile(filename))
+        config = Config(filename)
+        self.assertTrue(hasattr(config, 'PATH'))
+        self.assertTrue(hasattr(config.PATH, 'CC_ROOT'))
+        self.assertEqual(config.PATH.CC_ROOT, '/aloy/web_checker/')
+
+        config = Config()
+        self.assertTrue(hasattr(config, 'PATH'))
+        self.assertTrue(hasattr(config.PATH, 'CC_ROOT'))
+        self.assertEqual(config.PATH.CC_ROOT, '/aloy/web_checker/')
+
+        del os.environ['CC_CONFIG']
+
+        with self.assertRaises(KeyError):
+            config = Config()
