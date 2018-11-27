2c4fbcd3b77cd3346660cc1353b36887270b19b6
Martino Bertoni <martino.bertoni@irbbarcelona.org>
2018-11-27 17:03:59 +0100

    added signature factory, better definition for signature baseclasse, more doc, minor refactor

--- chemicalchecker/chemicalchecker/core/sign0.py
+++ chemicalchecker/chemicalchecker/core/sign0.py
@@ -3,29 +3,23 @@ from .signature_base import BaseSignature
 
 
 @logged
-class SignatureZero(BaseSignature):
+class sign0(BaseSignature):
     """A Signature bla bla."""
 
-    def __init__(self, config_file=None):
+    def __init__(self, path):
         """From the recipe we derive all the cleaning logic."""
-        BaseSignature.__init__(self, config_file)
-        self.__log.debug('SignatureZero')
-        self.__log.debug(self.config.PATH.CC_ROOT)
+        BaseSignature.__init__(self, path)
+        self.__log.debug('sign0 path: %s', path)
+        self.path = path
 
     def fit(self):
         """Takes an input and learns to produce an output."""
+        self.__log.debug('fit')
 
     def predict(self):
         """Uses the fitted models to go from input to output."""
+        self.__log.debug('predict')
 
     def validate(self):
         """Performs a validation across external data as MoA and ATC codes."""
-
-    def __iter__(self):
-        """Batch iteration, if necessary."""
-
-    def __getattr__(self):
-        """Return the vector corresponding to the key.
-
-        Works fast with bisect, but should return None if the key is not in
-        keys (ideally, keep a set to do this).."""
+        self.__log.debug('validate')
--- chemicalchecker/chemicalchecker/core/signature_base.py
+++ chemicalchecker/chemicalchecker/core/signature_base.py
@@ -1,37 +1,56 @@
+"""Implementation of the abstract signature class.
+
+Each signature class derived from this base class will have to implement the
+`fit`, `predict` and `validate` methods. As the underlying data format for
+every signature is the same, this class implements the iterator and attribute
+getter.
+"""
+import os
 import six
 from abc import ABCMeta, abstractmethod
-from chemicalchecker.util import Config
+
+from chemicalchecker.util import logged
 
 
+@logged
 @six.add_metaclass(ABCMeta)
 class BaseSignature(object):
-    """A Signature bla bla."""
+    """A Signature base class.
+
+    Implements methods and checks common to all signatures.
+    """
 
     @abstractmethod
-    def __init__(self, config_file=None):
+    def __init__(self, path):
         """From the recipe we derive all the cleaning logic."""
-        self.config = Config(config_file)
-        self.__log.debug('BaseSignature')
+        BaseSignature.__log.debug('__init__')
+        if not os.path.isfile(path):
+            BaseSignature.__log.warning("HDF5 file not available")
 
     @abstractmethod
     def fit(self):
         """Takes an input and learns to produce an output."""
+        BaseSignature.__log.debug('fit')
 
     @abstractmethod
     def predict(self):
         """Uses the fitted models to go from input to output."""
+        BaseSignature.__log.debug('predict')
 
     @abstractmethod
     def validate(self):
         """Performs a validation across external data as MoA and ATC codes."""
+        BaseSignature.__log.debug('validate')
 
-    @abstractmethod
     def __iter__(self):
         """Batch iteration, if necessary."""
+        BaseSignature.__log.debug('__iter__')
+        yield
 
-    @abstractmethod
     def __getattr__(self):
         """Return the vector corresponding to the key. 
 
         Works fast with bisect, but should return None if the key is not in
         keys (ideally, keep a set to do this).."""
+        BaseSignature.__log.debug('__getattr__')
+        yield
