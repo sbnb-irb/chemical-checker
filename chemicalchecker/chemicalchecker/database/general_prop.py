7c8831e130330a0adfd0d698fc2e984ac712ddbc
Oriol Guitart Pla <oriol.guitart@irbbarcelona.org>
2018-11-27 17:43:03 +0100

    Module database added with unit tests included. Only general_properties table fully working

---
+++ chemicalchecker/chemicalchecker/database/general_prop.py
@@ -0,0 +1,81 @@
+from chemicalchecker.util import logged
+from .database import Base, get_session, get_engine
+from sqlalchemy import Column, Integer, Text, Float
+
+
+@logged
+class GeneralProp(Base):
+    """A Signature bla bla."""
+    __tablename__ = 'physchem'
+    inchikey = Column(Text, primary_key=True)
+    mw = Column(Integer)
+    heavy = Column(Integer)
+    hetero = Column(Integer)
+    rings = Column(Integer)
+    ringaliph = Column(Integer)
+    ringarom = Column(Integer)
+    alogp = Column(Float)
+    mr = Column(Float)
+    hba = Column(Integer)
+    hbd = Column(Integer)
+    psa = Column(Float)
+    rotb = Column(Integer)
+    alerts_qed = Column(Integer)
+    alerts_chembl = Column(Integer)
+    ro5 = Column(Integer)
+    ro3 = Column(Integer)
+    qed = Column(Float)
+
+    @staticmethod
+    def add(kwargs):
+        """ Method to add a new row to the table.
+
+        Args:
+            kwargs(dict):The data in dictionary format .
+        """
+        GeneralProp.__log.debug(type(kwargs))
+        if type(kwargs) is dict:
+            prop = GeneralProp(**kwargs)
+
+        GeneralProp.__log.debug(prop.inchikey)
+        session = get_session()
+        session.add(prop)
+        session.commit()
+        session.close()
+
+    @staticmethod
+    def add_bulk(data, chunk=1000):
+        """ Method to add a new row to the table.
+
+        The data will be in array format. the order is very important
+        """
+        engine = get_engine()
+        for pos in xrange(0, len(data), chunk):
+
+            engine.execute(
+                GeneralProp.__table__.insert(),
+                [{"inchikey": row[0], "mw": row[1], "heavy": row[2], "hetero": row[4],
+                  "rings": row[4], "ringaliph": row[5], "ringarom": row[6], "alogp": row[7],
+                  "mr": row[8], "hba": row[9], "hbd": row[10], "psa": row[11], "rotb": row[12],
+                  "alerts_qed": row[13], "alerts_chembl": row[14], "ro5": row[15], "ro3": row[16], "qed": row[17]}
+                    for row in data[pos:pos + chunk]]
+            )
+
+    @staticmethod
+    def get(key):
+        """ Method to query general_properties table.
+
+
+        """
+        session = get_session()
+        query = session.query(GeneralProp).filter_by(inchikey=key)
+        res = query.one_or_none()
+
+        session.close()
+
+        return res
+
+    @staticmethod
+    def create_table():
+        engine = get_engine()
+        Base.metadata.create_all(engine)
