223ec895fce5a1e2b3440e17d0da698d8f733355
Martino Bertoni <martino.bertoni@irbbarcelona.org>
2018-11-27 17:03:59 +0100

    added signature factory, better definition for signature baseclasse, more doc, minor refactor

--- chemicalchecker/chemicalchecker/core/data.py
+++ chemicalchecker/chemicalchecker/core/data.py
@@ -1,8 +1,19 @@
+"""This class initialize and serve the different Chemical Checker data types.
+
+It is a factory of different signatures (including along with clusters and
+neighbors). Is the place where the classes implementing such data types are
+imported and initialized.
+"""
+from .sign0 import sign0
+#from .sign1 import sign1
+#from .sign2 import sign2
+#from .sign3 import sign3
+#from .clst import clst
 
 class DataFactory():
 
-    def __init__(self):
-        pass
-
     def make_data(self, datatype, data_path):
-        pass
+        if datatype in globals():
+            return eval(datatype)(data_path)
+        else:
+            raise Exception("Data type %s not available" % datatype)
