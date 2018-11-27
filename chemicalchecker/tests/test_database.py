8d2690dc874d7409db99dadf571058ed576e316a
Oriol Guitart Pla <oriol.guitart@irbbarcelona.org>
2018-11-27 18:05:42 +0100

    Add new test

--- chemicalchecker/tests/test_database.py
+++ chemicalchecker/tests/test_database.py
@@ -22,15 +22,17 @@ class TestDatabase(unittest.TestCase):
         if os.path.exists(testfile):
             os.remove(testfile)
 
-
     def test_add_bulk(self):
 
-        self.GeneralProp.add_bulk([["test1", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17],
+        res = self.GeneralProp.get('test2')
+        self.assertIsNone(res)
+
+        self.GeneralProp.add_bulk([["test1", 11, 21, 31, 41, 51, 61, 71, 81, 91, 101, 111, 121, 131, 141, 151, 161, 171],
                                    ["test2", 1, 22, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]])
 
         res = self.GeneralProp.get('test1')
         self.assertTrue(hasattr(res, 'mw'))
-        self.assertTrue(res.heavy == 2)
+        self.assertTrue(res.heavy == 21)
 
         res = self.GeneralProp.get('test2')
         self.assertTrue(hasattr(res, 'mw'))
