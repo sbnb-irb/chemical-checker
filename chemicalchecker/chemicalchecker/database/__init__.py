3587887e864ef598cbb67c58951b5ae70540aa3f
Oriol Guitart Pla <oriol.guitart@irbbarcelona.org>
2018-11-28 16:12:59 +0100

    Add several new table classes and its tests for the Database module

--- chemicalchecker/chemicalchecker/database/__init__.py
+++ chemicalchecker/chemicalchecker/database/__init__.py
@@ -1,3 +1,6 @@
 from .database import Database
 from .general_prop import GeneralProp
 from .dataset import Dataset
+from .pubchem import Pubchem
+from .libraries import Libraries
+from .structure import Structure
