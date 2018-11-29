5a58a8fbbf2c67988732506a4b9309b84d271861
Oriol Guitart Pla <oriol.guitart@irbbarcelona.org>
2018-11-29 10:30:38 +0100

    Add more psql methods to the database model

--- chemicalchecker/chemicalchecker/database/__init__.py
+++ chemicalchecker/chemicalchecker/database/__init__.py
@@ -1,6 +1,7 @@
-from .database import Database
+from .database import get_connection, get_engine, get_session, qstring, query
 from .general_prop import GeneralProp
 from .dataset import Dataset
 from .pubchem import Pubchem
 from .libraries import Libraries
 from .structure import Structure
+from .datafiles import Datafiles
