69084fcae8824fd4c8601f52b431113a4e4caac5
Oriol Guitart Pla <oriol.guitart@irbbarcelona.org>
2018-11-29 10:30:38 +0100

    Add more psql methods to the database model

--- chemicalchecker/chemicalchecker/database/database.py
+++ chemicalchecker/chemicalchecker/database/database.py
@@ -12,35 +12,81 @@ Base = declarative_base()
 config = Config()
 
 
-def get_connection():
+def get_connection(dbname=None):
+    """ Method to connect to PSQL DB through psycopg2
+
+    Args:
+        dbname(str):The name of DB to connect. If none, the name from config file is used .
+    Res:
+        connection
+    """
+    if dbname is None:
+        dbname = config.DB.main_DB
     datab = psycopg2.connect(host=config.DB.host, user=config.DB.username,
-                             password=config.DB.password, database=config.DB.main_DB)
+                             password=config.DB.password, database=dbname)
     return datab
 
 
-def get_engine():
+def get_engine(dbname=None):
+    """ Method to get engine for ORM SQLALCHEMY
+
+    Args:
+        dbname(str):The name of DB to connect. If none, the name from config file is used .
+    Res:
+        engine
+    """
     if config.DB.dialect == 'sqlite':
         con = config.DB.dialect + ':///' + config.DB.file
     else:
+        if dbname is None:
+            dbname = config.DB.main_DB
         con = config.DB.dialect + '://' + config.DB.username + ':' + config.DB.password + \
-            '@' + config.DB.host + ':' + config.DB.port + '/' + config.DB.main_DB
-    #engine = create_engine('sqlite:///orm_in_detail.sqlite', echo=True)
+            '@' + config.DB.host + ':' + config.DB.port + '/' + dbname
+    # engine = create_engine('sqlite:///orm_in_detail.sqlite', echo=True)
     engine = create_engine(con, echo=True, poolclass=NullPool)
     return engine
 
 
-def get_session():
+def get_session(dbname=None):
+    """ Method to get session for ORM SQLALCHEMY
+
+    Args:
+        dbname(str):The name of DB to connect. If none, the name from config file is used .
+    Res:
+        session
+    """
     Session = sessionmaker()
-    engine = get_engine()
+    engine = get_engine(dbname)
     Session.configure(bind=engine)
     session = Session()
     return session
 
 
-@logged
-class Database(object):
-    """A class to query the cc database bla bla."""
-    Base = None
+def qstring(query, dbname):
+    """ Method to query a PSQL DB
 
-    def __init__(self):
-        self.__log.debug('Database class')
+    Args:
+        dbname(str):The name of DB to connect. If none, the name from config file is used .
+    Res:
+        rows: the data queried in row format
+    """
+    con = get_connection(dbname=dbname)
+    con.set_isolation_level(0)
+    cur = con.cursor()
+    cur.execute(query)
+    rows = cur.fetchall()
+    con.close()
+    return rows
+
+
+def query(query, dbname):
+    """ Method to query a PSQL database which returns data
+
+    Args:
+        dbname(str):The name of DB to connect. If none, the name from config file is used .
+    """
+    con = get_connection(dbname=dbname)
+    con.set_isolation_level(0)
+    cur = con.cursor()
+    cur.execute(query)
+    con.close()
