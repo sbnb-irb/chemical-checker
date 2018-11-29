871d9361a0a11f5d33fbff80269db69474c31cd3
Martino Bertoni <martino.bertoni@irbbarcelona.org>
2018-11-29 10:14:04 +0100

    signature datafactory, initial implementation for several signatures, all to be completed

---
+++ chemicalchecker/chemicalchecker/core/neig1.py
@@ -0,0 +1,36 @@
+import os
+from chemicalchecker.util import logged
+from .signature_base import BaseSignature
+
+
+@logged
+class neig1(BaseSignature):
+    """A Signature bla bla."""
+
+    def __init__(self, data_path, model_path):
+        """From the recipe we derive all the cleaning logic."""
+        self.__log.debug('data_path: %s', data_path)
+        self.data_path = data_path
+        self.__log.debug('model_path: %s', model_path)
+        self.model_path = model_path
+        # edit model_path to the specific model
+        BaseSignature.__init__(self, data_path, model_path)
+
+    def fit(self, features):
+        """Take an input and learns to produce an output."""
+        BaseSignature.fit(self)
+        self.__log.debug('Faiss kNN fit %s' % features)
+        self.__log.debug('saving model to %s' % self.model_path)
+        os.touch(self.model_path)
+
+    def predict(self, features):
+        """Use the fitted models to go from input to output."""
+        BaseSignature.predict(self)
+        self.__log.debug('loading model from %s' % self.model_path)
+        self.__log.debug('Faiss kNN predict %s' % features)
+        return 'neig1_pred'
+
+    def validate(self, validation_set):
+        """Perform a validation across external data as MoA and ATC codes."""
+        BaseSignature.validate(self)
+        self.__log.debug('Faiss kNN validate on %s' % validation_set)
