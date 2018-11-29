a3fd32e34beefecbc5c2594ca2eb335fa474108e
Oriol Guitart Pla <oriol.guitart@irbbarcelona.org>
2018-11-29 09:16:24 +0100

    Add datafiles class for datafiles table

---
+++ chemicalchecker/chemicalchecker/database/datafiles.py
@@ -0,0 +1,52 @@
+from chemicalchecker.util import logged
+from .database import Base, get_session, get_engine
+from sqlalchemy import Column, Text
+
+
+@logged
+class Datafiles(Base):
+    """A Signature bla bla."""
+    __tablename__ = 'datafiles'
+    link = Column(Text, primary_key=True)
+    username = Column(Text)
+    password = Column(Text)
+    name = Column(Text)
+    dataset = Column(Text)
+    description = Column(Text)
+
+    @staticmethod
+    def add(kwargs):
+        """ Method to add a new row to the table.
+
+        Args:
+            kwargs(dict):The data in dictionary format .
+        """
+        Datafiles.__log.debug(type(kwargs))
+        if type(kwargs) is dict:
+            datafiles = Datafiles(**kwargs)
+
+        Datafiles.__log.debug(datafiles.code)
+        session = get_session()
+        session.add(datafiles)
+        session.commit()
+        session.close()
+
+    @staticmethod
+    def get(dataset):
+        """ Method to query datafiles table.
+
+        Args:
+            dataset(str):The code of the dataset to get all files related to that dataset.
+        """
+        session = get_session()
+        query = session.query(Datafiles).filter_by(dataset=dataset)
+        res = query.all()
+
+        session.close()
+
+        return res
+
+    @staticmethod
+    def _create_table():
+        engine = get_engine()
+        Base.metadata.create_all(engine)
