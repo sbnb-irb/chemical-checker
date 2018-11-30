06fa29a4357a90312a79322b60ac436749a6655c
Martino Bertoni <martino.bertoni@irbbarcelona.org>
2018-11-30 12:19:20 +0100

    moved import outside of function

--- chemicalchecker/tests/test_database.py
+++ chemicalchecker/tests/test_database.py
@@ -1,7 +1,11 @@
 import os
 import unittest
-
 from chemicalchecker.util import Config
+from chemicalchecker.database import GeneralProp
+from chemicalchecker.database import Dataset
+from chemicalchecker.database import Pubchem
+from chemicalchecker.database import Libraries
+from chemicalchecker.database import Structure
 
 
 class TestDatabase(unittest.TestCase):
@@ -10,10 +14,10 @@ class TestDatabase(unittest.TestCase):
         # path for test data
         test_dir = os.path.dirname(os.path.realpath(__file__))
         self.data_dir = os.path.join(test_dir, 'data')
-        conffile = os.path.join(self.data_dir, 'database_config.json')
-        self.cfg = Config(conffile)
-        from chemicalchecker.database import GeneralProp
-        from chemicalchecker.database import Dataset, Pubchem, Libraries, Structure
+        # set the enviroment path for the config to be found
+        os.environ["CC_CONFIG"] = os.path.join(
+            self.data_dir, 'database_config.json')
+        self.cfg = Config()
         self.GeneralProp = GeneralProp
         GeneralProp._create_table()
         self.Dataset = Dataset
@@ -26,7 +30,6 @@ class TestDatabase(unittest.TestCase):
         Structure._create_table()
 
     def tearDown(self):
-
         if os.path.exists(self.cfg.DB.file):
             os.remove(self.cfg.DB.file)
 
@@ -35,8 +38,11 @@ class TestDatabase(unittest.TestCase):
         res = self.GeneralProp.get('test2')
         self.assertIsNone(res)
 
-        self.GeneralProp.add_bulk([["test1", 11, 21, 31, 41, 51, 61, 71, 81, 91, 101, 111, 121, 131, 141, 151, 161, 171],
-                                   ["test2", 1, 22, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]])
+        self.GeneralProp.add_bulk([
+            ["test1", 11, 21, 31, 41, 51, 61, 71, 81, 91, 101, 111, 121, 131,
+             141, 151, 161, 171],
+            ["test2", 1, 22, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
+             17]])
 
         res = self.GeneralProp.get('test1')
         self.assertTrue(hasattr(res, 'mw'))
@@ -92,7 +98,8 @@ class TestDatabase(unittest.TestCase):
         res = self.Libraries.get('test1')
         self.assertIsNone(res)
 
-        self.Libraries.add({"lib": "test1", "files": "A", "name": "test", "description": "A", "urls": "True", "rank": 6})
+        self.Libraries.add({"lib": "test1", "files": "A", "name": "test",
+                            "description": "A", "urls": "True", "rank": 6})
 
         res = self.Libraries.get('test1')
         self.assertTrue(hasattr(res, 'files'))
