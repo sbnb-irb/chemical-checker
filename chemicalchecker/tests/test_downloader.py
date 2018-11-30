a4290f2a3aef66aa9789a798aa903443f259b6e5
Martino Bertoni <martino.bertoni@irbbarcelona.org>
2018-11-30 18:38:17 +0100

    fixed downloader

--- chemicalchecker/tests/test_downloader.py
+++ chemicalchecker/tests/test_downloader.py
@@ -13,19 +13,22 @@ class TestConfig(unittest.TestCase):
         self.data_dir = os.path.join(test_dir, 'data')
         os.environ["CC_CONFIG"] = os.path.join(
             self.data_dir, 'download_config.json')
+        self.tmp_path = os.path.join(self.data_dir, 'test_download_tmp')
+        os.makedirs(self.tmp_path)
+        self.dest_path = os.path.join(self.data_dir, 'test_download_dest')
 
     def tearDown(self):
         shutil.rmtree(self.tmp_path)
+        shutil.rmtree(self.dest_path)
 
     def test_download(self):
         url = 'http://www.disgenet.org/ds/DisGeNET/' + \
             'results/disease_mappings.tsv.gz'
-        self.tmp_path = os.path.join(self.data_dir, 'test_download')
-        downloader = Downloader(url, self.tmp_path)
+        downloader = Downloader(url, self.dest_path, self.tmp_path)
         downloader.download()
-        self.assertTrue(os.path.isdir(self.tmp_path))
+        self.assertTrue(os.path.isdir(self.dest_path))
         self.assertTrue(os.path.isfile(
-            os.path.join(self.tmp_path, 'disease_mappings.tsv')))
+            os.path.join(self.dest_path, 'disease_mappings.tsv')))
 
 
 """
