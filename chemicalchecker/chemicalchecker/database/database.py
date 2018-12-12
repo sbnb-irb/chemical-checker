639f1c1a10d5b6d69b9c66793e6e67f354afd4b8
Martino Bertoni <martino.bertoni@irbbarcelona.org>
2018-12-12 15:10:26 +0100

    moving psql query functions to util module

--- chemicalchecker/chemicalchecker/database/database.py
+++ chemicalchecker/chemicalchecker/database/database.py
@@ -1,5 +1,4 @@
 from chemicalchecker.util import Config
-from chemicalchecker.util import logged
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -7,24 +6,6 @@ from sqlalchemy.pool import NullPool
 
 
 Base = declarative_base()
-
-
-def get_connection(dbname=None):
-    """ Method to connect to PSQL DB through psycopg2
-
-    Args:
-        dbname(str):The name of DB to connect.
-            If none, the name from config file is used.
-    Returns:
-        connection
-    """
-    import psycopg2
-    config = Config()
-
-    conn_dict = config.DB.asdict()
-    conn_dict.pop('dialect')
-    datab = psycopg2.connect(**conn_dict)
-    return datab
 
 
 def get_engine(dbname=None):
@@ -63,35 +44,3 @@ def get_session(dbname=None):
     Session.configure(bind=engine)
     session = Session()
     return session
-
-
-def qstring(query, dbname):
-    """Method to query a PSQL DB.
-
-    Args:
-        dbname(str):The name of DB to connect.
-            If none, the name from config file is used.
-    Returns:
-        rows: the data queried in row format
-    """
-    con = get_connection(dbname=dbname)
-    con.set_isolation_level(0)
-    cur = con.cursor()
-    cur.execute(query)
-    rows = cur.fetchall()
-    con.close()
-    return rows
-
-
-def query(query, dbname):
-    """Method to query a PSQL database which returns data.
-
-    Args:
-        dbname(str):The name of DB to connect.
-            If none, the name from config file is used.
-    """
-    con = get_connection(dbname=dbname)
-    con.set_isolation_level(0)
-    cur = con.cursor()
-    cur.execute(query)
-    con.close()
