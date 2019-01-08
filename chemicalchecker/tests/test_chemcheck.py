3f377a5645f5ab19375a91ba7d126f21e040a5b1
Martino Bertoni <martino.bertoni@irbbarcelona.org>
2019-01-08 16:55:48 +0100

    Added molset logic (e.g. 'reference', 'full')

--- chemicalchecker/tests/test_chemcheck.py
+++ chemicalchecker/tests/test_chemcheck.py
@@ -19,17 +19,27 @@ class TestChemicalChecker(unittest.TestCase):
         test_dir = os.path.dirname(os.path.realpath(__file__))
         self.data_dir = os.path.join(test_dir, 'data')
 
+    def tearDown(self):
+        if os.path.exists(self.cc_root):
+            shutil.rmtree(self.cc_root)
+
     def test_init(self):
-        cc_root = os.path.join(self.data_dir, 'full')
+        cc_root = os.path.join(self.data_dir, 'alpha')
+        self.cc_root = cc_root
         self.assertFalse(os.path.isdir(cc_root))
-        cc_full = ChemicalChecker(cc_root)
+        cc = ChemicalChecker(cc_root)
         self.assertTrue(os.path.isdir(cc_root))
         for coords in coordinates():
-            path1 = os.path.join(cc_root, coords[:1])
+            path1 = os.path.join(cc_root, 'reference', coords[:1])
             self.assertTrue(os.path.isdir(path1))
-            path2 = os.path.join(cc_root, coords[:1], coords[:2])
+            path2 = os.path.join(cc_root, 'reference', coords[:1], coords[:2])
+            self.assertTrue(os.path.isdir(path2))
+            path1 = os.path.join(cc_root, 'full', coords[:1])
+            self.assertTrue(os.path.isdir(path1))
+            path2 = os.path.join(cc_root, 'full', coords[:1], coords[:2])
             self.assertTrue(os.path.isdir(path2))
 
-        sign_file = os.path.join(cc_root, 'A', 'A1', 'A1.001', 'sign1.h5')
-        self.assertEqual(cc_full.get_data_path('sign1', 'A1.001'), sign_file)
-        shutil.rmtree(cc_root)
+        sign_path = os.path.join(cc_root, 'reference',
+                                 'A', 'A1', 'A1.001', 'sign1')
+        self.assertEqual(cc.get_data_path(
+            'sign1', 'reference', 'A1.001'), sign_path)
