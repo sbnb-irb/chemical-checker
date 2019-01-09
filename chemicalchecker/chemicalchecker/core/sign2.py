1d0e74705fe0239b1086aedd68790a7d7cf4e51f
Martino Bertoni <martino.bertoni@irbbarcelona.org>
2019-01-09 11:06:14 +0100

    added sign2 and node2vec wrappers

--- chemicalchecker/chemicalchecker/core/sign2.py
+++ chemicalchecker/chemicalchecker/core/sign2.py
@@ -1,32 +1,100 @@
-from chemicalchecker.util import logged
+"""Signature type 2.
+
+Network embedding of the similarity matrix derived from signatures type 1.
+They have fixed length, which is convenient for machine learning, and capture
+both explicit and implicit similarity relationships in the data.
+
+Signatures type 2 are the result of a two-step process:
+
+1. Transform nearest-neighbor of signature type 1 to graph.
+
+2. Perform network embedding with Node2Vec.
+
+It is not possible to produce network embeddings for out-of-sample
+(out-of-vocabulary) nodes, so a multi-output regression needs to be performed
+a posteriori (from signatures type 1 to signatures type 2) in order to endow
+predict() capabilities. This is done using the AdaNet framework for for
+automatically learning high-quality models with minimal expert intervention.
+"""
+import os
 from .signature_base import BaseSignature
+from chemicalchecker.util import logged
+from chemicalchecker.tool import Node2Vec, AdaNet
 
 
 @logged
 class sign2(BaseSignature):
-    """A Signature bla bla."""
+    """Signature type 2 class."""
 
-    def __init__(self, data_path, model_path, dataset_info):
-        """From the recipe we derive all the cleaning logic."""
-        self.__log.debug('data_path: %s', data_path)
-        self.data_path = data_path
-        self.__log.debug('model_path: %s', model_path)
-        self.model_path = model_path
-        # edit model_path to the specific model
-        BaseSignature.__init__(self, data_path, model_path, dataset_info)
+    def __init__(self, signature_path, dataset, **params):
+        """Initialize the signature.
 
-    def fit(self, features):
-        """Take an input and learns to produce an output."""
-        BaseSignature.fit(self)
-        self.__log.debug('Node2Vec on %s' % features)
-        self.__log.debug('AdaNet fit %s with Node2Vec output' % features)
-        self.__log.debug('saving model to %s' % self.model_path)
+        Args:
+            data_path(str): Where the signature h5 file is saved.
+            model_path(str): Directory where the persistent model is saved.
+            stats_path(str): Where the statistics of fit are saved.
+            dataset(`Dataset`): `chemicalchecker.database.Dataset` object.
+            params(): Parameters, expected keys are 'graph', 'node2vec', and
+                'adanet'.
+        """
+        # Calling init on the base class to trigger file existence checks
+        BaseSignature.__init__(self, signature_path, dataset, **params)
+        # generate needed paths
+        self.data_path = os.path.join(signature_path, 'sign2.h5')
+        self.model_path = os.path.join(signature_path, 'model')
+        os.path.makedirs(self.model_path)
+        self.stats_path = os.path.join(signature_path, 'stats')
+        os.path.makedirs(self.stats_path)
+        # assign dataset
+        self.dataset = dataset
+        # logging
+        self.__log.debug('dataset: %s', dataset)
+        self.__log.debug('data_path: %s', self.data_path)
+        self.__log.debug('model_path: %s', self.model_path)
+        self.__log.debug('stats_path: %s', self.stats_path)
 
-    def predict(self, features):
-        """Use the fitted models to go from input to output."""
-        BaseSignature.predict(self)
-        self.__log.debug('loading model from %s' % self.model_path)
-        self.__log.debug('AdaNet predict %s' % features)
+    def fit(self, sign1):
+        """Take an input and learns to produce an output.
+
+        Args:
+            sign1(sign1): Signature type 1.
+        """
+        # step 1: Node2Vec (learn graph embedding) input is neig1
+        self.__log.debug('Node2Vec on %s' % sign1)
+        n2v = Node2Vec()
+        # use neig1 to generate the Node2Vec input graph (as edgelist)
+        graph_params = self.params['graph']
+        node2vec_path = os.path.join(self.model_path, 'node2vec')
+        graph_file = os.path.join(node2vec_path, 'graph.edgelist')
+        n2v.to_edgelist(sign1, graph_file, **graph_params)
+        # run Node2Vec to generate embeddings
+        node2vec_params = self.params['node2vec']
+        emb_file = os.path.join(node2vec_path, 'n2v.emb')
+        n2v.run(graph_file, emb_file, **node2vec_params)
+        # convert to signature h5 format
+        n2v.emb_to_h5(emb_file, self.data_path)
+        # step 2: AdaNet (learn to predict sign2 from sign1 without Node2Vec)
+        self.__log.debug('AdaNet fit %s with Node2Vec output' % sign1)
+        adanet_params = self.params['adanet']
+        adanet_path = os.path.join(self.model_path, 'adanet')
+        adanet_params.update({"model_path": adanet_path})
+        ada = AdaNet(**adanet_params)
+        # prepare input file
+        ada.prepare_train(sign1.data_path, self.data_path)
+        # learn NN with AdaNet
+        ada.train()
+        self.__log.debug('model saved to %s' % self.model_path)
+
+    def predict(self, sign1):
+        """Use the fitted AdaNet model to predict sign2."""
+        # load AdaNet model
+        adanet_path = os.path.join(self.model_path, 'adanet')
+        self.__log.debug('loading model from %s' % adanet_path)
+        ada = AdaNet(model_path=adanet_path)
+        # prepare input file
+        self.__log.debug('AdaNet predict %s' % sign1)
+        ada.prepare_predict(sign1.data_path)
+        return ada.predict()
 
     def statistics(self):
         """Perform a statistics """
