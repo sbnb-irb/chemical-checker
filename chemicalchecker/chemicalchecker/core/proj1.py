740eca3d55b7b39a8537f7d58181e1665023085a
Oriol Guitart Pla <oriol.guitart@irbbarcelona.org>
2019-01-16 10:49:57 +0100

    Change dataset_info to dataset. Add validation path to signatures

--- chemicalchecker/chemicalchecker/core/proj1.py
+++ chemicalchecker/chemicalchecker/core/proj1.py
@@ -6,14 +6,14 @@ from .signature_base import BaseSignature
 class proj1(BaseSignature):
     """A Signature bla bla."""
 
-    def __init__(self, data_path, model_path, dataset_info):
+    def __init__(self, data_path, model_path, dataset):
         """From the recipe we derive all the cleaning logic."""
         self.__log.debug('data_path: %s', data_path)
         self.data_path = data_path
         self.__log.debug('model_path: %s', model_path)
         self.model_path = model_path
         # edit model_path to the specific model
-        BaseSignature.__init__(self, data_path, model_path, dataset_info)
+        BaseSignature.__init__(self, data_path, model_path, dataset)
 
     def fit(self, features):
         """Take an input and learns to produce an output."""
