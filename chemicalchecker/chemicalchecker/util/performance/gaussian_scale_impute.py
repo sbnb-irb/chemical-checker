df5e5f21e4184bda1947ff5b643db04b748ea384
Oriol Guitart Pla <oriol.guitart@irbbarcelona.org>
2019-02-13 16:41:58 +0100

    Add new pre_process scripts and fix others. Also add util methods

---
+++ chemicalchecker/chemicalchecker/util/performance/gaussian_scale_impute.py
@@ -0,0 +1,67 @@
+from sklearn.preprocessing import Imputer, RobustScaler
+from fancyimpute import IterativeImputer as fancyImputer
+import numpy as np
+import os
+import pickle
+
+perc = 99.9
+
+
+def deextremize(X, z_extreme, up=None, dw=None):
+    # Be careful! This only works once normalized.
+    if up is None:
+        up = np.min([np.percentile(X, perc), z_extreme])
+    if dw is None:
+        dw = np.max([np.percentile(X, 100. - perc), -z_extreme])
+    X[X > up] = up
+    X[X < dw] = dw
+    return X, up, dw
+
+
+def scaleimpute(X, z_extreme=10, models_path=None, up=None, dw=None):
+    imputer_file = "imput.pcl"
+    scaler_file = "scale.pcl"
+    fancy_file = "fancy.pcl"
+    X = np.array(X)
+    if models_path is None:
+        imputer_file = None
+        scaler_file = None
+        fancy_file = None
+    else:
+        imputer_file = os.path.join(models_path, imputer_file)
+        scaler_file = os.path.join(models_path, scaler_file)
+        fancy_file = os.path.join(models_path, fancy_file)
+
+    if imputer_file is None or not os.path.exists(imputer_file):
+        imputer = Imputer(strategy="median")
+        imputer.fit(X)
+        with open(imputer_file, 'wb') as fh:
+            pickle.dump(imputer, fh)
+    else:
+        imputer = pickle.load(open(imputer_file, 'rb'))
+
+    M = imputer.transform(X)
+
+    if scaler_file is None or not os.path.exists(scaler_file):
+        scaler = RobustScaler()
+        scaler.fit(M)
+        with open(scaler_file, 'wb') as fh:
+            pickle.dump(scaler, fh)
+    else:
+        scaler = pickle.load(open(scaler_file, 'rb'))
+
+    M = scaler.transform(M)
+    M, up, dw = deextremize(M, z_extreme, up, dw)
+    M[np.isnan(X)] = np.nan
+    if np.any(np.isnan(M)):
+        if fancy_file is None or not os.path.exists(fancy_file):
+            fancy = fancyImputer()
+            fancy.fit(M)
+            with open(fancy_file, 'wb') as fh:
+                pickle.dump(fancy, fh)
+        else:
+            fancy = pickle.load(open(fancy_file, 'rb'))
+        M = fancy.transform(M)
+        return deextremize(M, z_extreme)
+    else:
+        return M, up, dw
