a04fb94fda4ab3a57402a1904860cf1f0d3abfac
Martino Bertoni <martino.bertoni@irbbarcelona.org>
2019-02-19 18:01:37 +0100

    tune default parameters

--- chemicalchecker/chemicalchecker/tool/adanet/adanet_wrap.py
+++ chemicalchecker/chemicalchecker/tool/adanet/adanet_wrap.py
@@ -183,7 +183,7 @@ class AdaNetWrapper(object):
         self.model_dir = kwargs.get("model_dir", None)
         self.activation = kwargs.get("activation", tf.nn.relu)
         self.shuffles = int(kwargs.get("shuffles", 10))
-        self.dropout_rate = float(kwargs.get("dropout_rate", 0.0))
+        self.dropout_rate = float(kwargs.get("dropout_rate", 0.2))
         self.adanet_iterations = int(kwargs.get("adanet_iterations", 10))
         self.subnetwork_generator = eval(kwargs.get(
             "subnetwork_generator", "ExtendDNNGenerator"))
