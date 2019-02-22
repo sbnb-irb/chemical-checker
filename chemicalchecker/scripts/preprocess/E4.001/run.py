d7414f12f841636fd9a0158676864b7afce10e1f
Oriol Guitart Pla <oriol.guitart@irbbarcelona.org>
2019-02-22 10:00:11 +0100

    Add preprocesses scripts E3,E4,E5

--- chemicalchecker/scripts/preprocess/E4.001/run.py
+++ chemicalchecker/scripts/preprocess/E4.001/run.py
@@ -1,22 +1,103 @@
 import os
 import sys
 import argparse
+import numpy as np
+import collections
+import h5py
 
 
 from chemicalchecker.util import logged
-
+from chemicalchecker.database import Dataset
+from chemicalchecker.database import Molrepo
 # Variables
 
-
-# Parse arguments
+features_file = "features.h5"
+entry_point_full = "disease"
 
 
 def get_parser():
     description = 'Run preprocess script.'
     parser = argparse.ArgumentParser(description=description)
+    parser.add_argument('-i', '--input_file', type=str,
+                        required=False, default='.', help='Input file only for predict method')
     parser.add_argument('-o', '--output_file', type=str,
                         required=False, default='.', help='Output file')
+    parser.add_argument('-m', '--method', type=str,
+                        required=False, default='fit', help='Method: fit or predict')
+    parser.add_argument('-mp', '--models_path', type=str,
+                        required=False, default='', help='The models path')
+    parser.add_argument('-ep', '--entry_point', type=str,
+                        required=False, default=None, help='The predict entry point')
     return parser
+
+
+# Parse arguments
+
+def parse_ctd(disfile, chemdis_file):
+
+    ctd_inchikey = {}
+    molrepos = Molrepo.get_by_molrepo_name("ctd")
+    for molrepo in molrepos:
+        if not molrepo.inchikey:
+            continue
+        ctd_inchikey[molrepo.src_id] = molrepo.inchikey
+
+    dis_tree = collections.defaultdict(list)
+    f = open(disfile, "r")
+    for l in f:
+        if l[0] == "#":
+            continue
+        l = l.rstrip("\n").split("\t")
+        dis_tree[l[1]] = l[5].split("|")
+    f.close()
+
+    tree_dis = collections.defaultdict(list)
+    for k, v in dis_tree.iteritems():
+        for x in v:
+            tree_dis[x] += [k]
+
+    def expand_tree(tn):
+        tns = []
+        x = tn.split("/")[0].split(".")
+        for i in xrange(len(x)):
+            tns += [".".join(x[:i + 1])]
+        tns += [tn]
+        return tns
+
+    f = open(chemdis_file, "r")
+    inchikey_raw = collections.defaultdict(set)
+
+    for l in f:
+        if l[0] == "#":
+            continue
+        l = l.rstrip("\n").split("\t")
+        if l[5] == "":
+            continue
+        dis = l[4]
+        cid = l[1]
+        if cid not in ctd_inchikey:
+            continue
+        inchikey = ctd_inchikey[cid]
+        ev = l[5]
+        for tn in dis_tree[dis]:
+            exp_tns = expand_tree(tn)
+            exp_dis = set()
+            for exp_tn in exp_tns:
+                exp_dis.update(tree_dis[exp_tn])
+        exp_dis = sorted(exp_dis)
+        for d in exp_dis:
+            if "MESH" not in d:
+                continue
+            x = []
+            if ev == "marker/mechanism":
+                x += [d + "(M)"]
+            if ev == "therapeutic":
+                x += [d + "(T)"]
+            for y in x:
+                inchikey_raw[inchikey].update([y.split("MESH:")[1]])
+    f.close()
+
+    return inchikey_raw
 
 
 @logged
@@ -24,11 +105,80 @@ def main():
 
     args = get_parser().parse_args(sys.argv[1:])
 
-    dataset = os.path.dirname(os.path.abspath(__file__))[-6:]
+    dataset_code = 'E4.001'  # os.path.dirname(os.path.abspath(__file__))[-6:]
+
+    dataset = Dataset.get(dataset_code)
+
+    map_files = {}
+
+    for ds in dataset.datasources:
+        map_files[ds.name] = ds.data_path
 
     main._log.debug(
-        "Running preprocess for dataset " + dataset + ". Saving output in " + args.output_file)
+        "Running preprocess for dataset " + dataset_code + ". Saving output in " + args.output_file)
 
+    if args.entry_point is None:
+        args.entry_point = entry_point_full
+
+    features = None
+
+    if args.method == "fit":
+
+        chemdis_file = os.path.join(
+            map_files["CTD_chemicals_diseases"], "CTD_chemicals_diseases.tsv")
+
+        disfile = os.path.join(
+            map_files["CTD_diseases"], "CTD_diseases.tsv")
+
+        main._log.info("Parsing CTD...")
+        inchikey_raw = parse_ctd(disfile, chemdis_file)
+
+    if args.method == "predict":
+
+        inchikey_raw = collections.defaultdict(list)
+
+        with h5py.File(os.path.join(args.models_path, features_file)) as hf:
+            features_list = hf["features"][:]
+            features = set(features_list)
+
+        with open(args.input_file) as f:
+
+            for l in f:
+                items = l.rstrip().split("\t")
+                val = items[1] + "(" + items[2] + ")"
+                if val not in features:
+                    continue
+                inchikey_raw[items[0]] += [val]
+
+    main._log.info("Saving raws")
+
+    keys = []
+    words = set()
+    for k in sorted(inchikey_raw.keys()):
+
+        keys.append(str(k))
+        words.update(inchikey_raw[k])
+
+    if features is not None:
+        orderwords = features_list
+    else:
+        orderwords = list(words)
+        orderwords.sort()
+    raws = np.zeros((len(keys), len(orderwords)), dtype=np.int8)
+    wordspos = {k: v for v, k in enumerate(orderwords)}
+
+    for i, k in enumerate(keys):
+        for word in inchikey_raw[k]:
+            raws[i][wordspos[word]] = 1
+
+    with h5py.File(args.output_file, "w") as hf:
+        hf.create_dataset("keys", data=np.array(keys))
+        hf.create_dataset("V", data=raws)
+        hf.create_dataset("features", data=np.array(orderwords))
+
+    if args.method == "fit":
+        with h5py.File(os.path.join(args.models_path, features_file), "w") as hf:
+            hf.create_dataset("features", data=np.array(orderwords))
 
 if __name__ == '__main__':
     main()
--- chemicalchecker/scripts/preprocess/E5.001/run.py
+++ chemicalchecker/scripts/preprocess/E5.001/run.py
@@ -1,22 +1,73 @@
 import os
 import sys
 import argparse
+import numpy as np
+import collections
+import h5py
+import xml.etree.ElementTree as ET
 
 
 from chemicalchecker.util import logged
-
+from chemicalchecker.database import Dataset
+from chemicalchecker.database import Molrepo
 # Variables
 
-
-# Parse arguments
+features_file = "features.h5"
+entry_point_full = "drug"
 
 
 def get_parser():
     description = 'Run preprocess script.'
     parser = argparse.ArgumentParser(description=description)
+    parser.add_argument('-i', '--input_file', type=str,
+                        required=False, default='.', help='Input file only for predict method')
     parser.add_argument('-o', '--output_file', type=str,
                         required=False, default='.', help='Output file')
+    parser.add_argument('-m', '--method', type=str,
+                        required=False, default='fit', help='Method: fit or predict')
+    parser.add_argument('-mp', '--models_path', type=str,
+                        required=False, default='', help='The models path')
+    parser.add_argument('-ep', '--entry_point', type=str,
+                        required=False, default=None, help='The predict entry point')
     return parser
+
+
+# Parse arguments
+
+def parse_ddis(drugbank_xml):
+
+    tree = ET.parse(drugbank_xml)
+
+    dbid_inchikey = {}
+    molrepos = Molrepo.get_by_molrepo_name("drugbank")
+    for molrepo in molrepos:
+        if not molrepo.inchikey:
+            continue
+        dbid_inchikey[molrepo.src_id] = molrepo.inchikey
+
+    inchikey_ddi = collections.defaultdict(list)
+
+    root = tree.getroot()
+    prefix = "{http://www.drugbank.ca}"
+    for drug in root:
+        for child in drug.findall(prefix + "drugbank-id"):
+            if "primary" not in child.attrib:
+                continue
+            if child.attrib["primary"] == "true":
+                # print "primary: " + child.text
+                db_id = child.text
+                if db_id not in dbid_inchikey:
+                    continue
+                drug_interactions = drug.find(prefix + 'drug-interactions')
+                drug_inter = drug_interactions.findall(
+                    prefix + 'drug-interaction')
+                # print len(drug_inter)
+                for inter in drug_inter:
+                    for child_did in inter.findall(prefix + "drugbank-id"):
+                        # print child_did.text
+                        inchikey_ddi[dbid_inchikey[db_id]] += [child_did.text]
+
+    return inchikey_ddi
 
 
 @logged
@@ -24,11 +75,76 @@ def main():
 
     args = get_parser().parse_args(sys.argv[1:])
 
-    dataset = os.path.dirname(os.path.abspath(__file__))[-6:]
+    dataset_code = 'E5.001'  # os.path.dirname(os.path.abspath(__file__))[-6:]
+
+    dataset = Dataset.get(dataset_code)
+
+    map_files = {}
+
+    for ds in dataset.datasources:
+        map_files[ds.name] = ds.data_path
 
     main._log.debug(
-        "Running preprocess for dataset " + dataset + ". Saving output in " + args.output_file)
+        "Running preprocess for dataset " + dataset_code + ". Saving output in " + args.output_file)
 
+    if args.entry_point is None:
+        args.entry_point = entry_point_full
+
+    features = None
+
+    if args.method == "fit":
+
+        drugbank_xml = os.path.join(
+            map_files["drugbank"], "full database.xml")
+
+        main._log.info("Parsing DDIs...")
+        inchikey_ddi = parse_ddis(drugbank_xml)
+
+    if args.method == "predict":
+
+        inchikey_ddi = collections.defaultdict(list)
+
+        with h5py.File(os.path.join(args.models_path, features_file)) as hf:
+            features_list = hf["features"][:]
+            features = set(features_list)
+
+        with open(args.input_file) as f:
+
+            for l in f:
+                items = l.rstrip().split("\t")
+                if items[1] not in features:
+                    continue
+                inchikey_ddi[items[0]] += [items[1]]
+
+    main._log.info("Saving raws")
+
+    keys = []
+    words = set()
+    for k in sorted(inchikey_ddi.keys()):
+
+        keys.append(str(k))
+        words.update(inchikey_ddi[k])
+
+    if features is not None:
+        orderwords = features_list
+    else:
+        orderwords = list(words)
+        orderwords.sort()
+    raws = np.zeros((len(keys), len(orderwords)), dtype=np.int8)
+    wordspos = {k: v for v, k in enumerate(orderwords)}
+
+    for i, k in enumerate(keys):
+        for word in inchikey_ddi[k]:
+            raws[i][wordspos[word]] = 1
+
+    with h5py.File(args.output_file, "w") as hf:
+        hf.create_dataset("keys", data=np.array(keys))
+        hf.create_dataset("V", data=raws)
+        hf.create_dataset("features", data=np.array(orderwords))
+
+    if args.method == "fit":
+        with h5py.File(os.path.join(args.models_path, features_file), "w") as hf:
+            hf.create_dataset("features", data=np.array(orderwords))
 
 if __name__ == '__main__':
     main()
