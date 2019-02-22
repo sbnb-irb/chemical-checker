e4d46ca115206bdc5bbbc600278bedd4f3845b61
Martino Bertoni <martino.bertoni@irbbarcelona.org>
2019-02-22 13:04:46 +0100

    added methods 1) convert signature to features, 2) compare signature to old database

--- chemicalchecker/chemicalchecker/core/sign0.py
+++ chemicalchecker/chemicalchecker/core/sign0.py
@@ -1,9 +1,14 @@
-from chemicalchecker.util import logged
-from chemicalchecker.util import Config
-from .signature_base import BaseSignature
-from subprocess import call
+
 import os
 import sys
+import h5py
+import numpy as np
+from tqdm import tqdm
+from subprocess import call
+
+from .signature_base import BaseSignature
+from chemicalchecker.util import logged
+from chemicalchecker.util import Config
 
 
 @logged
@@ -105,3 +110,75 @@ class sign0(BaseSignature):
 
     def statistics(self):
         """Perform a statistics."""
+
+    def to_features(self, signatures):
+        """Convert signature to explicit feature names.
+
+        Args:
+            signatures(array): a signature 0 for 1 or more molecules
+        Returns:
+            list of dict: 1 dictionary per signature where keys are
+                feature_name and value as values.
+        """
+        # early stop if features file is not there
+        feature_file = os.path.join(self.model_path, "features.h5")
+        if not os.path.isfile(feature_file):
+            self.__log.warn("No feature file found.")
+            return None
+        # read features names from file
+        with h5py.File(feature_file) as hf:
+            features = hf["features"][:]
+        # handle single signature
+        if len(signatures.shape) == 1:
+            signatures = [signatures]
+        # return list of dicts with feature_name as key and value as value
+        result = list()
+        for sign in signatures:
+            keys = features[sign != 0]
+            values = sign[sign != 0]
+            result.append(dict(zip(keys, values)))
+        return result
+
+    def _compare_to_old(self, old_db_dump, to_sample=1000):
+        """Compare current signature 0 to previous format.
+
+        The db dump can be obtained running the following:
+        psql -h aloy-dbsrv -U <user_name> -d mosaic -c "COPY (SELECT * FROM <table_name>) TO STDOUT WITH CSV" > file.csv
+
+        Args:
+            old_db_dump(str): file name with old raw features dumped from db.
+            to_sample(int): Number of signatures to compare in the set of
+                shared moleules.
+
+        """
+        # get old keys
+        with open(old_db_dump, 'r') as fh:
+            lines = fh.readlines()
+        old_keys = set(l[:27] for l in lines)
+        # compare to new
+        old_only_keys = old_keys - self.unique_keys
+        new_only_keys = self.unique_keys - old_keys
+        self.__log.info("Old keys: %s", len(old_keys))
+        self.__log.info("New keys: %s", len(self.unique_keys))
+        self.__log.info("Shared keys: %s", len(self.unique_keys & old_keys))
+        self.__log.info("Old only keys: %s", len(old_only_keys))
+        self.__log.info("New only keys: %s", len(new_only_keys))
+
+        old_has_more = 0
+        new_has_more = 0
+        # randomly sample 1000 entries
+        to_sample = min(len(lines), to_sample)
+        lines = np.random.choice(lines, to_sample, replace=False)
+        for line in tqdm(lines):
+            ink = line[:27]
+            if ink not in self.unique_keys:
+                continue
+            feat_old = set(line[29:-2].split(','))
+            feat_new = set(self.to_features(self[ink])[0].keys())
+            if len(feat_old - feat_new) != 0:
+                old_has_more += 1
+            if len(feat_new - feat_old) != 0:
+                new_has_more += 1
+        self.__log.info("Among %s shared sampled signatures:", to_sample)
+        self.__log.info("Old features set larger: %s", old_has_more)
+        self.__log.info("New features set larger: %s", new_has_more)
