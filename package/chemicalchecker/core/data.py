3189c62f1c23be61811e0e0c30e80f0cfc98900d
Martino Bertoni <martino.bertoni@irbbarcelona.org>
2019-02-28 15:16:46 +0100

    new function to 'signaturize' data

--- package/chemicalchecker/core/data.py
+++ package/chemicalchecker/core/data.py
@@ -4,6 +4,9 @@ It is a factory of different signatures (including along with clusters and
 neighbors). Is the place where the classes implementing such data types are
 imported and initialized.
 """
+import os
+import h5py
+
 from .sign0 import sign0
 from .sign1 import sign1
 from .clus1 import clus1
@@ -23,3 +26,21 @@ class DataFactory():
             return eval(cctype)(signature_path, validation_path, dataset, **params)
         else:
             raise Exception("Data type %s not available" % cctype)
+
+    @staticmethod
+    def signaturize(cctype, signature_path, matrix, keys=None, dataset=None):
+        """Signaturize a given matrix.
+
+        Produce a signature-like structure for the given input.
+        Given a matrix of anonymous molucules add keys to mantain the order.
+        """
+        data_path = os.path.join(signature_path, '%s.h5' % cctype)
+        if not keys:
+            keys = ["{0:027d}".format(n) for n in range(len(matrix))]
+        if not dataset:
+            dataset = "XX.001"
+        with h5py.File(data_path, 'w') as hf:
+            hf.create_dataset("keys", data=keys)
+            hf.create_dataset("V", data=matrix)
+            hf.create_dataset("shape", data=matrix.shape)
+        return eval(cctype)(signature_path, signature_path, dataset)
