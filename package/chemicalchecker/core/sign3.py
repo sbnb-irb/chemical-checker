8b728b7757e9f965a59e826668afcbdf86fa14bc
Martino Bertoni <martino.bertoni@irbbarcelona.org>
2019-03-01 13:04:27 +0100

    sign3 stepping in

--- package/chemicalchecker/core/sign3.py
+++ package/chemicalchecker/core/sign3.py
@@ -3,39 +3,23 @@
 Network embedding of observed *and* inferred similarity networks. Their added
 value, compared to signatures type 2, is that they can be derived for
 virtually *any* molecule in *any* dataset.
-
-Signatures type 3 are the result of a two-step process:
-
-1. Generate predictors that given sign2 in any dataset predict sign2 in
-current dataset. NB: To have as much as possible training molecules we use full
-sign2 of two datasets to find the intersection and after that remove redundant
-molecules.
-
-2. Train a meta-predictor able to handle missing data. The meta-predictor is
-trained against molecules with available sign2 in current dataset (Y).
-The data for training (X) is the collection of sign2 available in other spaces
-for each molecule.
-
 """
 import os
+import numpy as np
 from pathlib2 import Path
 from .signature_base import BaseSignature
-import chemicalchecker
-from chemicalchecker.util import HPC
-from chemicalchecker.util import Plot
+from chemicalchecker.util.hpc import HPC
+from chemicalchecker.util.plot import Plot
 from chemicalchecker.util import logged
 from chemicalchecker.util import Config
-try:
-    from chemicalchecker.tool import AdaNet, Traintest
-except:
-    pass
+from chemicalchecker.tool.adanet import AdaNet, Traintest
 
 
 @logged
 class sign3(BaseSignature):
     """Signature type 2 class."""
 
-    def __init__(self, signature_path, dataset, **params):
+    def __init__(self, signature_path, validation_path, dataset, **params):
         """Initialize the signature.
 
         Args:
@@ -45,9 +29,11 @@ class sign3(BaseSignature):
                 'adanet'.
         """
         # Calling init on the base class to trigger file existence checks
-        BaseSignature.__init__(self, signature_path, dataset, **params)
+        BaseSignature.__init__(self, signature_path,
+                               validation_path, dataset, **params)
+        self.validation_path = validation_path
         # generate needed paths
-        self.data_path = os.path.join(signature_path, 'sign2.h5')
+        self.data_path = os.path.join(signature_path, 'sign3.h5')
         self.model_path = os.path.join(signature_path, 'models')
         if not os.path.isdir(self.model_path):
             os.makedirs(self.model_path)
@@ -63,23 +49,137 @@ class sign3(BaseSignature):
         self.__log.debug('stats_path: %s', self.stats_path)
         # get parameters or default values
         self.params = dict()
+        self.params['graph'] = params.get('graph', None)
+        self.params['node2vec'] = params.get('node2vec', None)
         self.params['adanet'] = params.get('adanet', None)
 
-    def fit(self, sign2, reuse=True):
+    def most_frequent_sign2(self, chemchecker):
+        # get current space on which we'll train (using reference set to
+        # limit redundancy)
+        my_sign2 = chemchecker.get_signature(
+            'sign2', 'reference', self.dataset.code)
+        available_mols = list()
+        for ds in list(chemchecker.datasets):
+            if ds == self.dataset.code:
+                continue
+            sign2_ds = chemchecker.get_signature('sign2', 'full_map', ds)
+            available = np.isin(my_sign2.keys, sign2_ds.keys)
+            available_mols.append(available)
+            self.__log.info('%s shared molecules between %s and %s',
+                            sum(available), self.dataset, ds)
+        # get availability matrix
+        available_mols = available_mols.astype(float)
+        available_mols = np.stack(available_mols).T
+        # get probabilities for a given space (column)
+        space_probs = np.sum(available_mols, axis=0) / np.sum(available_mols)
+        # get probabilities for how many spaces for a molecule (row)
+        freqs = dict()
+        for row in available_mols:
+            r = np.sum(row)
+            if r not in freqs:
+                freqs[r] = 0.0
+            freqs[r] += 1
+        for k in freqs:
+            freqs[k] /= len(available_mols)
+        top10 = sorted(list(tuple(freqs.items())),
+                       key=lambda x: x[1], reverse=True)[:10]
+        self.__log.info("Most frequent combinations:")
+        for k, v in top10:
+            self.__log.info("%s  %.2f%%", k, v)
+        return top10
+
+    def print_most_common_combinations(self, chemchecker):
+        my_sign2 = chemchecker.get_signature(
+            'sign2', 'reference', self.dataset.code)
+        available_mols = list()
+        for ds in list(chemchecker.datasets):
+            if ds == self.dataset.code:
+                continue
+            sign2_ds = chemchecker.get_signature('sign2', 'full_map', ds)
+            available = np.isin(my_sign2.keys, sign2_ds.keys)
+            available_mols.append(available)
+            self.__log.info('%s shared molecules between %s and %s',
+                            sum(available), self.dataset, ds)
+        # get availability matrix
+        available_mols = np.stack(available_mols).T
+        # most common combinations?
+        freqs = dict()
+        for row in available_mols:
+            r = ''.join(row.astype(int).astype(str).tolist())
+            if r not in freqs:
+                freqs[r] = 0.0
+            freqs[r] += 1
+        for k in freqs:
+            freqs[k] /= len(available_mols)
+            freqs[k] *= 100
+        top10 = sorted(list(tuple(freqs.items())),
+                       key=lambda x: x[1], reverse=True)[:10]
+        self.__log.info("Most frequent combinations:")
+        for k, v in top10:
+            self.__log.info("%s  %.2f%%", k, v)
+
+    def get_sign2_matrix(self, chemchecker):
+        """Get combined matrix of stacked signature 2."""
+        # get current space on which we'll train (using reference set to
+        # limit redundancy)
+        my_sign2 = chemchecker.get_signature(
+            'sign2', 'reference', self.dataset.code)
+        # get signature 2 for molecule in current space (allow nan)
+        sign2_matrix = list()
+        for ds in list(chemchecker.datasets):
+            if ds == self.dataset.code:
+                continue
+            sign2_ds = chemchecker.get_signature('sign2', 'full_map', ds)
+            _, signs = sign2_ds.get_vectors(my_sign2.keys, include_nan=True)
+            sign2_matrix.append(signs)
+            available = np.isin(my_sign2.keys, sign2_ds.keys)
+            self.__log.info('%s shared molecules between %s and %s',
+                            sum(available), self.dataset, ds)
+        # horizontally stack al signatures
+        sign2_matrix = np.hstack(sign2_matrix)
+        return sign2_matrix, my_sign2[:]
+
+    def fit(self, chemchecker, reuse=True):
         """Learn a model.
 
 
         Args:
             sign2(list): Lst of Signature type 2.
         """
-        #########
-        # step 1:
-        #########
 
-        #########
-        # step 2:
-        #########
-        pass
+        # adanet
+        self.__log.debug('AdaNet fit %s based on other sign2', self.dataset)
+        self.most_frequent_sign2(chemchecker)
+        # get params and set folder
+        adanet_params = self.params['adanet']
+        adanet_path = os.path.join(self.model_path, 'adanet')
+        if adanet_params:
+            if 'model_dir' in adanet_params:
+                adanet_path = adanet_params.pop('model_dir')
+        if not reuse or not os.path.isdir(adanet_path):
+            os.makedirs(adanet_path)
+        # prepare train-test file
+        traintest_file = os.path.join(adanet_path, 'traintest.h5')
+        if adanet_params:
+            traintest_file = adanet_params.get(
+                'traintest_file', traintest_file)
+            adanet_params.pop('traintest_file')
+        if not reuse or not os.path.isfile(traintest_file):
+            features, labels = self.get_sign2_matrix(chemchecker)
+            Traintest.create(features, labels, traintest_file)
+
+        if adanet_params:
+            ada = AdaNet(model_dir=adanet_path,
+                         traintest_file=traintest_file, **adanet_params)
+        else:
+            ada = AdaNet(model_dir=adanet_path, traintest_file=traintest_file)
+        # learn NN with AdaNet
+        self.__log.debug('AdaNet training on %s' % traintest_file)
+        ada.train_and_evaluate()
+        # save AdaNet performances and plots
+        sign2_plot = Plot(self.dataset, adanet_path, self.validation_path)
+        ada.save_performances(adanet_path, sign2_plot)
+        self.__log.debug('model saved to %s' % adanet_path)
 
         Path(os.path.join(self.model_path, self.readyfile)).touch()
 
@@ -91,7 +191,7 @@ class sign3(BaseSignature):
     def cross_fit(sign_from, sign_to, model_path, params={'adanet': {}}, reuse=True):
         """Learn a predictor between sign_from and sign_to.
 
-        Signatures should be `full` to maximaze the intersaction, that's the
+        Signatures should be `full` to maximize the intersaction, that's the
         training input.
         """
         sign3.__log.debug('AdaNet cross fit signatures')
