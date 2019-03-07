302520a56770923b2379e352429137dad0676787
Martino Bertoni <martino.bertoni@irbbarcelona.org>
2019-03-07 10:43:04 +0100

    more tollerant unittests

--- package/tests/test_downloader.py
+++ package/tests/test_downloader.py
@@ -1,8 +1,21 @@
 import os
 import shutil
+import pytest
 import unittest
+import functools
 
 from chemicalchecker.util.download import Downloader
+
+
+def skip_if_import_exception(function):
+    """Assist in skipping tests failing because of missing dependencies."""
+    @functools.wraps(function)
+    def wrapper(*args, **kwargs):
+        try:
+            return function(*args, **kwargs)
+        except ImportError as err:
+            pytest.skip(str(err))
+    return wrapper
 
 
 class TestDownloader(unittest.TestCase):
@@ -21,6 +34,7 @@ class TestDownloader(unittest.TestCase):
         shutil.rmtree(self.tmp_path)
         shutil.rmtree(self.dest_path)
 
+    @skip_if_import_exception
     def test_download(self):
         url = 'http://www.disgenet.org/static/disgenet_ap1/files/downloads/disease_mappings.tsv.gz'
         downloader = Downloader(url, self.dest_path, self.tmp_path)
@@ -29,6 +43,7 @@ class TestDownloader(unittest.TestCase):
         self.assertTrue(os.path.isfile(
             os.path.join(self.dest_path, 'disease_mappings.tsv')))
 
+    @skip_if_import_exception
     def test_wildchar(self):
         url = 'ftp://ftp.ebi.ac.uk/pub/databases/GO/goa/HUMAN/goa_human.gaf.gz'
         downloader = Downloader(url, self.dest_path, self.tmp_path)
@@ -44,6 +59,7 @@ class TestDownloader(unittest.TestCase):
         with self.assertRaises(RuntimeError):
             Downloader.validate_url(url)
 
+    @skip_if_import_exception
     def test_file(self):
         url = 'file://' + os.path.realpath(__file__)
         downloader = Downloader(url, self.dest_path, self.tmp_path)
