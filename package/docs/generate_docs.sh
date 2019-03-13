a3bf67c6c8ece4d46c1abda826ee3c9b7a18e7cc
Martino Bertoni <martino.bertoni@irbbarcelona.org>
2019-03-13 16:19:56 +0100

    more reasonable with docs

--- package/docs/generate_docs.sh
+++
@@ -1,6 +0,0 @@
-# generate api rst from code docstrings
-# -f overwrite existing
-# -e each module on it's own page
-sphinx-apidoc -o source ../chemicalchecker -f -e
-# generate doc html in docs/_build/html
-make html
