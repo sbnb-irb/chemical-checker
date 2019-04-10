1ceb88cf9bd514f3f5fa8972210dd561c907f007
Oriol Guitart Pla <oriol.guitart@irbbarcelona.org>
2019-04-10 18:08:15 +0200

    Change setup to allow development mode

--- setup_chemicalchecker.sh
+++ setup_chemicalchecker.sh
@@ -1,56 +1,5 @@
 #!/bin/sh
 
-KEY="-----BEGIN OPENSSH PRIVATE KEY-----
-b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAACFwAAAAdzc2gtcn
-NhAAAAAwEAAQAAAgEA41gEjOrjNGPLmIJimz5p/XsgywMkszrQj9wPSloB0cERLRvYBfoy
-+Ut4+4/nRwpTS5w708UhktRjGEkvTRY0vgu2FcR+5MLtA1dZbCyHQ/Zf9WMzoFWGgi+kR2
-G0LBLYHUQYpuppCTN4RqEMqmSXX8mYNI1uW838x9PgDGRgE8K5auTROj0x1/JVnU66OlXf
-bTFvmAujzdkd+CdYhTbMb9HldaPh/CYHooVV6+vVukyz4iozZwW8u/J4oPF5Matcl+04GU
-5eF+qEATsSoxWunIfr4rMirA9w5noaBDA/LM83Q/zqX4WmqSeOBUQ5BIP97I0pXv6G6Y1D
-XoIIuHKPSSA+R2Q6i817SrlXiA2qJMSmaLA8f4zo2gMovo8w40lN9vpA/Ow8aZ6zfd1ScA
-/9tOvD8EHVsYve9boFk2U3hHE2c06qmUaGQm2xLtO2QHs+PItGIkyPLhYUPQVojtC0flOj
-LbmLB4UIPUkTI2wr7Kliv66ppkc/lSXgRMOVn/r3UfL6cf6u9ieIcd9QoKRFqyQmSXtR2v
-+95OrqBaFTRrmOaqdjVkpftjBCsD2IbKLnEFe8V9Jfarf8NOqsgxpnjIA8YmsAS02HPr60
-RcXR+eOQdCydne0dq3H6ZXTEy2+bfALqY/oT9S/XSTXCtuZUMAEtqXqjZuJsOIOma+7FWq
-cAAAdYbGjVT2xo1U8AAAAHc3NoLXJzYQAAAgEA41gEjOrjNGPLmIJimz5p/XsgywMkszrQ
-j9wPSloB0cERLRvYBfoy+Ut4+4/nRwpTS5w708UhktRjGEkvTRY0vgu2FcR+5MLtA1dZbC
-yHQ/Zf9WMzoFWGgi+kR2G0LBLYHUQYpuppCTN4RqEMqmSXX8mYNI1uW838x9PgDGRgE8K5
-auTROj0x1/JVnU66OlXfbTFvmAujzdkd+CdYhTbMb9HldaPh/CYHooVV6+vVukyz4iozZw
-W8u/J4oPF5Matcl+04GU5eF+qEATsSoxWunIfr4rMirA9w5noaBDA/LM83Q/zqX4WmqSeO
-BUQ5BIP97I0pXv6G6Y1DXoIIuHKPSSA+R2Q6i817SrlXiA2qJMSmaLA8f4zo2gMovo8w40
-lN9vpA/Ow8aZ6zfd1ScA/9tOvD8EHVsYve9boFk2U3hHE2c06qmUaGQm2xLtO2QHs+PItG
-IkyPLhYUPQVojtC0flOjLbmLB4UIPUkTI2wr7Kliv66ppkc/lSXgRMOVn/r3UfL6cf6u9i
-eIcd9QoKRFqyQmSXtR2v+95OrqBaFTRrmOaqdjVkpftjBCsD2IbKLnEFe8V9Jfarf8NOqs
-gxpnjIA8YmsAS02HPr60RcXR+eOQdCydne0dq3H6ZXTEy2+bfALqY/oT9S/XSTXCtuZUMA
-EtqXqjZuJsOIOma+7FWqcAAAADAQABAAACACk/yBuVZsITtVYnmiKW0X5Xf9y15kvKyRrB
-D9kquZfIUwh/O9Ph0Kvg4MOsGYePPQvD/irvYqcUeCJo7Pcsp0cwMsqCS4/QOlbX23bAIY
-7YPhL0++IEgpJOeJ0nAhzZEnNE7sAiWgp8Vz9haz3jYH/VwRMP8k0f89SCAe3drH0JYMEf
-YUMS5V9MLfKL3UhGWnKifJ/2xLLNyR1EblBFYBSN3fdZU+B/OfVoAe37bV0PG7hWddskdF
-kZpn91MMstT3CVbw9QY8wtFeRvRnNmtLbcBpWscQcNCiwXTW8jHeNQebORSSkpw9J/Rcp4
-1WuXU1Q/TJlazznDEs0sBM8KoiqH67Iow5lYUHJ7d//rG1B6QDWSuUb8XLsMnb7W/dsvCC
-UX3GZwxjVMqg0CsqWYCt59PHs5bVx1AGrIrV7k/E0BxECARfEtLN9jMPjK1ZHvrn9/R6fw
-/OQnHE7RfjdtDGUERNp3xMG2vRK7YFunJ9AQgkvqIX7LbFVeF+S4pKlzxiRaOoPUq16w/d
-YGUo8K3evPK8WPHBXgFCAbzJOhFtjRQkxeYq3Pxe70UMRHfhKk7PoNd5DT70Em3O4EDFYd
-nArXsExIoa/LcNzzrtQP1WpEaTB5/38lkuj6GifGCo4QeSSYG8+/TvFnD+DAlCuBK9mq5U
-Gn+k+f5+3JdUpk4x3xAAABAQCw+trdE4JE8rhatyz1LMxdCsNjOf5DbRN8iRNZjsaONN3M
-fbfv6mEyh1abWWwF/L6gSw9/1MQrH23oY1VP3B4+LECFGSP/FIqr2VdWakw9YoaPUFs+9z
-EqnA9SI40ztdoqng3Bl+0igTl9i6EjVI0T+EcQ0te8y2rc0TBA+usxULNUt/LZehHRA/yJ
-97mLIHH3kpFi1dWkOtVJMeNfqF6btl0yNRCghQDdJPhj7bhGt6aVjrMr+E6zAx13BJbIrw
-+/vPtMuW/Ex35OjqHnuCewM8IxIm6NLWLvgfOQjRROVX3t0wLBxhKlG7yYssKmxw7cXWpO
-kVVLdmRUpovdGaNYAAABAQD1OZHAI5kHwr4lDsVEvkpsuQxGWGzEApMZ9QN90eSEWa2EOq
-0gAyc9mfrxTKhNiyWclbNjRQD48XYXfOT57iDk3w7WtOBcbMgFk+/PJFo+xTGTIhoftVIY
-5zW2QjgqW8gQ/QsjTaeHhj/34XE91aq0qrZOgc6jMLnOr8yp4QKL1pyjXobxvNRWoeKs15
-JHJJDxCJOqmk/ualys3ZIJ4Vy0cNIR/zSzFIblFAKnVTkpz9D5qIB/yr2QriLY3WIlkmyW
-2SCalhEGUD4LpPrXtfyP/gteABAsG76eQcMVXNJzsFV04yP1kGZJYUZVeu3QB0ak5H8/s3
-OTQCII2VRLVYLZAAABAQDtVU/dMIAyjcwR2hKRkMXsAnaNGAdh2MqCcvkInYhn67y/g37m
-MfAZQA4ck91cFtXV7jjz/s//kLExPt40nuFXDUNCfnxUYvc1UeJ4JpUyMba7RnkwIIOc44
-fMQN0iknN2pN7deVgUGJ30v0T0MuyLPbjOKNAQfwxfy5a5dpERLzdLXuduA+g9cBtpUOAW
-y5M2KSBXwPkkL4KlgHCgtrNxJSOfuVabXj5hTGsOj3iPKoQvj9HSauMPMNvGMSryavo97b
-tClVG97rjH/ZR1YvfXIAdihPx0NncbWFETS2FE9iwEiT6dnMvZMXIbJF5h8f+geCwToFLZ
-jEkV9Uo6PFl/AAAAHmludGVyYWN0b21lM2RAaXJiYmFyY2Vsb25hLm9yZwECAwQ=
------END OPENSSH PRIVATE KEY-----
-"
-
 LOCAL_CCREPO=$HOME/local_checker
 
 REMOTE_CCREPO=git@gitlab.sbnb.org/project-specific-repositories/chemical_checker.git
@@ -66,6 +15,7 @@ usage () {
     echo ""
     echo "  -i      force update image"
     echo "  -e      edit config file"
+    echo "  -d      path to source code for development mode"
     echo "  -h      print this help"
     echo ""
     exit 1
@@ -80,16 +30,20 @@ version_gt () {
 unset FORCE_UPDATE_IMAGE FORCE_CREATE_IMAGE
 FORCE_UPDATE_IMAGE=false
 FORCE_CREATE_IMAGE=true
-while getopts 'ieh' c
+FORCE_CHANGE_ENV=false
+while getopts 'i:e:d:h' c
 do
   case $c in
     i) FORCE_UPDATE_IMAGE=true ; FORCE_CREATE_IMAGE=false;;
+    d) PATH_BRANCH=$OPTARG ; FORCE_CHANGE_ENV=true ; FORCE_CREATE_IMAGE=false ;;
     e) FORCE_CREATE_IMAGE=false;;
     h) usage ;; esac
 done
 
 echo $FORCE_UPDATE_IMAGE;
 echo $FORCE_CREATE_IMAGE;
+echo $FORCE_CHANGE_ENV;
+echo $PATH_BRANCH
 
 # check if needed binaries are available
 _=$(command -v singularity);
@@ -128,7 +82,7 @@ else
     
 fi
 
-ssh-agent bash -c "ssh-add <(echo '$KEY'); git archive --remote=ssh://$REMOTE_CCREPO HEAD container/singularity/cc-full.def | tar -xf - -O container/singularity/cc-full.def > cc.def"
+wget http://gitlab.sbnb.org/project-specific-repositories/chemical_checker/raw/master/container/singularity/cc-full.def -O cc.def
 if [ $? -eq 0 ]; then
     printf -- '\033[32m SUCCESS: def file cloned. \033[0m\n';
 else
@@ -166,6 +120,14 @@ else
     
 fi
 
+if [ "$FORCE_CHANGE_ENV" = true ]
+then
+    text_replace_py="/export PYTHONPATH/c\\    export PYTHONPATH=\""$PATH_BRANCH"/package\":\$PYTHONPATH"
+    text_replace_conf="/export CC_CONFIG/c\\    export CC_CONFIG=\""$PATH_BRANCH"/cc_config.json\""
+    sudo singularity exec  --writable $LOCAL_IMAGE_SANDBOX sed -i "$text_replace_py" /environment
+    sudo singularity exec  --writable $LOCAL_IMAGE_SANDBOX sed -i "$text_replace_conf" /environment
+    printf -- '\033[32m SUCCESS: Changing image environment variables. \033[0m\n' ;
+fi
 
 
 printf -- 'Changing chemical checker config file.\n';
