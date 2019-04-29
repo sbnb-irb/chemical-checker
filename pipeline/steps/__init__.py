43258ef59175c7a0eec0dd997d56c838516bd737
Oriol Guitart Pla <oriol.guitart@irbbarcelona.org>
2019-04-29 12:37:25 +0200

    Add new steps in update pipeline

--- pipeline/steps/__init__.py
+++ pipeline/steps/__init__.py
@@ -3,3 +3,4 @@ from s1_0_downloads import Downloads
 from s1_1_molrepos import Molrepos
 from s1_2_molprops import Molproperties
 from s1_3_validations import Validations
+from s2_signature0 import Signature0
