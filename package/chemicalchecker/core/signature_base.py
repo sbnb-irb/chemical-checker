90475016ec10bb2bd029004153cf003478db8e64
Martino Bertoni <martino.bertoni@irbbarcelona.org>
2019-05-17 15:44:24 +0200

    now validation is performed on every file found in the validation diretory

--- package/chemicalchecker/core/signature_base.py
+++ package/chemicalchecker/core/signature_base.py
@@ -10,6 +10,7 @@ import os
 import six
 import sys
 import h5py
+import json
 import pickle
 import tempfile
 import numpy as np
@@ -171,11 +172,24 @@ class BaseSignature(object):
         On Mode Of Action (MOA) and Anatomical Therapeutic Chemical (ATC).
         """
         plot = Plot(self.dataset, self.stats_path, self.validation_path)
+        stats = {"molecules": len(self.keys)}
         results = dict()
-        for validation_set in ['moa', 'atc']:
+        for validation_file in os.listdir(self.validation_path):
+            vset = validation_file.split('_')[0]
             res = plot.vector_validation(self, self.__class__.__name__,
-                                         prefix=validation_set, h5_input=True)
-            results[validation_set] = res
+                                         prefix=vset, h5_input=True)
+            results[vset] = res
+            stats.update({
+                "%s_ks_d" % vset: res[0][0],
+                "%s_ks_p" % vset: res[0][1],
+                "%s_auc" % vset: res[1],
+                "%s_cov" % vset: res[2],
+            })
+
+        validation_stat_file = os.path.join(
+            self.stats_path, 'validation_stats.json')
+        with open(validation_stat_file, 'w') as fp:
+            json.dump(stats, fp)
         plot.matrix_plot(self.data_path)
         return results
 
