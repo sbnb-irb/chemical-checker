30e4f70e588d51a7fbf0ea3ee103c7d3365b8db4
mduran <miquel.duran@irbbarcelona.org>
2019-05-23 18:13:33 +0200

    Targetmate work-in-progress

--- package/chemicalchecker/tool/targetmate/targetmate.py
+++ package/chemicalchecker/tool/targetmate/targetmate.py
@@ -1,19 +1,32 @@
 
+from sklearn.model_selection import check_cv
 
 @logged
 class TargetMate:
 	"""TargetMate Classifier
 
+    Parameters
+    ----------
+
+    cv : int or cross-validation generator, optional (default=None)
+        The default cross-validation generator used is Stratified K-Folds.
+        If an integer is provided, then it is the number of folds used.
+        See the module :mod:`sklearn.model_selection` module for the
+        list of possible cross-validation objects.
+
+
 	"""
-    def __init__(self, base_clf = None, datasets = "all", models_path = None):
+    def __init__(self, base_clf = None, cv = 5, datasets = "all", models_path = None):
     	# Set the base classifier
         if not base_clf:
         	from sklearn.linear_model import LogisticRegressionCV
         	self.base_clf = LogisticRegressionCV(class_weight = "balanced")
         else:
         	self.base_clf = base_clf
+        # Crossvalidation to determine the performances of the individual predictors
+        self.cv = cv
         # Store the paths to the sign3 (only the ones that have been already trained)
-        if datasets == "all":
+        if datasets not datasets:
             self.datasets = ["%s%d.001" for x in ["A","B","C","D","E"] for i in [1,2,3,4,5]] # TO-DO: @Martino, is there a way to just check how many signatures are available?
         else:
             self.datasets = datasets
@@ -26,7 +39,6 @@ class TargetMate:
         # XXX
         self.datasets
         
-
 	def fit(self, data, standardise = True):
         """
         XXXX
@@ -36,10 +48,15 @@ class TargetMate:
             with open(data, "r") as f:
                 data = []
                 for r in csv.reader(f, delimiter = "\t"):
-                	data += [(r[0], r[1])]
+                	data += [(r[0], int(r[1]))]
         # Get signatures
         self.__log.info("Calculating sign3 for every molecule. Invalid SMILES will be removed.")
         
+
+        # Initialize cross-validation generator
+        cv = check_cv(self.cv, y, classifier=True)
+        folds = list(cv.split(X, y))
+
             
     def predict(self, data_path):
         ""
