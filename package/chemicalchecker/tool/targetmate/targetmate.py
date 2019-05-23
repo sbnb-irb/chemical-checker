f118319fcd00c445d71836a3e71e2f91b20bb1ca
mduran <miquel.duran@irbbarcelona.org>
2019-05-23 17:45:42 +0200

    Working on targetmate

---
+++ package/chemicalchecker/tool/targetmate/targetmate.py
@@ -0,0 +1,49 @@
+
+
+@logged
+class TargetMate:
+	"""TargetMate Classifier
+
+	"""
+    def __init__(self, base_clf = None, datasets = "all", models_path = None):
+    	# Set the base classifier
+        if not base_clf:
+        	from sklearn.linear_model import LogisticRegressionCV
+        	self.base_clf = LogisticRegressionCV(class_weight = "balanced")
+        else:
+        	self.base_clf = base_clf
+        # Store the paths to the sign3 (only the ones that have been already trained)
+        if datasets == "all":
+            self.datasets = ["%s%d.001" for x in ["A","B","C","D","E"] for i in [1,2,3,4,5]] # TO-DO: @Martino, is there a way to just check how many signatures are available?
+        else:
+            self.datasets = datasets
+        
+        # XXX
+        if models_path is None
+            self.models_path = ""
+        else:
+        	self.models_path = models_path
+        # XXX
+        self.datasets
+        
+
+	def fit(self, data, standardise = True):
+        """
+        XXXX
+        """
+        # Read data if it is a file
+        if type(data) == str:
+            with open(data, "r") as f:
+                data = []
+                for r in csv.reader(f, delimiter = "\t"):
+                	data += [(r[0], r[1])]
+        # Get signatures
+        self.__log.info("Calculating sign3 for every molecule. Invalid SMILES will be removed.")
+        
+            
+    def predict(self, data_path):
+        ""
+
+
+	def predict_proba(self, ):
+        ""
