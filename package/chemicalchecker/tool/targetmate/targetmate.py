524c85a1637f9d04517f256d1abb98ba294d9c10
Miquel Duran-Frigola <miquel.duran@irbbarcelona.org>
2019-05-24 08:41:31 +0200

    Working on targetmat

--- package/chemicalchecker/tool/targetmate/targetmate.py
+++ package/chemicalchecker/tool/targetmate/targetmate.py
@@ -1,12 +1,19 @@
-
 from sklearn.model_selection import check_cv
 
 @logged
 class TargetMate:
-	"""TargetMate Classifier
+	"""TargetMate Classifier.
 
+    An ensemble-based classifier based on CC signatures of different types.
+    A base classifier is specified, and predictions are made for each dataset
+    individually. A meta-prediction is then provided based on individual predictions,
+    together with a measure of confidence for each prediction.
+    The classifier is greatly inspired by PidginV3: https://pidginv3.readthedocs.io/en/latest/usage/index.html
+    
     Parameters
     ----------
+    base_clf : classifier instance, containing fit and predict_proba methods (default=None)
+        By default, sklearn LogisticRegressionCV is used.
 
     cv : int or cross-validation generator, optional (default=None)
         The default cross-validation generator used is Stratified K-Folds.
@@ -14,9 +21,13 @@ class TargetMate:
         See the module :mod:`sklearn.model_selection` module for the
         list of possible cross-validation objects.
 
+    datasets : list of CC datasets (A1.001-E5.999) (default=None)
+        All datasets having a smile-to-sign3 predictor trained are used by default.
 
+    models_path : str (default=None)
+        Directory where models will be stored.
 	"""
-    def __init__(self, base_clf = None, cv = 5, datasets = "all", models_path = None):
+    def __init__(self, base_clf = None, cv = 5, datasets = None, models_path = None):
     	# Set the base classifier
         if not base_clf:
         	from sklearn.linear_model import LogisticRegressionCV
@@ -30,10 +41,9 @@ class TargetMate:
             self.datasets = ["%s%d.001" for x in ["A","B","C","D","E"] for i in [1,2,3,4,5]] # TO-DO: @Martino, is there a way to just check how many signatures are available?
         else:
             self.datasets = datasets
-        
         # XXX
         if models_path is None
-            self.models_path = ""
+            self.models_path = "./" # TO-DO: @Martino, can you please help me with the default here?
         else:
         	self.models_path = models_path
         # XXX
