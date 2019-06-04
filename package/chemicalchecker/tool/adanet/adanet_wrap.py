26356116dd777ab658ff2e274b76f0424fa0fb50
Martino Bertoni <martino.bertoni@irbbarcelona.org>
2019-06-04 16:50:13 +0200

    string nightmare is over

--- package/chemicalchecker/tool/adanet/adanet_wrap.py
+++ package/chemicalchecker/tool/adanet/adanet_wrap.py
@@ -238,7 +238,7 @@ class Traintest(object):
             if len(split_names) != len(split_fractions):
                 raise Exception(
                     "Split names and fraction should be same amount.")
-            split_names = [s.decode() for s in split_names]
+            split_names = [s.encode() for s in split_names]
             split_idxs = Traintest.get_split_indeces(rows, split_fractions)
 
             Traintest.__log.info('Traintest saving to %s', out_file)
