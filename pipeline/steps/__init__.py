6bd9b0e9e35a9e12cf61ecbb105e20cd2d7b9386
Oriol Guitart Pla <oriol.guitart@irbbarcelona.org>
2019-06-13 15:49:05 +0200

    More steps for the pipeline package

--- pipeline/steps/__init__.py
+++ pipeline/steps/__init__.py
@@ -8,3 +8,7 @@ from s3_signature1 import Signature1
 from s4_clustering1 import Clustering1
 from s5_neigh1 import Neigh1
 from s6_projection1 import Projection1
+from s7_signature2 import Signature2
+from s8_signature3 import Signature3
+from s9_link_exemplary import ExemplaryLinks
+from s10_neigh2 import Neigh2
