c622eb83bc180008ea32332551266a924e065f74
Martino Bertoni <martino.bertoni@irbbarcelona.org>
2019-06-26 10:26:16 +0200

    added predicthpc method for targetmate

--- package/chemicalchecker/tool/targetmate/targetmate.py
+++ package/chemicalchecker/tool/targetmate/targetmate.py
@@ -144,6 +144,7 @@ class TargetMate:
             self.datasets = datasets
         # preloaded neural netoworks
         if sign3_predict_fn is None:
+            self.sign3_predict_fn = dict()
             for ds in self.datasets:
                 self.__log.debug("Loading sign3 predictor for %s" % ds)
                 s3 = self.cc.get_signature("sign3", "full", ds)
@@ -757,7 +758,76 @@ class TargetMate:
         params = {}
         params["num_jobs"] = kwargs.get("num_jobs", len(elements) / 100)
         params["jobdir"] = job_path
-        params["job_name"] = "TARGETMATE"
+        params["job_name"] = "TARGETMATE_fit"
+        params["elements"] = elements
+        params["wait"] = False
+        params["memory"] = 8
+        params["cpu"] = cpu
+        # job command
+        singularity_image = cfg.PATH.SINGULARITY_IMAGE
+        command = "SINGULARITYENV_PYTHONPATH={} SINGULARITYENV_CC_CONFIG={}" +\
+            " OMP_NUM_THREADS={} singularity exec {} python {} <TASK_ID> <FILE>"
+        command = command.format(
+            os.path.join(cfg.PATH.CC_REPO, 'package'), cc_config, str(cpu),
+            singularity_image, script_name)
+        # submit jobs
+        cluster = HPC(Config())
+        cluster.submitMultiJob(command, **params)
+        return cluster
+
+    @staticmethod
+    def predict_all_hpc(models_path, signature_path, results_path, **kwargs):
+        """Run HPC jobs to predict with all models for input molecules.
+
+        Args:
+            models_path(str): Where models are saved.
+            signature_path(str): Directory with all sign3 for molecules to
+                predict.
+            job_path(str): Path (usually in scratch) where the script files are
+                generated.
+            results_path(str): Path where to save predictions.
+        """
+        # read config file
+        cc_config = kwargs.get("cc_config", os.environ['CC_CONFIG'])
+        cfg = Config(cc_config)
+        # create job directory if not available
+        job_base_path = cfg.PATH.CC_TMP
+        tmp_dir = tempfile.mktemp(prefix='tmp_', dir=job_base_path)
+        job_path = kwargs.get("job_path", tmp_dir)
+        if not os.path.isdir(job_path):
+            os.mkdir(job_path)
+        # check cpus
+        cpu = kwargs.get("cpu", 4)
+        # create script file
+        script_lines = [
+            "import sys, os",
+            "from tqdm import tqdm",
+            "import pickle",
+            "from chemicalchecker.tool.targetmate import TargetMate",
+            "task_id = sys.argv[1]",  # <TASK_ID>
+            "filename = sys.argv[2]",  # <FILE>
+            "inputs = pickle.load(open(filename, 'rb'))",  # load pickled data
+            "for mdl_dir in tqdm(inputs[task_id]):",
+            "    mdl_dir = str(mdl_dir)",  # elements for current job
+            "    mdl_name = os.path.normpath(mdl_dir).split('/')[-1]",
+            "    result_file = os.path.join('%s', mdl_name)" % results_path,
+            "    if os.path.isfile(result_file): continue",
+            "    tm = pickle.load(open(os.path.join(mdl_dir,'TargetMate.pkl'),'r'))",
+            "    results = tm.predict('%s')" % signature_path,
+            "    pickle.dump(results, open(result_file,'w'))",
+            "print('JOB DONE')"
+        ]
+        script_name = os.path.join(job_path, 'predict_all_targetmate.py')
+        with open(script_name, 'w') as fh:
+            for line in script_lines:
+                fh.write(line + '\n')
+        # hpc parameters
+        elements = [os.path.join(models_path, f)
+                    for f in sorted(os.listdir(models_path))]
+        params = {}
+        params["num_jobs"] = kwargs.get("num_jobs", len(elements) / 100)
+        params["jobdir"] = job_path
+        params["job_name"] = "TARGETMATE_predict"
         params["elements"] = elements
         params["wait"] = False
         params["memory"] = 8
