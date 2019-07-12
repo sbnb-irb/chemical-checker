fixed masking when required dataset has multiple dimensions

package/chemicalchecker/core/signature_data.py
@@ -83,18 +83,18 @@ class DataSignature(object):
                 raise Exception("HDF5 file has no '%s'." % h5_dataset_name)
             if mask is None:
                 ndim = hf[h5_dataset_name].ndim
-                if hasattr(hf[h5_dataset_name][(0,)*ndim], 'encode'):
+                if hasattr(hf[h5_dataset_name][(0,) * ndim], 'encode'):
                     encoder = np.vectorize(lambda x: x.encode())
                     return encoder(hf[h5_dataset_name][:])
                 else:
                     return hf[h5_dataset_name][:]
             else:
                 ndim = hf[h5_dataset_name].ndim
-                if hasattr(hf[h5_dataset_name][(0,)*ndim], 'encode'):
+                if hasattr(hf[h5_dataset_name][(0,) * ndim], 'encode'):
                     encoder = np.vectorize(lambda x: x.encode())
                     return encoder(hf[h5_dataset_name][mask])
                 else:
-                    return hf[h5_dataset_name][mask]
+                    return hf[h5_dataset_name][mask, :]
 
     def __getitem__(self, key):
         """Return the vector corresponding to the key.