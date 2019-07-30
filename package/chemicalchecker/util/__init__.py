e7be5ae477d6da5b15e4113f58da9036d7e2c788
Martino Bertoni <martino.bertoni@irbbarcelona.org>
2019-07-30 15:31:52 +0200

    fix logging overriding by tenserflow

--- package/chemicalchecker/util/__init__.py
+++ package/chemicalchecker/util/__init__.py
@@ -1,4 +1,22 @@
-# only basic imports here
+try:
+    # FIXME(https://github.com/abseil/abseil-py/issues/99)
+    # FIXME(https://github.com/abseil/abseil-py/issues/102)
+    # Unfortunately, many libraries that include absl (including Tensorflow)
+    # will get bitten by double-logging due to absl's incorrect use of
+    # the python logging library:
+    #   2019-07-19 23:47:38,829 my_logger   779 : test
+    #   I0719 23:47:38.829330 139904865122112 foo.py:63] test
+    #   2019-07-19 23:47:38,829 my_logger   779 : test
+    #   I0719 23:47:38.829469 139904865122112 foo.py:63] test
+    # The code below fixes this double-logging.  FMI see:
+    #   https://github.com/tensorflow/tensorflow/issues/26691#issuecomment-500369493
+    import logging
+    import absl.logging
+    logging.root.removeHandler(absl.logging._absl_handler)
+    absl.logging._warn_preinit_stderr = False
+except Exception as e:
+    print("Failed to fix absl logging bug", e)
+    pass
 from .logging.our_logging import *
 from autologging import logged
 from .logging.profilehooks import profile
