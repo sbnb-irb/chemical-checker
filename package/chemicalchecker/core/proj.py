1fd3499ab7fbe1e14b63403c80fece882771dbc1
Martino Bertoni <martino.bertoni@irbbarcelona.org>
2019-08-08 15:48:18 +0200

    adapt args kwargs default projector

--- package/chemicalchecker/core/proj.py
+++ package/chemicalchecker/core/proj.py
@@ -4,6 +4,7 @@ from .signature_base import BaseSignature
 from .signature_data import DataSignature
 
 from .projector import Default
+from .projector import PCA
 
 from chemicalchecker.util import logged
 
@@ -12,36 +13,30 @@ from chemicalchecker.util import logged
 class proj(BaseSignature, DataSignature):
     """A Signature bla bla."""
 
-    def __init__(self, signature_path, dataset, **params):
+    def __init__(self, signature_path, dataset, proj_type='Default', **kwargs):
         """Initialize the projection class.
 
         Args:
             signature_path(str): the path to the signature directory.
             dataset(object): The dataset object with all info related
-            params(dict): the key is a projector name with the value being
+            kwargs(dict): the key is a projector name with the value being
                 a dictionary of its parameters.
         """
         # Calling init on the base class to trigger file existance checks
-        BaseSignature.__init__(self, signature_path, dataset, **params)
+        BaseSignature.__init__(self, signature_path, dataset, **kwargs)
         self.__log.debug('signature path is: %s', signature_path)
 
         # define which projector is needed
-        if len(params) == 0:
-            params = {'Default': {}}
-        if len(params) > 1:
-            raise Exception("Please specificy one single algorithm")
-        proj_name = params.keys()[0]
-        self.data_path = os.path.join(signature_path, "proj_%s.h5" % proj_name)
+        self.data_path = os.path.join(signature_path, "proj_%s.h5" % proj_type)
         DataSignature.__init__(self, self.data_path)
         self.__log.debug('data_path: %s', self.data_path)
 
-        self.projector = eval(proj_name)(signature_path, dataset,
-                                         **params[proj_name])
+        self.projector = eval(proj_type)(signature_path, dataset, **kwargs)
 
-    def fit(self, signature, validations=True):
+    def fit(self, signature, validations=True, *args, **kwargs):
         """Take an input and call corresponding projector."""
-        self.projector.fit(signature, validations)
+        self.projector.fit(signature, validations, *args, **kwargs)
 
-    def predict(self, signature, destination):
+    def predict(self, signature, destination, *args, **kwargs):
         """Load projector and predict projection for new data."""
-        self.projector.predict(signature, destination)
+        self.projector.predict(signature, destination, *args, **kwargs)
