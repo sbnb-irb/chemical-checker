47c62219d8d862c9ec9a72651c7e43e1c5c0fa33
Martino Bertoni <martino.bertoni@irbbarcelona.org>
2019-08-09 16:29:27 +0200

    plot function to proj class

--- package/chemicalchecker/core/proj.py
+++ package/chemicalchecker/core/proj.py
@@ -34,9 +34,45 @@ class proj(BaseSignature, DataSignature):
         self.projector = eval(proj_type)(signature_path, dataset, **kwargs)
 
     def fit(self, signature, validations=True, *args, **kwargs):
-        """Take an input and call corresponding projector."""
+        """Take an input learn a 2D representation."""
         self.projector.fit(signature, validations, *args, **kwargs)
 
     def predict(self, signature, destination, *args, **kwargs):
-        """Load projector and predict projection for new data."""
+        """Predict projection for new data."""
         self.projector.predict(signature, destination, *args, **kwargs)
+
+    def plot(self, kind='shaded', *args, **kwargs):
+        """Load projected data and plot it."""
+        # load data in memory
+        proj_data = self.projector[:]
+        # plot projection
+        range_x = max(abs(np.min(proj_data[:, 0])),
+                      abs(np.max(proj_data[:, 0])))
+        range_y = max(abs(np.min(proj_data[:, 1])),
+                      abs(np.max(proj_data[:, 1])))
+        range_max = max(range_y, range_x)
+        frame = range_max / 10.
+        range_max += frame
+        cmap = kwargs.pop('cmap', 'viridis')
+        x_range = kwargs.pop('x_range', (-range_max, range_max))
+        y_range = kwargs.pop('y_range', (-range_max, range_max))
+        plot_size = kwargs.pop('plot_size', (1000, 1000))
+        noise_scale = kwargs.pop('noise_scale', None)
+        self.__log.info("Plot %s range x: %s y: %s" % (kind, x_range, y_range))
+        plot_path = os.path.join(self.stats_path,
+                                 self.projector.__class__.__name__)
+        if not os.path.isdir(plot_path):
+            os.mkdir(plot_path)
+        plot = Plot(self.dataset, plot_path)
+        if kind == 'shaded':
+            plot.datashader_projection(
+                proj_data,
+                self.projector.__class__.__name__,
+                cmap=cmap,
+                x_range=x_range,
+                y_range=y_range,
+                plot_size=plot_size,
+                noise_scale=noise_scale,
+                **kwargs)
+        elif kind == 'largevis':
+            plot.projection_plot(proj_data, **kwargs)
