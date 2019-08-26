6b016de005dcb81889db3f66fa8f12e8018efc95
Martino Bertoni <martino.bertoni@irbbarcelona.org>
2019-08-26 10:50:43 +0200

    projecting categories in separate plots as well

--- package/chemicalchecker/core/proj.py
+++ package/chemicalchecker/core/proj.py
@@ -82,10 +82,19 @@ class proj(BaseSignature, DataSignature):
 
     def plot_over(self, data, name, *args, **kwargs):
         """Load projected data and plot it."""
-        # base of the plot
-        proj_data = np.vstack((self[:], data[:]))
-        category = np.hstack((np.ones(self.shape[0]), np.zeros(len(data))))
-        # plot projection
+        # data can be a list of numpy array hence plotting multiple categories
+        if isinstance(data, list):
+            # stack projetion data
+            proj_data = np.vstack(tuple([self[:]] + data))
+            # stack categories
+            category = np.hstack(tuple(
+                [np.ones(x.shape[0]) * i for i, x in enumerate(
+                    [self[:]] + data)]))
+        else:
+            proj_data = np.vstack((self[:], data))
+            category = np.hstack(
+                (np.ones(self.shape[0]) * 0, np.ones(len(data)) * 1))
+        # handle arguments
         range_x = max(abs(np.min(proj_data[:, 0])),
                       abs(np.max(proj_data[:, 0])))
         range_y = max(abs(np.min(proj_data[:, 1])),
@@ -98,13 +107,17 @@ class proj(BaseSignature, DataSignature):
         y_range = kwargs.pop('y_range', (-range_max, range_max))
         plot_size = kwargs.pop('plot_size', (1000, 1000))
         noise_scale = kwargs.pop('noise_scale', None)
+        category_colors = kwargs.pop('category_colors', None)
+        how = kwargs.pop('how', 'eq_hist')
+        transparent = kwargs.pop('transparent', False)
+        spread = kwargs.pop('spread', 1)
         self.__log.info("Plot range x: %s y: %s" % (x_range, y_range))
         plot_path = os.path.join(self.stats_path,
                                  self.projector.__class__.__name__)
         if not os.path.isdir(plot_path):
             os.mkdir(plot_path)
         plot = Plot(self.dataset, plot_path)
-        plot.datashader_projection(
+        df = plot.datashader_projection(
             proj_data,
             name,
             cmap=cmap,
@@ -113,4 +126,9 @@ class proj(BaseSignature, DataSignature):
             plot_size=plot_size,
             noise_scale=noise_scale,
             category=category,
+            category_colors=category_colors,
+            how=how,
+            transparent=transparent,
+            spread=spread,
             **kwargs)
+        return df
