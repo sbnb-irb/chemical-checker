30c7a9652f3772d4fb02aaab39c0dba0ed802864
Martino Bertoni <martino.bertoni@irbbarcelona.org>
2019-09-04 10:58:31 +0200

    merging targetmate updates from github

---
+++ package/chemicalchecker/tool/targetmate/datasets/chembl/__init__.py
@@ -0,0 +1,1 @@
+from .fetch_chembl import Chembl, ChemblDb
---
+++ package/chemicalchecker/tool/targetmate/utils/metrics.py
@@ -0,0 +1,35 @@
+import numpy as np
+from sklearn import metrics
+
+ALPHA = 1e-6
+
+def roc_score(y_true, y_pred):
+    y_true = list(y_true)
+    y_pred = list(y_pred)
+    score = metrics.roc_auc_score(y_true, y_pred)
+    weight = (max(score, 0.5) - 0.5) / (1. - 0.5)
+    return score, weight + ALPHA
+
+def pr_score(y_true, y_pred):
+    y_true = list(y_true)
+    y_pred = list(y_pred)
+    prec, rec, _ = metrics.precision_recall_curve(y_true, y_pred)
+    score = metrics.auc(rec, prec)
+    weight = (score - 0.) / (1. - 0.)
+    return score, weight + ALPHA
+
+def bedroc_score(y_true, y_pred, alpha = 20.0):
+    y_true = np.array(y_true)
+    y_pred = np.array(y_pred)
+    big_n = len(y_true)
+    n = sum(y_true == 1)
+    order = np.argsort(-y_pred)
+    m_rank = (y_true[order] == 1).nonzero()[0]
+    s = np.sum(np.exp(-alpha * m_rank / big_n))
+    r_a = n / big_n
+    rand_sum = r_a * (1 - np.exp(-alpha))/(np.exp(alpha/big_n) - 1)
+    fac = r_a * np.sinh(alpha / 2) / (np.cosh(alpha / 2) - np.cosh(alpha/2 - alpha * r_a))
+    cte = 1 / (1 - np.exp(alpha * (1 - r_a)))
+    score = s * fac / rand_sum + cte
+    weight = (score - 0.) / (1. - 0.)
+    return score, weight + ALPHA
