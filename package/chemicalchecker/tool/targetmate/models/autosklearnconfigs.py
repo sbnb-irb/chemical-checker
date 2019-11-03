8cca5e280eb813165edbb6365516ac014dc8a5ef
mduran <miquel.duran@irbbarcelona.org>
2019-11-03 23:31:44 +0100

    debug validation, now it doesnt read the last model produced

---
+++ package/chemicalchecker/tool/targetmate/models/autosklearnconfigs.py
@@ -0,0 +1,19 @@
+
+
+class AutoSklearnAdapter(object):
+
+	def __init__(self, **kwargs):
+
+
+    def fit(self, X, y):
+    	self.refit(X, y)
+    	
+
+class AutoSklearnClassifierConfigs:
+
+	def __init__(self, **kwargs):
+        pass
+
+	def as_pipeline(self, X, y, **kwargs):
+		mod = 
+		return 
