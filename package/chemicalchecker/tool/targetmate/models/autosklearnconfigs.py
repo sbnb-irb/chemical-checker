3d7d3521d304a86eb762db10ca2cf29d1d625c9d
mduran <miquel.duran@irbbarcelona.org>
2019-11-14 09:12:26 +0100

    Ensemble targetmate works with autosklearn

--- package/chemicalchecker/tool/targetmate/models/autosklearnconfigs.py
+++ package/chemicalchecker/tool/targetmate/models/autosklearnconfigs.py
@@ -1,7 +1,11 @@
 from autosklearn.classification import AutoSklearnClassifier as BaseAutoSklearnClassifier
 from autosklearn.metrics import roc_auc
+from sklearn import model_selection
 import numpy as np
 import random
+import uuid
+import os
+#os.environ["OMP_NUM_THREADS"] = "1"
 
 
 class AutoSklearnClassifier(BaseAutoSklearnClassifier):
@@ -18,22 +22,46 @@ class AutoSklearnClassifier(BaseAutoSklearnClassifier):
 
 class AutoSklearnClassifierConfigs:
 
-    def __init__(self, n_jobs, **kwargs):
+    def __init__(self, n_jobs, tmp_path, resampling_strategy="cv", **kwargs):
+        """Just set up paths"""
+        auto_path = os.path.join(os.path.join(tmp_path, "autosklearn"))
+        tmp_folder = os.path.join(auto_path, "tmp")
+        output_folder = os.path.join(auto_path, "output")
+        os.makedirs(tmp_folder, exist_ok = True)
+        os.makedirs(output_folder, exist_ok = True)
+        self.tmp_folder = tmp_folder
+        self.output_folder = output_folder
+        self.n_jobs = n_jobs
+        if resampling_strategy == "cv":
+            self.resampling_strategy = model_selection.StratifiedKFold
+            self.resampling_strategy_arguments = {"n_splits": 5, "shuffle": True}
+        else:
+            self.resampling_strategy = model_selection.StratifiedShuffleSplit
+            self.resampling_strategy_arguments = {"train_size": 0.8, "n_splits": 1}
+        
+    def instantiate(self):
+        """Set up a classifier"""
+        tag = str(uuid.uuid4())
+        tmp_folder = os.path.join(self.tmp_folder, tag)
+        output_folder = os.path.join(self.output_folder, tag)
+        # Instantiate classifier
         self.base_mod = AutoSklearnClassifier(
-            time_left_for_this_task = 300,
-            resampling_strategy = 'cv',
-            resampling_strategy_arguments = {'folds': 5},
-            n_jobs = n_jobs,
+            time_left_for_this_task = 3600,
+            resampling_strategy = self.resampling_strategy,
+            resampling_strategy_arguments = self.resampling_strategy_arguments,
+            tmp_folder = tmp_folder,
+            output_folder = output_folder,
             delete_tmp_folder_after_terminate = False,
             delete_output_folder_after_terminate = False
             )
 
     def as_pipeline(self, X, y, **kwargs):
+        """Prefit and refit"""
+        self.instantiate()
         shuff = np.array(range(len(y)))
         random.shuffle(shuff)
         mod = self.base_mod
         mod.prefit(X, y)
-        mod.fit(X, y)
         return mod
 
 
