21c9f1f7eaca25da15ef3d24f4304d3bd2c8813c
mduran <miquel.duran@irbbarcelona.org>
2019-11-14 09:12:26 +0100

    Ensemble targetmate works with autosklearn

--- package/chemicalchecker/tool/targetmate/tmsetup.py
+++ package/chemicalchecker/tool/targetmate/tmsetup.py
@@ -72,7 +72,7 @@ class TargetMateSetup(HPCUtils):
             if overwrite:
                 # Cleaning models directory
                 self.__log.debug("Cleaning %s" % self.models_path)
-                shutil.rmtree(self.models_path)
+                shutil.rmtree(self.models_path, ignore_errors=True)
                 os.mkdir(self.models_path)
         self.bases_models_path, self.signatures_models_path, self.predictions_models_path = self.directory_tree(self.models_path)
         self._bases_models_path, self._signatures_models_path, self._predictions_models_path = self.bases_models_path, self.signatures_models_path, self.predictions_models_path 
@@ -239,11 +239,11 @@ class TargetMateSetup(HPCUtils):
     def wipe(self):
         """Delete temporary data"""
         self.__log.debug("Removing %s" % self.tmp_path)
-        shutil.rmtree(self.tmp_path)
+        shutil.rmtree(self.tmp_path, ignore_errors=True)
         for job_path in self.job_paths:
             if os.path.exists(job_path):
                 self.__log.debug("Removing %s" % job_path)
-                shutil.rmtree(job_path)
+                shutil.rmtree(job_path, ignore_errors=True)
 
 
 @logged
@@ -302,7 +302,7 @@ class TargetMateClassifierSetup(TargetMateSetup):
             self.algo = TPOTClassifierConfigs(self.algo, n_jobs=self.n_jobs)
         if self.model_config == "autosklearn":
             from .models.autosklearnconfigs import AutoSklearnClassifierConfigs
-            self.algo = AutoSklearnClassifierConfigs(n_jobs=self.n_jobs)
+            self.algo = AutoSklearnClassifierConfigs(n_jobs=self.n_jobs, tmp_path=self.tmp_path)
         # Weight algo
         self.weight_algo = VanillaClassifierConfigs(weight_algo, n_jobs=self.n_jobs)
         # Minimum size of the minority class
