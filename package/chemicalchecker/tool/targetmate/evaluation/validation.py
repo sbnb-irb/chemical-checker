397a0a7054ca7e8a95e6af8a9be651d98e893c7e
mduran <miquel.duran@irbbarcelona.org>
2019-11-16 16:21:35 +0100

    works with stacked, to be tested

--- package/chemicalchecker/tool/targetmate/evaluation/validation.py
+++ package/chemicalchecker/tool/targetmate/evaluation/validation.py
@@ -1,6 +1,7 @@
-"""Validate a TargetMate classifier"""
+"""Validate a TargetMate model (regression or classifier)"""
 import os
 import numpy as np
+import h5py
 import pickle
 from chemicalchecker.util import logged
 from sklearn import model_selection
@@ -36,59 +37,68 @@ class Performances:
         """Given an active (1) / inactive (0) dataset, assign 1 to inactives"""
         return np.abs(1 - yt)
 
-    def classifier_performances(self, yt, yp):
+    def classifier_performances(self, Yt, Yp):
         """Calculate standard prediction performance metrics.
         In addition, it calculates the corresponding weights.
         For the moment, AUROC, AUPR and BEDROC are used.
         Args:
-            yt(array): Truth data (binary).
-            yp(array): Prediction scores (probabilities).
+            Yt(list of lists): Truth data (binary).
+            Yp(list of lists): Prediction scores (probabilities).
         """
-        shape = yp.shape
-        if len(shape) == 1:
-            self.__log.debug("Prediction has only one dimension, i.e. score for active")
-            self.auroc  = metrics.roc_score(yt, yp)[0]
-            self.aupr   = metrics.pr_score(yt, yp)[0]
-            self.bedroc = metrics.bedroc_score(yt, yp)[0]
-        else:
-            assert shape[1] == 2, "Too many classes. For the moment, TargetMate only works with active/inactive (1/0)"
-            assert set(yt) == set([0,1]), "Only 1/0 is accepted for the moment."
-            if len(shape) == 2:
-                self.__log.debug("Prediction has two dimensions (active/inactive). Not ensemble.")
-                self.auroc  = np.zeros(2)
-                self.aupr   = np.zeros(2)
-                self.bedroc = np.zeros(2)
-                self.__log.debug("Measuring performance for inactives")
-                yt_ = self.truth_inv(yt)
-                self.auroc[0]  = metrics.roc_score(yt_, yp[:,0])[0]
-                self.aupr[0]   = metrics.pr_score(yt_, yp[:,0])[0]
-                self.bedroc[0] = metrics.bedroc_score(yt_, yp[:,0])[0]
-                self.__log.debug("Measuring performances for actives")
-                yt_ = yt
-                self.auroc[1]  = metrics.roc_score(yt_, yp[:,1])[0]
-                self.aupr[1]   = metrics.pr_score(yt_, yp[:,1])[0]
-                self.bedroc[1] = metrics.bedroc_score(yt_, yp[:,1])[0]
-            if len(shape) == 3:
-                self.__log.debug("Prediction has three dimensions (active/inactives and ensemble of datasets)")
-                ens_size = shape[2]
-                self.auroc  = np.zeros((2, ens_size))
-                self.aupr   = np.zeros((2, ens_size))
-                self.bedroc = np.zeros((2, ens_size))
-                self.__log.debug("Measuring performance for inactives")
-                yt_ = self.truth_inv(yt)
-                for k in range(0, ens_size):
-                    self.auroc[0, k]  = metrics.roc_score(yt_, yp[:,0,k])[0]
-                    self.aupr[0, k]   = metrics.pr_score(yt_, yp[:,0,k])[0]
-                    self.bedroc[0, k] = metrics.bedroc_score(yt_, yp[:,0,k])[0]
-                self.__log.debug("Measuring performance for inactives")
-                yt_ = yt
-                for k in range(0, ens_size):
-                    self.auroc[1, k]  = metrics.roc_score(yt_, yp[:,1,k])[0]
-                    self.aupr[1, k]   = metrics.pr_score(yt_, yp[:,1,k])[0]
-                    self.bedroc[1, k] = metrics.bedroc_score(yt_, yp[:,1,k])[0]
+        self.__log.debug("Initialize variables")
+        self.auroc  = []
+        self.bedroc = []
+        self.aupr   = []
+        self.__log.debug("Calculating performances")
+        for yt, yp in zip(Yt, Yp):
+            shape = yp.shape
+            if len(shape) == 1:
+                self.__log.debug("Prediction has only one dimension, i.e. score for active")
+                auroc  = metrics.roc_score(yt, yp)[0]
+                aupr   = metrics.pr_score(yt, yp)[0]
+                bedroc = metrics.bedroc_score(yt, yp)[0]
+            else:
+                assert shape[1] == 2, "Too many classes. For the moment, TargetMate only works with active/inactive (1/0)"
+                assert set(yt) == set([0,1]), "Only 1/0 is accepted for the moment."
+                if len(shape) == 2:
+                    self.__log.debug("Prediction has two dimensions (active/inactive). Not ensemble.")
+                    auroc  = np.zeros(2)
+                    aupr   = np.zeros(2)
+                    bedroc = np.zeros(2)
+                    self.__log.debug("Measuring performance for inactives")
+                    yt_ = self.truth_inv(yt)
+                    auroc[0]  = metrics.roc_score(yt_, yp[:,0])[0]
+                    aupr[0]   = metrics.pr_score(yt_, yp[:,0])[0]
+                    bedroc[0] = metrics.bedroc_score(yt_, yp[:,0])[0]
+                    self.__log.debug("Measuring performances for actives")
+                    yt_ = yt
+                    auroc[1]  = metrics.roc_score(yt_, yp[:,1])[0]
+                    aupr[1]   = metrics.pr_score(yt_, yp[:,1])[0]
+                    bedroc[1] = metrics.bedroc_score(yt_, yp[:,1])[0]
+                if len(shape) == 3:
+                    self.__log.debug("Prediction has three dimensions (active/inactives and ensemble of datasets)")
+                    ens_size = shape[2]
+                    auroc  = np.zeros((2, ens_size))
+                    aupr   = np.zeros((2, ens_size))
+                    bedroc = np.zeros((2, ens_size))
+                    self.__log.debug("Measuring performance for inactives")
+                    yt_ = self.truth_inv(yt)
+                    for k in range(0, ens_size):
+                        auroc[0, k]  = metrics.roc_score(yt_, yp[:,0,k])[0]
+                        aupr[0, k]   = metrics.pr_score(yt_, yp[:,0,k])[0]
+                        bedroc[0, k] = metrics.bedroc_score(yt_, yp[:,0,k])[0]
+                    self.__log.debug("Measuring performance for inactives")
+                    yt_ = yt
+                    for k in range(0, ens_size):
+                        auroc[1, k]  = metrics.roc_score(yt_, yp[:,1,k])[0]
+                        aupr[1, k]   = metrics.pr_score(yt_, yp[:,1,k])[0]
+                        bedroc[1, k] = metrics.bedroc_score(yt_, yp[:,1,k])[0]
+        self.auroc  += [auroc]
+        self.bedroc += [bedroc]
+        self.aupr   += [aupr]
 
     def regressor_performances(self, yt, yp):
-        """ """
+        """TO-DO"""
         pass
 
     def compute(self, yt, yp):
@@ -110,6 +120,64 @@ class Performances:
                 "x": "x"
             }
         return perfs_dict
+
+
+@logged
+class Gatherer:
+    """Gather data"""
+
+    def __init__(self, is_ensemble):
+        self.is_ensemble  = is_ensemble
+        self.train_idx    = []
+        self.test_idx     = []
+        self.y_true_train = []
+        self.y_true_test  = []
+        self.y_pred_train = []
+        self.y_pred_test  = []
+        if self.is_ensemble:
+            self.y_pred_ens_train = []
+            self.y_pred_ens_test  = []
+
+    def gather(self, tm, data, splits):
+        self.__log.info("Gathering data (splits, samples, outcomes[, datasets])")
+        self.__log.debug("Iterating over splits")
+        i = 0
+        for tr_idx, te_idx in splits:
+            tm.repath_predictions_by_fold_and_set(fold_number=i, is_train=True, is_tmp=True, reset=True)
+            pred_train = tm.load_predictions()
+            tm.repath_predictions_by_fold_and_set(fold_number=i, is_train=False, is_tmp=True, reset=True)
+            pred_test  = tm.load_predictions()
+            data_train = data[tr_idx]
+            data_test  = data[te_idx]
+            self.train_idx += [tr_idx]
+            self.test_idx  += [te_idx]
+            self.y_true_train += [data_train.activity]
+            self.y_true_test  += [data_test.activity]
+            self.y_pred_train += [pred_train.y_pred]
+            self.y_pred_test  += [pred_test.y_pred]
+            if self.is_ensemble:
+                self.y_pred_ens_train += [pred_train.y_pred_ens]
+                self.y_pred_ens_test  += [pred_test.y_pred_ens]
+            i += 1
+
+
+@logged
+class Scorer:
+    """Run performances"""
+
+    def __init__(self, is_classifier, is_ensemble):
+        self.is_classifier = is_classifier
+        self.is_ensemble = is_ensemble
+
+    def score(self, g):
+        self.perfs_train = Performances(self.is_classifier).compute(g.y_true_train, g.y_pred_train)
+        self.perfs_test  = Performances(self.is_classifier).compute(g.y_true_test,  g.y_pred_test )
+        if self.is_ensemble:
+            self.perfs_ens_train = Performances(self.is_classifier).compute(g.y_true_train, g.y_pred_ens_train)
+            self.perfs_ens_test  = Performances(self.is_classifier).compute(g.y_true_test,  g.y_pred_ens_test)
+        else:
+            self.perfs_ens_train = None
+            self.perfs_ens_test  = None
 
 
 @logged
@@ -139,35 +207,14 @@ class Validation(HPCUtils):
         self.test_size = test_size
         self.destination_dir = destination_dir
         
-    @staticmethod
-    def stack(ar_a, ar_b):
-        if ar_a is None:
-            return ar_b
-        else:
-            if len(ar_b.shape) == 1:
-                return np.hstack((ar_a, ar_b))
-            else:
-                return np.vstack((ar_a, ar_b))
-
     def compute(self, tm, data, train_idx, test_idx):
         """Do the cross-validation"""
         # Initialize
+        self.tmp_path = tm.tmp_path
         self.is_classifier = tm.is_classifier
         self.is_ensemble = tm.is_ensemble
         self.conformity = tm.conformity
         self.weights = tm.weights
-        self.smiles = data.smiles
-        # Setup
-        self.__log.debug("Initializing all the results arrays")
-        self.train_idx = None
-        self.test_idx = None
-        self.y_true_train = None
-        self.y_true_test = None
-        self.y_pred_train = None
-        self.y_pred_test = None
-        if self.is_ensemble:
-            self.y_pred_ens_train = None
-            self.y_pred_ens_test = None
         # Signaturize
         self.__log.info("Signaturizing all data")
         tm.signaturize(data.smiles, is_tmp=True)
@@ -181,7 +228,7 @@ class Validation(HPCUtils):
                     if tm.is_classifier:
                         kf = model_selection.StratifiedKFold(n_splits=self.n_splits, shuffle=True)
                     else:
-                        self.__log.error("CV FOR REGRESSIO NOT YET DONE")
+                        self.__log.error("CV FOR REGRESSION NOT YET DONE")
                         # TO-DO
                     #kf = tm.kfolder()
                 else:
@@ -228,66 +275,64 @@ class Validation(HPCUtils):
             jobs += tm.predict(data, idxs=test_idx, is_tmp=True, wait=False)
             i += 1
         self.waiter(jobs)
+        self.n_splits = i
         # Gather data
-        self.__log.info("Gathering data")
-        i = 0
-        for train_idx, test_idx in splits:
-            tm.repath_predictions_by_fold_and_set(fold_number=i, is_train=True, is_tmp=True, reset=True)
-            pred_train = tm.load_predictions()
-            tm.repath_predictions_by_fold_and_set(fold_number=i, is_train=False, is_tmp=True, reset=True)
-            pred_test  = tm.load_predictions()
-            data_train = data[train_idx]
-            data_test  = data[test_idx]
-            self.train_idx = self.stack(self.train_idx, train_idx)
-            self.test_idx  = self.stack(self.test_idx,  test_idx )
-            self.y_true_train = self.stack(self.y_true_train, data_train.activity)
-            self.y_true_test  = self.stack(self.y_true_test, data_test.activity)
-            self.y_pred_train = self.stack(self.y_pred_train, pred_train.y_pred)
-            self.y_pred_test  = self.stack(self.y_pred_test, pred_test.y_pred)
-            if self.is_ensemble:
-                self.y_pred_ens_train = self.stack(self.y_pred_ens_train, pred_train.y_pred_ens)
-                self.y_pred_ens_test  = self.stack(self.y_pred_ens_test, pred_test.y_pred_ens)
-            i += 1
+        gather = Gatherer(is_ensemble = self.is_ensemble)
+        gather.gather(tm, data, splits)
+        self.gather_path = os.path.join(self.tmp_path, "validation-gather.pkl")
+        with open(self.gather_path, "wb") as f:
+            pickle.dump(gather, f)
 
     def score(self):
-        self.perfs_train = Performances(self.is_classifier).compute(self.y_true_train, self.y_pred_train)
-        self.perfs_test  = Performances(self.is_classifier).compute(self.y_true_test,  self.y_pred_test )
-        if self.is_ensemble:
-            self.perfs_ens_train = Performances(self.is_classifier).compute(self.y_true_train, self.y_pred_ens_train)
-            self.perfs_ens_test  = Performances(self.is_classifier).compute(self.y_true_test,  self.y_pred_ens_test )
-
+        self.__log.info("Loading gathered predictions")
+        with open(self.gather_path, "rb") as f:
+            gather = pickle.load(f)
+        scores = Scorer(is_classifier = self.is_classifier, is_ensemble = self.is_ensemble)
+        scores.score(gather)
+        self.scores_path = os.path.join(self.tmp_path, "validation_scores.pkl")
+        with open(self.scores_path, "wb") as f:
+             pickle.dump(scores, f)
+        
     def as_dict(self):
+        self.__log.debug("Reading gatherer")
+        with open(self.gather_path, "rb") as f:
+            gather = pickle.load(f)
+        self.__log.debug("Reading scores")
+        with open(self.scores_path, "rb") as f:
+            scores = pickle.load(f)
+        self.__log.info("Converting to dictionary")
         valid = {
+            "n_splits": self.n_splits,
+            "dim_dict": ("splits", "molecules", "outcomes", "ensemble"),
             "is_classifier": self.is_classifier,
             "is_ensemble": self.is_ensemble,
             "conformity": self.conformity,
             "datasets": self.datasets,
             "weights": self.weights,
-            "smiles": self.smiles,
             "destination_dir": self.destination_dir,
             "train": {
-                    "idx"   : self.train_idx,
-                    "y_true": self.y_true_train,
-                    "y_pred": self.y_pred_train,
-                    "perfs" : self.perfs_train.as_dict()
+                    "idx"   : gather.train_idx,
+                    "y_true": gather.y_true_train,
+                    "y_pred": gather.y_pred_train,
+                    "perfs" : scores.perfs_train.as_dict()
                 },
             "test": {
-                    "idx"   : self.test_idx,
-                    "y_true": self.y_true_test,
-                    "y_pred": self.y_pred_test,
-                    "perfs" : self.perfs_test.as_dict()
+                    "idx"   : gather.test_idx,
+                    "y_true": gather.y_true_test,
+                    "y_pred": gather.y_pred_test,
+                    "perfs" : scores.perfs_test.as_dict()
             }
         }
         if self.is_ensemble:
             valid["ens_train"] = {
-                "y_true": self.y_true_train,
-                "y_pred": self.y_pred_ens_train,
-                "perfs" : self.perfs_ens_train.as_dict()
+                "y_true": gather.y_true_train,
+                "y_pred": gather.y_pred_ens_train,
+                "perfs" : scores.perfs_ens_train.as_dict()
             }
             valid["ens_test"]  = {
-                "y_true": self.y_true_test,
-                "y_pred": self.y_pred_ens_test,
-                "perfs" : self.perfs_ens_test.as_dict()
+                "y_true": gather.y_true_test,
+                "y_pred": gather.y_pred_ens_test,
+                "perfs" : scores.perfs_ens_test.as_dict()
             }
         return valid
     
