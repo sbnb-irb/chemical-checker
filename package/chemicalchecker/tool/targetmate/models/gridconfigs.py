ef9c36ee89f4cf8cec125abd2c036c9ddb2de7b7
mduran <miquel.duran@irbbarcelona.org>
2019-11-26 10:16:46 +0100

    gridconfigs done

--- package/chemicalchecker/tool/targetmate/models/gridconfigs.py
+++ package/chemicalchecker/tool/targetmate/models/gridconfigs.py
@@ -4,9 +4,6 @@ from autosklearn.metrics import roc_auc
 import numpy as np
 from chemicalchecker.util import logged
 
-SEED = 42
-n_iter = 20
-
 search_configs = {
     
     "random_forest" : {
@@ -28,6 +25,8 @@ search_configs = {
 class GridClassifierConfigs:
 
     def __init__(self, base_mod, n_jobs, **kwargs):
+        self.SEED = 42
+        self.n_iter = 3
         self.base_mod = base_mod
         self.n_jobs = n_jobs
 
@@ -37,16 +36,16 @@ class GridClassifierConfigs:
         space = 1
         for k, v in params.items():
             space *= len(v)
-        n_iter = int(np.min([space, n_iter]))
+        n_iter = int(np.min([space, self.n_iter]))
         mod = RandomizedSearchCV(estimator = base_mod(),
                                  param_distributions = params,
-                                 n_iter = n_iter,
+                                 n_iter = self.n_iter,
                                  n_jobs = self.n_jobs,
                                  iid = False,
                                  cv = 5,
                                  verbose = 2,
                                  scoring = "roc_auc",
-                                 random_state = SEED
+                                 random_state = self.SEED
                                  )        
         mod.fit(X, y)
         self.__log.info("Best score: %.3f" % mod.best_score_)
