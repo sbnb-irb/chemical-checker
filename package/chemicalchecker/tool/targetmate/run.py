e1422edb00affe8b6250edf764c6174169332b13
mduran <miquel.duran@irbbarcelona.org>
2019-12-23 17:36:41 +0100

    wip targetmate

--- package/chemicalchecker/tool/targetmate/run.py
+++ package/chemicalchecker/tool/targetmate/run.py
@@ -1,8 +1,11 @@
+from chemicalchecker.util import logged
+
 from .evaluation import Validation
 from .utils.tasks import Tasker
+from .multi import MultiSetup
 
-
-class MultiValidation(object):
+@logged
+class MultiValidation(MultiSetup):
     """Helper class to create multiple validations."""
     def __init__(self,
                  data,
@@ -16,10 +19,13 @@ class MultiValidation(object):
             datasets(list or list of lists): Datasets to be used.
             **kwargs: Arguments of the Validation class.
         """
+        self.__log.info("Initialize multiple validation")
+        MultiSetup.__init__(self, data=data, models_path=models_path, **kwargs)
         self.validation = Validation(**kwargs)
-        self.tasks = Tasker(data, models_path)
 
     def run(self, TargetMate, **kwargs):
+        self.precalc_signatures(**kwargs)
+        self.__log.info("Multiple trainings")
         tm_list   = []
         data_list = []
         for data, models_path, _ in self.tasks:
@@ -29,9 +35,9 @@ class MultiValidation(object):
         self.validation.validate(tm=tm_list, data=data_list)
 
 
-class MultiFit(object):
+class MultiFit(MultiSetup):
     pass
 
 
-class MultiPredict(object):
+class MultiPredict(MultiSetup):
     pass
