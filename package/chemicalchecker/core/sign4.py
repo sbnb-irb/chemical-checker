bugfix: nan masking must be applyied after subsampling

package/chemicalchecker/core/sign4.py
@@ -1506,20 +1506,20 @@ def epoch_per_iteration_heuristic(samples, features, clip=(16, 1024)):
     return np.clip(pow2, *clip)
 
 
-def subsample(tensor, p_original=0.05, p_one_dataset=0.5,
+def subsample(tensor, p_original=0.05, p_one_dataset=0.0,
               one_dataset=None):
     """Function to subsample stacked data."""
     # it is safe to make a local copy of the input matrix
     new_data = np.copy(tensor)
     # we will have a masking matrix at the end
     mask = np.zeros_like(new_data).astype(bool)
+    if p_one_dataset > 0.0 and one_dataset is None:
+        raise Exception('Please specify dataset to keep')
     for idx, row in enumerate(new_data):
         # the following assume the stacked signature to have a fixed width
         presence = ~np.isnan(row[0::128])
         # drop everything except the predifined dataset
         if np.random.rand() < p_one_dataset:
-            if one_dataset is None:
-                raise Exception('Please specify dataset to keep')
             presence_add = one_dataset
         else:
             # low probability of keeping the original sample

package/chemicalchecker/util/splitter/pairstraintest.py
@@ -369,10 +369,10 @@ class PairTraintest(object):
 
     @staticmethod
     def generator_fn(file_name, split, batch_size=None, only_x=False,
-                     replace_nan=None,
+                     replace_nan=None, augment_scale=10,
                      augmentation_fn=None, augmentation_kwargs={}):
         """Return the generator function that we can query for batches."""
-        reader = PairTraintest(file_name, split, replace_nan=replace_nan)
+        reader = PairTraintest(file_name, split)
         reader.open()
         # read shapes
         x_shape = reader._f[reader.x_name].shape
@@ -403,6 +403,9 @@ class PairTraintest(object):
                     if augmentation_fn is not None:
                         x1 = augmentation_fn(x1, **augmentation_kwargs)
                         x2 = augmentation_fn(x2, **augmentation_kwargs)
+                    if replace_nan is not None:
+                        x1[np.where(np.isnan(x1))] = replace_nan
+                        x2[np.where(np.isnan(x2))] = replace_nan
                     yield x1, x2
                 else:
                     pairs, y = reader.get_py(beg_idx, end_idx)
@@ -411,6 +414,9 @@ class PairTraintest(object):
                     if augmentation_fn is not None:
                         x1 = augmentation_fn(x1, **augmentation_kwargs)
                         x2 = augmentation_fn(x2, **augmentation_kwargs)
+                    if replace_nan is not None:
+                        x1[np.where(np.isnan(x1))] = replace_nan
+                        x2[np.where(np.isnan(x2))] = replace_nan
                     yield [x1, x2], y
                 beg_idx, end_idx = beg_idx + batch_size, end_idx + batch_size
 