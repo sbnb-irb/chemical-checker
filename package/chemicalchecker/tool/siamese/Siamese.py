0e4cf2aa1002f377f20fe82b75c81fce04bbbaf0
PauBadiaM <paubadiamompel@gmail.com>
2020-01-20 14:56:34 +0100

    Lr is not 0 now

--- package/chemicalchecker/tool/siamese/Siamese.py
+++ package/chemicalchecker/tool/siamese/Siamese.py
@@ -6,7 +6,7 @@ import random
 import os
 
 from keras.datasets import mnist
-from keras.models import Model
+from keras.models import Model, load_model
 from keras.layers import Input, Flatten, Dense, Dropout, Lambda
 from keras.optimizers import RMSprop
 
@@ -37,10 +37,11 @@ class Siamese(object):
             epochs(int): The number of epochs (default: 200)
         """
 
-        self.epochs = int(kwargs.get("epochs", 15))
-        self.batch_size = int(kwargs.get("batch_size", 128))
-        self.learning_rate = int(kwargs.get("learning_rate", 0.001))
-        self.replace_nan = int(kwargs.get("replace_nan", 0))
+        self.epochs = int(kwargs.get("epochs", 5))
+        self.batch_size = int(kwargs.get("batch_size", 100))
+        self.learning_rate = float(kwargs.get("learning_rate", 0.001))
+        self.replace_nan = float(kwargs.get("replace_nan", 0.0))
+        self.augment_scale = int(kwargs.get("augment_scale", 1))
 
         self.models_path = os.path.abspath(models_path)
         if not os.path.exists(models_path):
@@ -54,50 +55,56 @@ class Siamese(object):
         self.transformer = None
 
 
-    def create_base_network(self, input_shape):
-        '''Create network architecture'''
-        input = Input(shape=input_shape)
-        x = Dense(3200, activation='relu')(input) # 1024
-        x = Dropout(0.1)(x)
-        x = Dense(1024, activation='relu')(x) # 1024
-        x = Dropout(0.1)(x)
-        x = Dense(512, activation='relu')(x) # 512
-        x = Dropout(0.1)(x)
-        x = Dense(128, activation='relu')(x)
-        return Model(input, x)
+    def fit(self, data_path, tr_shapes, tr_gen, val_shapes, val_gen):
+        def euclidean_distance(vects):
+            x, y = vects
+            sum_square = K.sum(K.square(x - y), axis=1, keepdims=True)
+            return K.sqrt(K.maximum(sum_square, K.epsilon()))
 
 
-      
-    def euclidean_distance(self, vects):
-        x, y = vects
-        sum_square = K.sum(K.square(x - y), axis=1, keepdims=True)
-        return K.sqrt(K.maximum(sum_square, K.epsilon()))
+        def eucl_dist_output_shape(shapes):
+            shape1, shape2 = shapes
+            return (shape1[0], 1)
 
 
-    def eucl_dist_output_shape(self, shapes):
-        shape1, shape2 = shapes
-        return (shape1[0], 1)
+        def contrastive_loss(y_true, y_pred):
+            '''Contrastive loss from Hadsell-et-al.'06
+            http://yann.lecun.com/exdb/publis/pdf/hadsell-chopra-lecun-06.pdf
+            '''
+            margin = 1
+            square_pred = K.square(y_pred)
+            margin_square = K.square(K.maximum(margin - y_pred, 0))
+            return K.mean(y_true * square_pred + (1 - y_true) * margin_square)
 
 
-    def contrastive_loss(self, y_true, y_pred):
-        '''Contrastive loss from Hadsell-et-al.'06
-        http://yann.lecun.com/exdb/publis/pdf/hadsell-chopra-lecun-06.pdf
-        '''
-        margin = 1
-        square_pred = K.square(y_pred)
-        margin_square = K.square(K.maximum(margin - y_pred, 0))
-        return K.mean(y_true * square_pred + (1 - y_true) * margin_square)
+        def create_base_network(input_shape):
+            '''Create network architecture'''
+            input = Input(shape=input_shape)
+            #x = Dense(3200, activation='relu')(input) # 1024
+            #x = Dropout(0.1)(x)
+            x = Dense(1024, activation='relu')(input) # 1024
+            x = Dropout(0.1)(x)
+            x = Dense(512, activation='relu')(x) # 512
+            x = Dropout(0.1)(x)
+            x = Dense(128, activation='relu')(x)
+            return Model(input, x)
 
 
-    def accuracy(self, y_true, y_pred):
-        '''Compute classification accuracy with a fixed threshold on distances.
-        '''
-        return K.mean(K.equal(y_true, K.cast(y_pred < 0.5, y_true.dtype)))
+        def accuracy(y_true, y_pred):
+            '''Compute classification accuracy with a fixed threshold on distances.
+            '''
+            return K.mean(K.equal(y_true, K.cast(y_pred < 0.5, y_true.dtype)))
 
+        #shapes, dtypes, gen = PairTraintest.generator_fn(data_path, 'train_train', 
+        #            batch_size=50, replace_nan=self.replace_nan, augmentation_fn=subsample, 
+        #            augmentation_kwargs=dict(p_dataset=0.5, dataset=[6]), augment_scale=2)
 
-    def build_siamese(self, input_shape):
+        #shapes_val, dtypes_val, gen_val = PairTraintest.generator_fn(data_path, 'train_test', 
+        #            batch_size=100, replace_nan=self.replace_nan)
 
-        base_network = self.create_base_network(input_shape)
+        input_shape = (tr_shapes[0][1],)
+
+        base_network = create_base_network(input_shape)
 
         input_a = Input(shape=input_shape)
         input_b = Input(shape=input_shape)
@@ -105,52 +112,56 @@ class Siamese(object):
         processed_a = base_network(input_a)
         processed_b = base_network(input_b)
 
-        distance = Lambda(self.euclidean_distance,
-                  output_shape=self.eucl_dist_output_shape)([processed_a, processed_b])
+        distance = Lambda(euclidean_distance,
+                          output_shape=eucl_dist_output_shape)([processed_a, processed_b])
 
-        siamese = Model([input_a, input_b], distance)
+        model = Model([input_a, input_b], distance)
 
-        self.__log.debug("Num of layers: %d" % len(siamese.layers))
-        return siamese
+        model.summary()
 
+        rms = RMSprop()
+        model.compile(loss=contrastive_loss, optimizer=rms, metrics=[accuracy])
+        history = model.fit_generator(generator=tr_gen(),
+                  steps_per_epoch=np.ceil(tr_shapes[0][0] * self.augment_scale / self.batch_size),
+                  epochs=self.epochs,
+                  validation_data=val_gen(),
+                  validation_steps=np.ceil(val_shapes[0][0] / self.batch_size))
 
-
-    def fit(self, data_path, use_geterator=True):
-
-        shapes, dtypes, gen = PairTraintest.generator_fn(data_path, 'train_train', batch_size=self.batch_size, replace_nan=self.replace_nan, augmentation_fn=subsample, augmentation_kwargs=dict(one_dataset=[False]*5 + [True] + [False]*19))
-
-        self.input_shape = (shapes[0][1],)
-        
-        self.siamese = self.build_siamese(self.input_shape)
-        self.siamese.summary()
-
-        rms = RMSprop(learning_rate=self.learning_rate)
-        self.siamese.compile(loss=self.contrastive_loss, optimizer=rms, metrics=[self.accuracy])
-
-        history = self.siamese.fit_generator(generator=gen(), epochs=self.epochs, steps_per_epoch=np.ceil(shapes[0][0] / self.batch_size))
-
-        self.siamese.save(self.siamese_model_file)
+        model.save(self.siamese_model_file)
 
         self._plot_history(history, os.path.join(self.models_path, "siamese_validation_plot.png"))
 
-    """
-    def compute_accuracy(generator):
-        y_pred = self.siamese.predict_generator(generator)
-        pred = y_pred.ravel() < 0.5
-        return np.mean(pred == y_true)
-
-
     def evaluate(self, data_path):
+        def compute_accuracy(generator, p_obj):
+            y_pred = self.siamese.predict_generator(generator, steps=np.ceil(shapes[0][0] / 100))
+            pred = y_pred.ravel() < 0.5
+            y_true = p_obj.get_all_y()
+            print("y_true", y_true.shape)
+            print("y_pred", y_pred.shape)
+            p_obj.close()
+            return np.mean(pred == y_true)
         
-        generator_tr_te = 0
-        generator_te_te = 0
+        self.siamese = load_model(self.siamese_model_file, compile=False)
 
-        acc_tr_te = compute_accuracy(generator_tr_te)
+        shapes, dtypes, gen = PairTraintest.generator_fn(data_path, 'train_test', 
+                    batch_size=100, replace_nan=self.replace_nan, augment_scale=1, only_x=True)
+
+
+        pair_object = PairTraintest(data_path, split="train_test")
+        pair_object.open()
+
+        acc_tr_te = compute_accuracy(gen(), pair_object)
         self.__log.debug("Accuracy Tr_Te: %f" % acc_tr_te)
 
-        acc_te_te = compute_accuracy(generator_te_te)
-        self.__log.debug("Accuracy Tr_Te: %f" % acc_tr_te)
-    """
+        shapes, dtypes, gen = PairTraintest.generator_fn(data_path, 'test_test', 
+                    batch_size=100, replace_nan=self.replace_nan, augment_scale=1, only_x=True)
+
+        pair_object = PairTraintest(data_path, split="test_test")
+        pair_object.open()
+
+        acc_te_te = compute_accuracy(gen(), pair_object)
+        self.__log.debug("Accuracy Te_Te: %f" % acc_te_te)
+    
 
     def predict(self, data_path, dest_file, chunk_size=1000, input_dataset='V'):
         """Take data .h5 and produce an encoded data.
@@ -162,7 +173,7 @@ class Siamese(object):
             dataset(string): The name of the dataset in the .h5 file to encode(default: 'V')
         """
 
-        self.siamese = load_model(self.autoencoder_model_file)
+        self.siamese = load_model(self.siamese_model_file)
         self.transformer = self.siamese.layers[2]
         
         with h5py.File(dest_file, "w") as results, h5py.File(data_path, 'r') as hf:
@@ -175,19 +186,29 @@ class Siamese(object):
 
             for i in range(0, input_size, chunk_size):
                 chunk = slice(i, i + chunk_size)
-
                 results['V'][chunk] = self.transformer.predict(hf[input_dataset][chunk])
 
 
     def _plot_history(self, history, destination=None):
         plt.figure(figsize=(8, 4), dpi=600)
-        plt.subplot(1, 2, 1)
-        plt.title('Loss evolution')
-        plt.plot(history.history["loss"], label="Train loss", lw=1)
-        plt.subplot(1, 2, 2)
-        plt.title('Accuracy evolution')
-        plt.plot(history.history["accuracy"], label="Test loss", lw=1)
+
+        plt.subplot(2, 2, 1)
+        plt.title('Train loss evolution')
+        plt.plot(history.history["loss"], label="Train loss", lw=1, color="red")
+
+        plt.subplot(2, 2, 2)
+        plt.title('Train accuracy evolution')
+        plt.plot(history.history["accuracy"], label="Train accuracy", lw=1, color="red")
         plt.ylim(0,1)
+
+        plt.subplot(2, 2, 3)
+        plt.title('Val loss evolution')
+        plt.plot(history.history["val_loss"], label="Val loss", lw=1, color="green")
+
+        plt.subplot(2, 2, 4)
+        plt.title('Val accuracy evolution')
+        plt.plot(history.history["val_accuracy"], label="Val accuracy", lw=1, color="green")
+        
         plt.legend(loc='best')
         if destination is not None:
             plt.savefig(destination)
