d29d02643ebc32f75bc15f13b0eaf11e388e0249
Oriol Guitart Pla <oriol.guitart@irbbarcelona.org>
2020-02-03 16:57:38 +0100

    Fix config path

--- pipelines/dream_ctd2_pipelines.py
+++ pipelines/dream_ctd2_pipelines.py
@@ -10,7 +10,7 @@ import h5py
 from chemicalchecker import ChemicalChecker
 from chemicalchecker.util.pipeline import Pipeline, PythonCallable, CCFit
 
-os.environ['CC_CONFIG'] = '/aloy/home/oguitart/pipelines/dream_ctd2_config.json'
+os.environ['CC_CONFIG'] = '/aloy/home/oguitart/projects/source/chemical_checker/pipelines/dream_ctd2_config.json'
 
 
 CC_PATH = "/aloy/web_checker/package_cc/dream_ctd2/"
