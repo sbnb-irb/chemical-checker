fb55794e75797ad350713602bc2cffc660766624
Oriol Guitart Pla <oriol.guitart@irbbarcelona.org>
2020-02-13 11:36:57 +0100

    Exclude some modules from the package

--- package/setup.py
+++ package/setup.py
@@ -58,11 +58,11 @@ setup(
     include_package_data=True,
     keywords='chemicalchecker',
     name='chemicalchecker',
-    packages=find_packages(),
+    packages=find_packages(exclude=['chemicalchecker.tool.hotnet*', 'chemicalchecker.tool.targetmate*']),
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
     url='http://gitlab.sbnb.org/project-specific-repositories/chemical_checker',
-    version='0.2.1',
+    version='0.2.2',
     zip_safe=False,
 )
