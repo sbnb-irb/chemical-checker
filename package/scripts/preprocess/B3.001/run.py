8a1f73a390fa0f5bc83b337578ab87d7b1adbc4f
Oriol Guitart <oguitart@pasteur.fr>
2020-03-13 16:18:17 +0100

    Change B3 preprocess

--- package/scripts/preprocess/B3.001/run.py
+++ package/scripts/preprocess/B3.001/run.py
@@ -8,6 +8,7 @@ import pickle
 import logging
 from chemicalchecker.util import logged
 from chemicalchecker.database import Dataset, Molrepo
+from chemicalchecker.core.preprocess import Preprocess
 
 
 # Variables
@@ -18,25 +19,6 @@ map_pdb_file = "pdb.pickl"
 entry_point_structures = "structures"
 entry_point_domains = "domains"
 entry_point_dm_hierch = "domain_hierarchies"
-
-
-def get_parser():
-    description = 'Run preprocess script.'
-    parser = argparse.ArgumentParser(description=description)
-    parser.add_argument('-i', '--input_file', type=str,
-                        required=False, default='.', help='Input file only for predict method')
-    parser.add_argument('-o', '--output_file', type=str,
-                        required=False, default='.', help='Output file')
-    parser.add_argument('-m', '--method', type=str,
-                        required=False, default='fit', help='Method: fit or predict')
-    parser.add_argument('-mp', '--models_path', type=str,
-                        required=False, default='', help='The models path')
-    parser.add_argument('-ep', '--entry_point', type=str,
-                        required=False, default=None, help='The predict entry point')
-    return parser
-
-
-# Parse arguments
 
 def parse_ecod(ecod_domains):
 
@@ -121,7 +103,7 @@ def parse_data_pdb(data, map_family_id, map_pdb):
 @logged(logging.getLogger("[ pre-process %s ]" % dataset_code))
 def main(args):
 
-    args = get_parser().parse_args(args)
+    args = Preprocess.get_parser().parse_args(args)
 
     dataset = Dataset.get(dataset_code)
 
@@ -201,33 +183,8 @@ def main(args):
                         continue
                     RAW[k] += [w]
 
-    keys = []
-    words = set()
-
-    for k in sorted(RAW.keys()):
-        keys.append(str(k))
-        words.update(RAW[k])
-
-    if features is not None:
-        orderwords = features_list
-    else:
-        orderwords = list(words)
-        orderwords.sort()
-    raws = np.zeros((len(keys), len(orderwords)), dtype=np.int8)
-    wordspos = {k: v for v, k in enumerate(orderwords)}
-
-    for i, k in enumerate(keys):
-        for word in RAW[k]:
-            raws[i][wordspos[word]] += 1
-
-    with h5py.File(args.output_file, "w") as hf:
-        hf.create_dataset("keys", data=np.array(keys))
-        hf.create_dataset("V", data=raws)
-        hf.create_dataset("features", data=np.array(orderwords))
-
-    if args.method == "fit":
-        with h5py.File(os.path.join(args.models_path, features_file), "w") as hf:
-            hf.create_dataset("features", data=np.array(orderwords))
+    Preprocess.save_output(args.output_file, RAW, args.method,
+                args.models_path, dataset.discrete, features)
 
 if __name__ == '__main__':
     main(sys.argv[1:])
