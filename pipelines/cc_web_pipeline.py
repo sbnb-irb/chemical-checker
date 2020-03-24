1496e3bf96b25e7fcc3f30322e8b5f8c291103cb
Oriol Guitart <oguitart@pasteur.fr>
2020-03-24 15:36:23 +0100

    New task libraries for cc web pipe

--- pipelines/cc_web_pipeline.py
+++ pipelines/cc_web_pipeline.py
@@ -10,7 +10,7 @@ from chemicalchecker.util import HPC
 from chemicalchecker.util import psql
 from chemicalchecker.core import DataSignature
 from chemicalchecker.database import Molrepo
-from chemicalchecker.util.pipeline import Pipeline, PythonCallable, Pubchem, ShowTargets
+from chemicalchecker.util.pipeline import Pipeline, PythonCallable, Pubchem, ShowTargets, Libraries
 from chemicalchecker.util.pipeline import Coordinates, Projections, Plots, MolecularInfo, SimilarsSign3
 
 os.environ['CC_CONFIG'] = '/aloy/home/oguitart/projects/source/chemical_checker/pipelines/cc_web_update.json'
@@ -18,14 +18,28 @@ os.environ['CC_CONFIG'] = '/aloy/home/oguitart/projects/source/chemical_checker/
 
 CC_PATH = "/aloy/web_checker/package_cc/newpipe/"
 MOLECULES_PATH = '/aloy/web_checker/molecules/'
-
 OLD_DB = 'mosaic'
 DB = 'cc_web_2019_05'
-
 uniprot_db_version = '2019_01'
 
-data_calculators = ['morgan_fp_r2_2048', 'e3fp_3conf_1024', 'murcko_1024_cframe_1024',
-                    'maccs_keys_166', 'general_physchem_properties', 'chembl_target_predictions_v23_10um']
+libraries = {"apd": "['Approved drugs','Approved drug molecules from DrugBank',\
+                'http://zinc15.docking.org/catalogs/dbap/items.txt?count=all&output_fields=smiles%20zinc_id','zinc']",
+             "exd": "['Experimental drugs','Experimental and investigational drug molecules from DrugBank',\
+             'http://zinc15.docking.org/catalogs/dbex/items.txt?count=all&output_fields=smiles%20zinc_id;http://zinc15.docking.org/catalogs/dbin/items.txt?count=all&output_fields=smiles%20zinc_id','zinc']",
+             "met": "['Human metabolites','Endogenous human metabolites from Human Metabolome Database (HMDb)',\
+             'http://zinc15.docking.org/catalogs/hmdbendo/items.txt?count=all&output_fields=smiles%20zinc_id','zinc']",
+             "tcm": "['Tradicional Chinese medicines','Compounds extracted from traditional Chinese medicinal plants',\
+             'http://zinc15.docking.org/catalogs/tcmnp/items.txt?count=all&output_fields=smiles%20zinc_id','zinc']",
+             "lincs": "['LINCS compounds','Collection of compounds of the LINCS initiative',\
+             'http://zinc15.docking.org/catalogs/lincs/items.txt?count=all&output_fields=smiles%20zinc_id','zinc']",
+             "pwck": "['Prestwick chemical library','Prestwick commercial collection',\
+             'http://zinc15.docking.org/catalogs/prestwick/items.txt?count=all&output_fields=smiles%20zinc_id','zinc']",
+             "nihcc": "['NIH clinical collection','NIH clinical collection',\
+             'http://zinc15.docking.org/catalogs/nihcc/items.txt?count=all&output_fields=smiles%20zinc_id','zinc']",
+             "ncidiv": "['NCI diversity collection','NCI diversity collection',\
+             'http://zinc15.docking.org/catalogs/ncidiv/items.txt?count=all&output_fields=smiles%20zinc_id','zinc']",
+             "tool": "['Tool compounds','Tool compounds','http://zinc15.docking.org/toolcompounds.smi?count=all','zinc']"
+             }
 
 
 pp = Pipeline(pipeline_path="/aloy/scratch/oguitart/web_cc")
@@ -120,5 +134,10 @@ pp.add_task(sim3_task)
 minfo_params = {'DB': DB, 'CC_ROOT': CC_PATH}
 minfo_task = MolecularInfo(name='molinfo', **minfo_params)
 pp.add_task(minfo_task)
+
+# TASK: Generate molecular info
+libs_params = {'DB': DB, 'CC_ROOT': CC_PATH, 'libraries': libraries}
+libs_task = Libraries(name='libraries', **libs_params)
+pp.add_task(libs_task)
 
 pp.run()
