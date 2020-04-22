9f99f1102cc11a1fde4bfec21ec804f01911f43c
PauBadiaM <paubadiamompel@gmail.com>
2020-04-22 08:39:11 +0200

    Modified __init___ for the new smiles predictor

--- package/chemicalchecker/util/splitter/__init__.py
+++ package/chemicalchecker/util/splitter/__init__.py
@@ -4,4 +4,5 @@ from .neighborpair import NeighborPairTraintest
 from .neighbortriplet import NeighborTripletTraintest
 from .neighborerror import NeighborErrorTraintest
 from .ae_siam_traintest import AE_SiameseTraintest
+from .smilestriplet import SmilesTripletTraintest
 
