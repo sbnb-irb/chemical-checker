3d2fcd889578ea70600e189c179e3172fe217853
sbnb-adm <sbnb_website@irbbarcelona.org>
2020-06-19 19:07:47 +0200

    Boolean correction for is_db column

--- package/chemicalchecker/database/datasource.py
+++ package/chemicalchecker/database/datasource.py
@@ -102,6 +102,7 @@ class Datasource(Base):
 
         # NS: the last column has to be changed to boolean values otherwise SQLalchmy passes strings
         df.calcdata=df.calcdata.apply(lambda x: False if x=='f' else True)        
+        df.is_db=df.is_db.apply(lambda x: False if x=='f' else True)
 
         # check columns
         needed_cols = Datasource._table_attributes()
