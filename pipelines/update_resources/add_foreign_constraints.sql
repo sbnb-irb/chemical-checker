106ea704b3b7a8360faf683a1f8a5a6c3a85771f
sbnb-adm <sbnb_website@irbbarcelona.org>
2020-07-01 16:08:10 +0200

    keep foregin key constraint when updating table 'datasource'

---
+++ pipelines/update_resources/add_foreign_constraints.sql
@@ -0,0 +1,15 @@
+/* When the table datasource is updated, make sure the foregin key constraints remain*/
+
+
+ALTER TABLE dataset_has_datasource
+ADD CONSTRAINT "dataset_has_datasource_datasource_name_fkey"
+FOREIGN KEY (datasource_name)
+REFERENCES datasource(datasource_name);
+
+
+ALTER TABLE molrepo_has_datasource
+ADD CONSTRAINT "molrepo_has_datasource_datasource_name_fkey"
+FOREIGN KEY (datasource_name)
+REFERENCES datasource(datasource_name);
+
+
