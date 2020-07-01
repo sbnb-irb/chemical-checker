1c36328c7af82a61c24e34cb27e8993aaf94d006
sbnb-adm <sbnb_website@irbbarcelona.org>
2020-07-01 15:32:40 +0200

    keep foregin key constraint when updating table 'datasource'

--- pipelines/create_datasource.py
+++ pipelines/create_datasource.py
@@ -1,11 +1,12 @@
 # Nico, 19/06/2020
 # Update the datasource table from cc_package database on aloy-dbsrv
-# Don't forget to remove the prvious datasource table first
+# Don't forget to remove the previous datasource table first
 
 from chemicalchecker.database import Datasource
 import os
 
 CSVfileIn="configs/current_resource_CSV"
+sql_file="configs/foreign_constraint.sql"
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 os.environ['CC_CONFIG'] = os.path.join(current_dir,'configs/cc_package.json')
@@ -23,14 +24,6 @@ Datasource._create_table()
 Datasource.from_csv(CSVfileIn)
 print("TABLE CREATED---->datasource")
 
-
-
-# start 45 download jobs (one per Datasource), job will wait until finished
-#job = Datasource.download_hpc('/aloy/scratch/sbnb-adm/tmp_job_download')
-# check if the downloads are really done
-#if not Datasource.test_all_downloaded():
-#    print("Something went WRONG while DOWNLOAD, should retry")
-    # print the faulty one
-#    for ds in Datasource.get():
-#        if not ds.available():
-#            print("Datasource %s not available" % ds)
+# Ensure the foreign key constraints remain as such (no password needed for sbnb-adm)
+command="psql -h aloy-dbsrv -d cc_package <"+sql_file
+os.system(command)
