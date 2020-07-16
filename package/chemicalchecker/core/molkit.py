0b24dbf876fb5b2121222e55d67de28e8cca0585
Martino Bertoni <martino.bertoni@irbbarcelona.org>
2020-07-16 17:36:14 +0200

    added key type dectection, made available at "core" level

--- package/chemicalchecker/core/molkit.py
+++ package/chemicalchecker/core/molkit.py
@@ -1,27 +1,37 @@
-"""A simple class to inpect small molecules
+"""A simple class to inspect small molecules, easily convert between different
+identifier, and ultimately find if signatures available.
 """
 import collections
-import h5py
-import os
+
 from chemicalchecker.util.parser.converter import Converter
+from chemicalchecker.util.keytype import KeyTypeDetector
 from chemicalchecker.util import logged
-from .signature_data import DataSignature
+
 
 @logged
 class Mol(object):
 
-    def __init__(self, cc, mol_str, str_type):
-        """Initialize molecule.
+    def __init__(self, cc, mol_str, str_type=None):
+        """Initialize a molecule.
 
         Args:
             cc: Chemical Checker instance.
             mol_str: Compound identifier (e.g. SMILES string)
-            str_type: Type of identifier ('inchikey', 'inchi' and 'smiles' are accepted).
+            str_type: Type of identifier ('inchikey', 'inchi' and 'smiles' are
+                accepted) if 'None' we do our best to guess.
         """
+        if str_type is None:
+            str_type = KeyTypeDetector.type(mol_str)
+            if str_type is None:
+                raise Exception(
+                    "Molecule '%s' not recognized as valid format"
+                    " (options are: 'inchikey', 'inchi' and 'smiles')" %
+                    mol_str)
         conv = Converter()
         if str_type == "inchikey":
             self.inchikey = mol_str
-            self.inchi = conv.inchikey_to_inchi(self.inchikey)[0]["standardinchi"]
+            self.inchi = conv.inchikey_to_inchi(
+                self.inchikey)[0]["standardinchi"]
         if str_type == "inchi":
             self.inchi = mol_str
             self.inchikey = conv.inchi_to_inchikey(self.inchi)
@@ -33,15 +43,17 @@ class Mol(object):
 
     def isin(self, cctype, dataset_code, molset="full"):
         """Check if the molecule is in the dataset of interest"""
-        sign_path = self.cc.get_signature_path(cctype, molset, dataset_code)
-        h5_file = [x for x in os.listdir(sign_path) if ".h5" in x][0]
-        sign_path = os.path.join(self.cc.get_signature_path(cctype, molset, dataset_code), h5_file)
-        sign = DataSignature(sign_path)
+        sign = self.cc.get_signature(cctype, molset, dataset_code)
         try:
             keys = set(sign.keys)
         except Exception:
             keys = set(sign.row_keys)
         return self.inchikey in keys
+
+    def signature(self, cctype, dataset_code, molset="full"):
+        """Check if the molecule is in the dataset of interest"""
+        sign = self.cc.get_signature(cctype, molset, dataset_code)
+        return sign[self.inchikey]
 
     def report_available(self, dataset_code="*", cctype="*", molset="full"):
         """Check in what datasets the key is present"""
@@ -54,7 +66,7 @@ class Mol(object):
                     if self.isin(cctype, dataset, molset):
                         d1[dataset] += [cctype]
             if d1:
-                d0[molset] = dict((k,v) for k,v in d1.items())
+                d0[molset] = dict((k, v) for k, v in d1.items())
         return d0
 
     def show(self):
