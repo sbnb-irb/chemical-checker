d4807a35c6027e3e40bf4bb27fb53bd8dcc743d8
Martino Bertoni <martino.bertoni@irbbarcelona.org>
2020-08-12 17:09:36 +0200

    some more docs

--- package/chemicalchecker/core/__init__.py
+++ package/chemicalchecker/core/__init__.py
@@ -1,3 +1,10 @@
+"""Main Chemical Checker functionalities are outlined here.
+
+In this subpackage we define the main classes that are normally used in a
+Chemical Checker project.
+For example the :class:`chemcheck.ChemicalChecker` or :class:`sign3` classes
+are defined in following files.
+"""
 from .data import DataFactory
 from .chemcheck import ChemicalChecker
 from .validation import Validation
