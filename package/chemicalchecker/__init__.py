231fa67ffc3d93a2dbe9ab27a2e2f993907b2c5b
Martino Bertoni <martino.bertoni@irbbarcelona.org>
2020-08-12 17:09:36 +0200

    some more docs

--- package/chemicalchecker/__init__.py
+++ package/chemicalchecker/__init__.py
@@ -1,14 +1,18 @@
 # -*- coding: utf-8 -*-
 
-"""Welcome to the Chemical Checker Package documentation.
+"""*Welcome to the Chemical Checker Package documentation!*
+
+The chemical checker package aims to provide an interface to use all chemical
+checker tools. From the generation of new data (signatures, etc) to accessing
+already produced data.
 
  Organization
-	The package is organized with four subpackages:
+    The package is organized in four subpackages:
 
-		* :mod:`package.core`
-		* :mod:`package.database`
-		* :mod:`package.tool`
-		* :mod:`package.util`
+        * :mod:`package.core`
+        * :mod:`package.database`
+        * :mod:`package.tool`
+        * :mod:`package.util`
 
 """
 
