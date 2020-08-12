36d508a23698466f20c05c205c90de2c012f58e0
Martino Bertoni <martino.bertoni@irbbarcelona.org>
2020-08-12 12:12:59 +0200

    some improved doc

--- package/chemicalchecker/__init__.py
+++ package/chemicalchecker/__init__.py
@@ -1,6 +1,16 @@
 # -*- coding: utf-8 -*-
 
-"""Top-level package for chemicalchecker."""
+"""Welcome to the Chemical Checker Package documentation.
+
+ Organization
+	The package is organized with four subpackages:
+
+		* :mod:`package.core`
+		* :mod:`package.database`
+		* :mod:`package.tool`
+		* :mod:`package.util`
+
+"""
 
 from .core import ChemicalChecker
 
