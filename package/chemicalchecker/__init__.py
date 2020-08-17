d803d7259708c4de56d63b1b3623b2cf1e0ff9f3
Martino Bertoni <martino.bertoni@irbbarcelona.org>
2020-08-17 11:21:55 +0200

    more docs, fixed imports

--- package/chemicalchecker/__init__.py
+++ package/chemicalchecker/__init__.py
@@ -1,6 +1,4 @@
-# -*- coding: utf-8 -*-
-
-"""*Welcome to the Chemical Checker Package documentation!*
+"""**Welcome to the Chemical Checker Package documentation!**
 
 The chemical checker package aims to provide an interface to use all chemical
 checker tools. From the generation of new data (signatures, etc) to accessing
@@ -9,11 +7,18 @@ already produced data.
  Organization
     The package is organized in four subpackages:
 
-        * :mod:`package.core`
-        * :mod:`package.database`
-        * :mod:`package.tool`
-        * :mod:`package.util`
+        * :mod:`package.core`: The ``core`` module holds the main Chemical
+          Checker functionalities. It holds the ChemicalChecekr entry-point,
+          the Signature definitions and ``HDF5`` implementation.
 
+        * :mod:`package.database`: The ``database`` module include DB access
+          and table definitions.
+
+        * :mod:`package.tool`: The ``tool`` modules gather experimental code
+          and wrappers to external software.
+
+        * :mod:`package.util`: The ``util`` module pull together general
+          utilities.
 """
 
 from .core import ChemicalChecker
