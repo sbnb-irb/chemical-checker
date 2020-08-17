f09d2405bdedfbc3f6a092d1b4f1bf15c508fe43
Martino Bertoni <martino.bertoni@irbbarcelona.org>
2020-08-17 17:25:13 +0200

    removed generalprops from unittest

--- package/tests/test_database.py
+++ package/tests/test_database.py
@@ -1,7 +1,6 @@
 import os
 import unittest
 from chemicalchecker.util import Config
-from chemicalchecker.database import GeneralProp
 from chemicalchecker.database import Dataset
 from chemicalchecker.database import Molecule
 
@@ -16,8 +15,6 @@ class TestDatabase(unittest.TestCase):
         os.environ["CC_CONFIG"] = os.path.join(
             self.data_dir, 'database_config.json')
         self.cfg = Config()
-        self.GeneralProp = GeneralProp
-        GeneralProp._create_table()
         self.Dataset = Dataset
         Dataset._create_table()
         self.Molecule = Molecule
@@ -29,28 +26,12 @@ class TestDatabase(unittest.TestCase):
 
     def test_add_bulk(self):
 
-        res = self.GeneralProp.get('test2')
-        self.assertIsNone(res)
-
-        self.GeneralProp.add_bulk([
-            ["test1", 11, 21, 31, 41, 51, 61, 71, 81, 91, 101, 111, 121, 131,
-             141, 151, 161, 171, "wqwwq"],
-            ["test2", 1, 22, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
-             17, "wqqw"]], on_conflict_do_nothing=False)
-
-        res = self.GeneralProp.get('test1')
-        self.assertTrue(hasattr(res, 'mw'))
-        self.assertTrue(res.heavy == 21)
-
-        res = self.GeneralProp.get('test2')
-        self.assertTrue(hasattr(res, 'mw'))
-        self.assertTrue(res.heavy == 22)
-
         res = self.Molecule.get('test2')
         self.assertIsNone(res)
 
         self.Molecule.add_bulk([["test1", "ttt"],
-                                ["test2", "zzz"]], on_conflict_do_nothing=False)
+                                ["test2", "zzz"]],
+                               on_conflict_do_nothing=False)
 
         res = self.Molecule.get('test1')
         self.assertTrue(hasattr(res, 'inchi'))
@@ -65,7 +46,8 @@ class TestDatabase(unittest.TestCase):
         res = self.Dataset.get('test2')
         self.assertIsNone(res)
 
-        self.Dataset.add({"dataset_code": "A1.001", "level": "A", "unknowns": True})
+        self.Dataset.add({"dataset_code": "A1.001",
+                          "level": "A", "unknowns": True})
 
         res = self.Dataset.get('A1.001')
         self.assertTrue(hasattr(res, 'level'))
