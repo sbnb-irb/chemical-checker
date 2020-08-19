478f7eadba2bccce77bca46484cbafea78eecf6d
Martino Bertoni <martino.bertoni@irbbarcelona.org>
2020-08-19 12:58:37 +0200

    cleaned package root

--- package/setup.py
+++ package/setup.py
@@ -5,12 +5,6 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
-    readme = readme_file.read()
-
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
-
 requirements = [
     'numpy',
     'h5py',
@@ -24,7 +18,9 @@ requirements = [
     'sklearn',
     'csvsort',
     'matplotlib<3.0',
-    'seaborn'
+    'seaborn',
+    'tqdm',
+    'apache-airflow'
 ]
 
 setup_requirements = ['pytest-runner']
@@ -41,7 +37,7 @@ setup(
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'License :: MIT License',
         'Natural Language :: English',
         "Programming Language :: Python :: 2",
         'Programming Language :: Python :: 2.7',
@@ -51,14 +47,26 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ],
-    description="Python Package with everything needed to create and query the Chemical Checker.",
+    description="Chemical Checker Package.",
     install_requires=requirements,
     license="GNU General Public License v3",
-    long_description=readme + '\n\n' + history,
+    long_description='''The Chemical Checker (CC) is a data-driven resource of small molecule
+bioactivity data. The main goal of the CC is to express data in a format
+that can be used off-the-shelf in daily computational drug discovery
+tasks. The resource is organized in **5 levels** of increasing
+complexity, ranging from the chemical properties of the compounds to
+their clinical outcomes. In between, we consider targets, off-targets,
+perturbed biological networks and several cell-based assays, including
+gene expression, growth inhibition, and morphological profiles. The CC
+is different to other integrative compounds database in almost every
+aspect. The classical, relational representation of the data is
+surpassed here by a less explicit, more machine-learning-friendly
+abstraction of the data''',
     include_package_data=True,
     keywords='chemicalchecker',
     name='chemicalchecker',
-    packages=find_packages(exclude=['chemicalchecker.tool.hotnet*', 'chemicalchecker.tool.targetmate*']),
+    packages=find_packages(
+        exclude=['chemicalchecker.tool.hotnet*', 'chemicalchecker.tool.targetmate*']),
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
