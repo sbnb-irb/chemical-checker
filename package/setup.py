6f915fc495cce8ddff0da7e676f3decd43669c43
Martino Bertoni <martino.bertoni@irbbarcelona.org>
2020-08-20 15:29:44 +0200

    cleaned setup.py

--- package/setup.py
+++ package/setup.py
@@ -1,9 +1,5 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-"""The setup script."""
-
 from setuptools import setup, find_packages
+import chemicalchecker
 
 requirements = [
     'numpy',
@@ -30,26 +26,12 @@ test_requirements = [
     'mock'
 ]
 
-
 setup(
-    author="SBNB",
-    author_email='sbnb@irbbarcelona.org',
-    classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: MIT License',
-        'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-    ],
+    name='chemicalchecker',
+    version=chemicalchecker.__version__,
+    author=chemicalchecker.__author__,
+    author_email=chemicalchecker.__email__,
     description="Chemical Checker Package.",
-    install_requires=requirements,
-    license="GNU General Public License v3",
     long_description='''The Chemical Checker (CC) is a data-driven resource of small molecule
 bioactivity data. The main goal of the CC is to express data in a format
 that can be used off-the-shelf in daily computational drug discovery
@@ -62,15 +44,26 @@ is different to other integrative compounds database in almost every
 aspect. The classical, relational representation of the data is
 surpassed here by a less explicit, more machine-learning-friendly
 abstraction of the data''',
-    include_package_data=True,
-    keywords='chemicalchecker',
-    name='chemicalchecker',
-    packages=find_packages(
-        exclude=['chemicalchecker.tool.hotnet*', 'chemicalchecker.tool.targetmate*']),
-    setup_requires=setup_requirements,
-    test_suite='tests',
-    tests_require=test_requirements,
     url='http://gitlab.sbnb.org/packages/chemical_checker',
-    version='0.2.2',
+    packages=find_packages(),
+    install_requires=requirements,
+    setup_requires=setup_requirements,
+    tests_require=test_requirements,
+    test_suite='tests',
     zip_safe=False,
+    include_package_data=True,
+    license="MIT License",
+    keywords='chemicalchecker bioactivity signatures chemoinformatics',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: MIT License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ]
 )
