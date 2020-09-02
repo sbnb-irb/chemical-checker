d6d7f246d71994fad293e492c65c2d6862e1c5f0
nsoler <nicolas.soler@irbbarcelona.org>
2020-09-02 17:45:23 +0200

    fix y_b'str' in traintest.py

--- package/chemicalchecker/tool/adanet/adanet_wrap.py
+++ package/chemicalchecker/tool/adanet/adanet_wrap.py
@@ -62,9 +62,7 @@ class AdaNetWrapper(object):
             x_ds = 'x'
             y_ds = 'y'
 
-            decoded_keys=[k.decode() if type(k) is bytes else k for k in hf.keys()]  # NS convert the bytes into strings
-            print("SHERLOCK BEFORE--->", list(hf.keys()))
-            print("SHERLOCK AFTER--->", decoded_keys)
+            #decoded_keys=[k.decode() if type(k) is bytes else k for k in hf.keys()]  # NS convert the bytes into strings
             if 'x_train' in decoded_keys:
                 x_ds = 'x_train'
                 y_ds = 'y_train'
