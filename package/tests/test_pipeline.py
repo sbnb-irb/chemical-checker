c98b08aefce7e14bee31f0b4ff07e3d653d682b2
Martino Bertoni <martino.bertoni@irbbarcelona.org>
2020-09-10 18:33:17 +0200

    initial REAL pipeline unittest

--- package/tests/test_pipeline.py
+++ package/tests/test_pipeline.py
@@ -1,5 +1,4 @@
 import os
-import shutil
 import pytest
 import unittest
 import functools
@@ -23,43 +22,67 @@ class TestPipeline(unittest.TestCase):
         test_dir = os.path.dirname(os.path.realpath(__file__))
         #test_dir = '/aloy/home/oguitart/temps'
         self.data_dir = os.path.join(test_dir, 'data')
-        self.pp_dir = os.path.join(self.data_dir, 'package_test_predict')
+        self.pipeline_dir = os.path.join(self.data_dir, 'pipeline_test')
+        if os.path.isdir(self.pipeline_dir):
+            os.system("chmod -R 777 " + self.pipeline_dir)
+            os.system("rm -rf " + self.pipeline_dir)
         os.environ["CC_CONFIG"] = os.path.join(
             self.data_dir, 'config.json')
 
     def tearDown(self):
-        if os.path.isdir(self.pp_dir):
-            os.system("chmod -R 777 " + self.pp_dir)
-            os.system("rm -rf " + self.pp_dir)
-            # shutil.rmtree(self.pp_dir)
+        if os.path.isdir(self.pipeline_dir):
+            os.system("chmod -R 777 " + self.pipeline_dir)
+            os.system("rm -rf " + self.pipeline_dir)
+            # shutil.rmtree(self.pipeline_dir)
 
     @skip_if_import_exception
-    def test_pipeline(self):
-        from chemicalchecker.util.pipeline import Pipeline, CCPredict
+    def test_pipeline_fit_sign0(self):
+        from chemicalchecker.util.pipeline import Pipeline, CCPredict, CCFit
 
-        CC_ROOT = '/aloy/web_checker/current'
+        pipeline_dir = self.pipeline_dir
+        data_file = os.path.join(self.data_dir, 'E1_preprocess.h5')
+        CC_ROOT = os.path.join(pipeline_dir, 'cc')
 
-        self.pp = Pipeline(pipeline_path=self.pp_dir)
-        self.assertTrue(os.path.isdir(self.pp.readydir))
-        output_path = self.pp_dir
-        s0_params = {"output_path": output_path, 'CC_ROOT': CC_ROOT, "datasets": ["E4.001"],
-                     "ds_params": {"E4.001": {'key_type': 'inchikey', 'data_file': '/aloy/scratch/oguitart/tmp/entry_profile.tsv'}}
-                     }
+        pp = Pipeline(pipeline_path=pipeline_dir)
+        self.assertTrue(os.path.isdir(pp.readydir))
 
+        s0_params = {
+            "output_path": pipeline_dir,
+            'CC_ROOT': CC_ROOT,
+            'cc_old_path': '/aloy/web_checker/package_cc/paper',
+            "datasets": ["E1.001"],
+            "ds_params": {
+                "E1.001": {
+                    'key_type': 'inchikey',
+                    'data_file': data_file,
+                    'do_triplets': False,
+                    'validations': False
+                }
+            }
+        }
+
+        s0_task = CCFit(cc_type='sign0', **s0_params)
+        pp.add_task(s0_task)
+        pp.run()
+
+        sign0_file = os.path.join(CC_ROOT,'full/E/E1/E1.001/sign0/sign0.h5')
+        self.assertTrue(os.path.isfile(sign0_file))
+
+        """
         s0_pred_task = CCPredict(cc_type='sign0', **s0_params)
-        self.pp.add_task(s0_pred_task)
+        pp.add_task(s0_pred_task)
 
-        s1_params = {"output_path": output_path, 'CC_ROOT': CC_ROOT,
-                     "datasets": ["E4.001"]}
+        s1_params = {"output_path": pipeline_dir, 'CC_ROOT': CC_ROOT,
+                     "datasets": ["E1.001"]}
 
         s1_pred_task = CCPredict(cc_type='sign1', **s1_params)
-        self.pp.add_task(s1_pred_task)
+        pp.add_task(s1_pred_task)
 
-        s2_params = {"output_path": output_path, 'CC_ROOT': CC_ROOT,
-                     "datasets": ["E4.001"]}
+        s2_params = {"output_path": pipeline_dir, 'CC_ROOT': CC_ROOT,
+                     "datasets": ["E1.001"]}
 
         s2_pred_task = CCPredict(cc_type='sign2', **s2_params)
-        self.pp.add_task(s2_pred_task)
+        pp.add_task(s2_pred_task)
 
         s0_pred_task.mark_ready()
         self.assertTrue(s0_pred_task.is_ready())
@@ -69,9 +92,8 @@ class TestPipeline(unittest.TestCase):
 
         s2_pred_task.mark_ready()
         self.assertTrue(s2_pred_task.is_ready())
-
+        """
         # This needs to be uncommented when the signatures predict work
-        # self.pp.run()
 
-        s2_pred_task.clean()
-        self.assertFalse(s2_pred_task.is_ready())
+        #s2_pred_task.clean()
+        #self.assertFalse(s2_pred_task.is_ready())
