597ec028eecd3862de915f97eb6ae94c25f2b623
nsoler <nsoler@irbbarcelona.org>
2020-09-16 16:32:37 +0200

    added the filter_universe script

---
+++ package/scripts/filter_universe/restrict_universe.py
@@ -0,0 +1,28 @@
+import itertool
+import numpy as np
+from chemicalchecker import ChemicalChecker
+
+# NS filter the A spaces sign0 to keep only molecules present in the B spaces and after (bioactive molecules)
+cc_repo="/aloy/web_checker/package_cc/2020_01/"
+cc= ChemicalChecker(cc_repo)
+
+# Get the union of molecules for exemplary B spaces and above 
+universe = cc.universe
+spaces_to_filter = ['A1.001', 'A2.001', 'A3.001', 'A4.001', 'A5.001', 'B4.002', 'D2.002']
+
+for space in spaces_to_filter:
+
+    s0 = cc.get_signature('sign0', 'full', space)
+
+    # get the vectors corresponding to our (restricted) universe
+    inchk_univ, _ = s0.get_vectors(keys=universe)
+
+    # obtain a mask for sign0 in order to obtain a filtered h5 file
+    mask= np.isin(s0.keys, list(inchk_univ))
+
+    filtered_h5=os.path.join(os.path.dirname(s0.data_path), 'sign0_univ.h5')
+
+
+    s0.make_filtered_copy(filtered_h5, mask)
+
+    # After that check that your file is ok and move it to sign0.h5
