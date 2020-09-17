c84f47042929ae8ca71984a4a0aed6b032aa291d
Martino Bertoni <martino.bertoni@irbbarcelona.org>
2020-09-17 17:56:46 +0200

    fix import of plot utils

--- package/chemicalchecker/util/plot/__init__.py
+++ package/chemicalchecker/util/plot/__init__.py
@@ -3,3 +3,4 @@ from .plot import Plot
 from .multiplot import MultiPlot
 from .multiccplot import MultiCCPlot
 from .diagnosticsplot import DiagnosisPlot
+from .util import coord_color, set_style, rgb2hex, lighten_color, cc_colors
