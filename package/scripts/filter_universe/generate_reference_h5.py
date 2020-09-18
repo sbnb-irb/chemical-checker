22f27ded31dc094196e27be93a9e59ec96102bed
nsoler <nsoler@irbbarcelona.org>
2020-09-18 21:59:44 +0200

    add the generate_reference script after filtering thesign0 universe

--- package/scripts/filter_universe/generate_reference_h5.py
+++ package/scripts/filter_universe/generate_reference_h5.py
@@ -1,5 +1,11 @@
 # Re-generating the reference datasets after shrinking the universe of A spaces
+from chemicalchecker import ChemicalChecker
 from chemicalchecker.util.remove_near_duplicates import RNDuplicates
+import os, h5py
+
+os.environ['CC_CONFIG'] = "/opt/chemical_checker/setup/users/cc_config_nico.json"
+
+cc= ChemicalChecker("/aloy/web_checker/package_cc/2020_01/")
 
 Aspaces = ('A1.001','A2.001', 'A3.001', 'A4.001', 'A5.001', 'B4.002')
 for sp in Aspaces:
