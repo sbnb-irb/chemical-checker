02bc41ba625085af4423f589206674e0c5f2894a
nsoler <nsoler@irbbarcelona.org>
2020-09-23 11:52:52 +0200

    Added a check to the fit() method for sign0,1,2,3

--- package/chemicalchecker/core/signature_base.py
+++ package/chemicalchecker/core/signature_base.py
@@ -98,7 +98,7 @@ class BaseSignature(object):
             BaseSignature.__log.warning("Model already available.")
 
         if self.is_fit():
-            raise Exception("The fit has already been done.\nPlease remove manually the folder {} and re-run the method\.n(or use overwrite=True)".format(self.signature_path))            
+            raise Exception("The fit has already been done.\nPlease remove manually the folder {} and re-run the method.\n(or use overwrite=True)".format(self.signature_path))            
 
         # if os.path.exists(os.path.join(self.model_path, self.readyfile)):
         #     os.remove(os.path.join(self.model_path, self.readyfile))
