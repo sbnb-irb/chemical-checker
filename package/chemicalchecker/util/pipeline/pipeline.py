b76d484a69959fc0a81ae6309c7a067956f5d000
Martino Bertoni <martino.bertoni@irbbarcelona.org>
2020-11-03 17:03:03 +0100

    added logic for including or excluding tasks by name

--- package/chemicalchecker/util/pipeline/pipeline.py
+++ package/chemicalchecker/util/pipeline/pipeline.py
@@ -12,7 +12,8 @@ class Pipeline():
     Run pipelines according to config files.
     """
 
-    def __init__(self, pipeline_path, config, keep_jobs=False):
+    def __init__(self, pipeline_path, config, keep_jobs=False,
+                 only_tasks=[], exclude_tasks=[]):
         """Initialize a Pipeline instance.
 
         pipeline_path (str): Path where the pipeline will set its structure.
@@ -21,8 +22,10 @@ class Pipeline():
         """
         self.tasks = []
         self.pipeline_path = pipeline_path
-        Pipeline.__log.info("PIPELINE PATH: {}".format(self.pipeline_path))
+        self.__log.info("PIPELINE PATH: {}".format(self.pipeline_path))
         self.config = config
+        self.only_tasks = only_tasks
+        self.exclude_tasks = exclude_tasks
 
         self.readydir = os.path.join(self.pipeline_path, "ready")
         self.logdir = os.path.join(self.pipeline_path, "log")
@@ -52,48 +55,58 @@ class Pipeline():
         fh.setFormatter(formatter)
         logger.addHandler(fh)
 
+    def _check_task(self, task):
+        """Decide if task can be included."""
+        if any([task.name.startswith(st) for st in self.exclude_tasks]):
+            self.__log.info("Task EXCLUDED: '%s'", task.name)
+            return False
+        if len(self.only_tasks) > 0:
+            if any([task.name.startswith(st) for st in self.only_tasks]):
+                self.__log.info("Task SELECTED: '%s'", task.name)
+                return True
+            else:
+                self.__log.info("Task EXCLUDED: '%s'", task.name)
+                return False
+        self.__log.info("Task INCLUDED: '%s'", task.name)
+        return True
+
     def add_task(self, task):
         """Add tasks to the pipeline."""
         task.set_dirs(self.readydir, self.tmpdir, self.cachedir)
         task.keep_jobs = self.keep_jobs
         task.config = self.config
-        self.tasks.append(task)
+        if self._check_task(task):
+            self.tasks.append(task)
 
     def insert_task(self, position, task):
         """Add tasks to the pipeline."""
         task.set_dirs(self.readydir, self.tmpdir, self.cachedir)
         task.keep_jobs = self.keep_jobs
         task.config = self.config
-        self.tasks.insert(position, task)
+        if self._check_task(task):
+            self.tasks.insert(position, task)
 
-    def run(self, include_tasks=None):
+    def run(self):
         """Run the pipeline."""
-        if include_tasks is None:
-            include_tasks = [t.name for t in self.tasks]
         for task in self.tasks:
-            # check if among those we want to tun
-            if task.name not in include_tasks:
-                Pipeline.__log.info(
-                    "Task: '%s'. Skipping...", task.name)
-                continue
             # check if already done
             if task.is_ready():
-                Pipeline.__log.info(
+                self.__log.info(
                     "Task: '%s' already done. Skipping...", task.name)
                 continue
             # run it
-            Pipeline.__log.info(
+            self.__log.info(
                 "Starting task: '%s' ...", task.name)
             task.run()
             # after runnin we expect the step to be in ready state
             if not task.is_ready():
                 # if not we report the error
-                Pipeline.__log.error(
+                self.__log.error(
                     "Pipeline failed in task %s. Please, check errors.",
                     task.name)
                 break
             else:
-                Pipeline.__log.info(
+                self.__log.info(
                     "Done task: '%s' ...", task.name)
 
     def clean(self, step=None):
