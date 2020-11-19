3881c8a83fedf39956b4e0d1f444589ac8099cb9
nsoler <nicolas.soler@irbbarcelona.org>
2020-11-19 13:23:45 +0100

    get_global_signature method in cc class

--- package/chemicalchecker/core/chemcheck.py
+++ package/chemicalchecker/core/chemcheck.py
@@ -686,10 +686,11 @@ class ChemicalChecker():
             spaces =[''.join(t) for t in itertools.product('A B C D E'.split(),'1 2 3 4 5'.split(), ['.001'])]
             try:
                 global_sign= np.concatenate([mol.signature('sign3', sp) for sp in spaces],axis=0)
+                return global_sign
             except Exception as e2:
                 self.__log.warning("Problem with generating global signature from "+mol_str)
                 self.__log.warning(e2)
-            return global_sign
+                return None
         else:
             return None
 
