055fe2f787165f6cb968e9c64aaf70912c57fd33
Martino Bertoni <martino.bertoni@irbbarcelona.org>
2020-11-25 11:42:17 +0100

    bugfix, still bytes

--- package/chemicalchecker/core/signature_data.py
+++ package/chemicalchecker/core/signature_data.py
@@ -626,7 +626,14 @@ class DataSignature(object):
         if "mappings" not in self.info_h5:
             raise Exception("Data file has no mappings.")
         with h5py.File(self.data_path, 'r') as hf:
-            mappings = dict(hf['mappings'][:])
+            mappings_raw = hf['mappings'][:]
+        mappings = dict()
+        for row in mappings_raw:
+            if isinstance(row[0], bytes):
+                ink1 = row[0].decode()
+            if isinstance(row[1], bytes):
+                ink2 = row[1].decode()
+            mappings[ink1] = ink2
         # avoid trivial mappings (where key==value)
         to_map = list(set(mappings.keys()) - set(mappings.values()))
         if len(to_map) == 0:
@@ -640,10 +647,6 @@ class DataSignature(object):
                 hf.create_dataset('V', data=src_vectors, dtype=np.float32)
                 hf.create_dataset("shape", data=src_vectors.shape)
             return
-        # convert to string if bytes
-        if isinstance(to_map[0], bytes):
-            for idx, byt in enumerate(to_map):
-                to_map[idx] = byt.decode()
         # prepare key-vector arrays
         dst_keys = list()
         dst_vectors = list()
