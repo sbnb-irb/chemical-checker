bf53d9dec562f2fe6b0bfa347cad7c54f00aecc8
Martino Bertoni <martino.bertoni@irbbarcelona.org>
2020-12-17 17:03:15 +0100

    added incremental pca, better logic without faulty predictor

--- package/chemicalchecker/core/projector/umap_.py
+++ package/chemicalchecker/core/projector/umap_.py
@@ -1,16 +1,17 @@
 import os
 import h5py
-import joblib
 import datetime
 import numpy as np
 from tqdm import tqdm
 from time import time
+from sklearn.decomposition import IncrementalPCA
+from sklearn.linear_model import LinearRegression
 
 from chemicalchecker.core.signature_base import BaseSignature
 from chemicalchecker.core.signature_data import DataSignature
 
-from chemicalchecker.util.plot import Plot
 from chemicalchecker.util import logged
+from chemicalchecker.util.plot import Plot
 
 
 @logged
@@ -24,11 +25,6 @@ class UMAP(BaseSignature, DataSignature):
             signature_path(str): the path to the signature directory.
             dataset(object): The dataset object with all info related.
         """
-        try:
-            import umap
-        except ImportError:
-            raise ImportError("requires umap " +
-                              "https://umap-learn.readthedocs.io/en/latest/")
         # Calling init on the base class to trigger file existance checks
         BaseSignature.__init__(
             self, signature_path, dataset, **params)
@@ -50,31 +46,69 @@ class UMAP(BaseSignature, DataSignature):
         DataSignature.__init__(self, self.data_path)
         self.__log.debug('data_path: %s', self.data_path)
         self.name = "_".join([str(self.dataset), "proj", self.proj_name])
-        # get default parameters
-        self.params = dict(metric='cosine', init='random')
-        self.params.update(params)
-        # if already fitted load the model and projetions
-        self.algo_path = os.path.join(self.model_path, 'algo.pkl')
-        self.algo = umap.UMAP(n_components=2, **self.params)
+        self.oos_mdl_path = os.path.join(self.model_path, 'oos.pkl')
 
-    def fit(self, signature, validations=True, chunk_size=100):
+    def fit(self, signature, validations=True, chunk_size=5000,
+            oos_predictor=False, proj_params={}, pre_pca=True):
         """Fit to signature data."""
+        try:
+            import umap
+        except ImportError:
+            raise ImportError("requires umap " +
+                              "https://umap-learn.readthedocs.io/en/latest/")
         # perform fit
+        projector = umap.UMAP(n_components=2, **proj_params)
         self.__log.info("Projecting with %s..." % self.__class__.__name__)
+        for k, v in proj_params.items():
+            self.__log.info('  %s %s', k, v)
         self.__log.info("Input shape: %s" % str(signature.info_h5['V']))
         t_start = time()
-        with h5py.File(signature.data_path, "r") as src:
-            proj_data = self.algo.fit_transform(src["V"][:])
+        # pre PCA
+        if pre_pca:
+            # find n_components to get 0.9 explained variance
+            ipca = IncrementalPCA(n_components=signature.shape[1])
+            with h5py.File(signature.data_path, "r") as src:
+                src_len = src["V"].shape[0]
+                for i in tqdm(range(0, src_len, chunk_size), 'fit expl_var'):
+                    chunk = slice(i, i + chunk_size)
+                    ipca.partial_fit(src["V"][chunk])
+            nr_comp = np.argmax(ipca.explained_variance_ratio_.cumsum() > 0.9)
+            # fit pca
+            ipca = IncrementalPCA(n_components=nr_comp)
+            with h5py.File(signature.data_path, "r") as src:
+                src_len = src["V"].shape[0]
+                for i in tqdm(range(0, src_len, chunk_size), 'fit'):
+                    chunk = slice(i, i + chunk_size)
+                    ipca.partial_fit(src["V"][chunk])
+            # transform
+            proj_data = list()
+            with h5py.File(signature.data_path, "r") as src:
+                src_len = src["V"].shape[0]
+                for i in tqdm(range(0, src_len, chunk_size), 'transform'):
+                    chunk = slice(i, i + chunk_size)
+                    proj_data.append(ipca.transform(src["V"][chunk]))
+            data = np.vstack(proj_data)
+        else:
+            # read data
+            with h5py.File(signature.data_path, "r") as src:
+                data = src["V"][:]
+        # do projection
+        self.__log.info("Final input shape: %s" % str(data.shape))
+        proj_data = projector.fit_transform(data)
+        if oos_predictor:
+            # tsne does not predict so we train linear model
+            mdl = LinearRegression()
+            mdl.fit(data, proj_data)
+            pickle.dump(mdl, open(self.oos_mdl_path, 'wb'))
         t_end = time()
         t_delta = datetime.timedelta(seconds=t_end - t_start)
         self.__log.info("Projecting took %s" % t_delta)
-        # save model
-        joblib.dump(self.algo, open(self.algo_path, 'wb'))
         # save h5
         sdtype = DataSignature.string_dtype()
         with h5py.File(signature.data_path, "r") as src, \
                 h5py.File(self.data_path, "w") as dst:
-            dst.create_dataset("keys", data=src['keys'].asstr()[:], dtype=sdtype)
+            dst.create_dataset(
+                "keys", data=src['keys'].asstr()[:], dtype=sdtype)
             dst.create_dataset("name", data=np.array([self.name], sdtype))
             date_str = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             dst.create_dataset("date", data=np.array([date_str], sdtype))
@@ -86,23 +120,25 @@ class UMAP(BaseSignature, DataSignature):
             for i in tqdm(range(0, src_len, chunk_size), 'write'):
                 chunk = slice(i, i + chunk_size)
                 dst['V'][chunk] = proj_data[chunk]
+        # make plot
+        plot = Plot(self.dataset, self.stats_path)
+        xlim, ylim = plot.projection_plot(proj_data, bw=0.1, levels=10)
         # run validation
         if validations:
             self.validate()
         self.mark_ready()
 
-    def predict(self, signature, destination, chunk_size=100, plot=False, plot_kws=None):
+    def predict(self, signature, destination, chunk_size=100):
         """Predict new projections."""
-        # load pickled projector
-        try:
-            self.algo = joblib.load(open(self.algo_path))
-        except Exception as ex:
-            self.__log.warning("Cannot load projector: %s" % str(ex))
+        if not os.path.isfile(self.oos_mdl_path):
+            raise Exception('Out-of-sample predictor was not trained.')
+        mdl = pickle.load(open(self.oos_mdl_path, 'rb'))
         # create destination file
         sdtype = DataSignature.string_dtype()
         with h5py.File(signature.data_path, "r") as src, \
                 h5py.File(destination, "w") as dst:
-            dst.create_dataset("keys", data=src['keys'].asstr()[:], dtype=sdtype)
+            dst.create_dataset(
+                "keys", data=src['keys'].asstr()[:], dtype=sdtype)
             dst.create_dataset("name", data=np.array([self.name], sdtype))
             date_str = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             dst.create_dataset("date", data=np.array([date_str], sdtype))
@@ -113,36 +149,4 @@ class UMAP(BaseSignature, DataSignature):
             dst.create_dataset("V", (src_len, 2), dtype=np.float32)
             for i in tqdm(range(0, src_len, chunk_size), 'transform'):
                 chunk = slice(i, i + chunk_size)
-                dst['V'][chunk] = self.algo.transform(src['V'][chunk])
-        prediction = DataSignature(destination)
-        if plot:
-            # load data in memory
-            proj_data = np.vstack(self[:], prediction[:])
-            # plot projection
-            range_x = max(abs(np.min(proj_data[:, 0])),
-                          abs(np.max(proj_data[:, 0])))
-            range_y = max(abs(np.min(proj_data[:, 1])),
-                          abs(np.max(proj_data[:, 1])))
-            range_max = max(range_y, range_x)
-            frame = range_max / 10.
-            range_max += frame
-            cmap = plot_kws.pop('cmap', 'viridis')
-            plot_path = plot_kws.pop('plot_path', './')
-            x_range = plot_kws.pop('x_range', (-range_max, range_max))
-            y_range = plot_kws.pop('y_range', (-range_max, range_max))
-            plot_size = plot_kws.pop('plot_size', (1000, 1000))
-            noise_scale = plot_kws.pop('noise_scale', None)
-            self.__log.info("Plot prediction range x: %s y: %s" % (x_range,
-                                                                   y_range))
-            if not os.path.isdir(plot_path):
-                os.mkdir(plot_path)
-            plot = Plot(self.dataset, plot_path)
-            plot.datashader_projection(
-                proj_data,
-                self.projector.__class__.__name__,
-                cmap=cmap,
-                x_range=x_range,
-                y_range=y_range,
-                plot_size=plot_size,
-                noise_scale=noise_scale,
-                **plot_kws)
+                dst['V'][chunk] = mdl.predict(src['V'][chunk])
