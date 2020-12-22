0f308fb89d5813865f4922629e85bb842f3b7cf5
Martino Bertoni <martino.bertoni@irbbarcelona.org>
2020-12-22 17:33:44 +0100

    simplified dependencies, removed redundant definition file

--- setup/setup_chemicalchecker.sh
+++ setup/setup_chemicalchecker.sh
@@ -108,7 +108,7 @@ then
     fi
     mkdir $INSTALL_DIR;
     cd $INSTALL_DIR;
-    SINGULARITY_DEFINITION="$LOCAL_REPO/container/singularity/cc-full.def"
+    SINGULARITY_DEFINITION="$LOCAL_REPO/container/singularity/cc_py37.def"
     BINARIES_FOLDER="$LOCAL_REPO/container/singularity/binaries/"
     cp $SINGULARITY_DEFINITION $INSTALL_DIR;  # Node2vec binary
     cp -r $BINARIES_FOLDER $INSTALL_DIR;    
@@ -118,7 +118,7 @@ then
     sudo rm -rf $LOCAL_IMAGE_SANDBOX;
 
     printf -- 'Creating singularity sandbox image... \n';
-    sudo singularity build --sandbox $LOCAL_IMAGE_SANDBOX cc-full.def;
+    sudo singularity build --sandbox $LOCAL_IMAGE_SANDBOX cc_py37.def;
     if [ $? -eq 0 ]; then
         printf -- '\033[32m SUCCESS: Image sandbox created correctly. \033[0m\n';
     else
