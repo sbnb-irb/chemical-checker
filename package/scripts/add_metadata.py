855a8cddb00b4fb8c5e3f88e7b61aba0b057a33f
nsoler <nicolas.soler@irbbarcelona.org>
2021-01-25 15:37:56 +0100

    script to export all signs at once

--- package/scripts/add_metadata.py
+++ package/scripts/add_metadata.py
@@ -13,7 +13,7 @@ def add_metadata(version="2020_01",signatures='0123', pathrepo="/aloy/web_checke
     """
     version: (str), version of the cc package
     signature: (str or int), number refering to the signature. ex: '012' for sign0, sign1, sign2.
-    path: (str), path to the cc signature repo
+    pathrepo: (str), path to the cc signature repo
     """
     signatures=str(signatures) # in case we have an int.
 
@@ -38,12 +38,81 @@ def add_metadata(version="2020_01",signatures='0123', pathrepo="/aloy/web_checke
                                 if k not in f.attrs:
                                     f.attrs.create(name=k,data=v)
                                 else:
-                                    print(k,"already in f.attrs")
+                                    print(k,"already in attrs")
 
                     else:
                         print(fichero, "doesn't exist, skipping")
 
+def export_sign(target_dir, version="2020_01", signatures='2', pathrepo="/aloy/web_checker/package_cc/",molsets=('full',),copy_backup=False, add_metadata=True):
+    """
+    Export all signatures from a given cctype (ex: sign2) in a single folder
+    Add Metadata to the output files if not present in the original h5 file
 
-if __name__ == '__main__':
+    target_dir (str): target directory where the signatures will be copied
+    version (str): version of the checker
+    signature: (str or int), number refering to the signature. ex: '012' for sign0, sign1, sign2.
+    pathrepo: (str), path to the cc signature repo
+    molsets: (list or tuple), either 'full' or 'reference'
+    copy_backup (Bool): copy signx_BACKUP.h5 instead of signx.h5 if present
+    add_metadata (Bool): Add metadata to the copied file
+    """
+
+    for sign in signatures:
+        cctype='sign'+signature
+        sign_dir= os.path.join(target_dir,cctype)
+
+        if not os.path.exists(sign_dir):
+            try:
+                os.makedirs(sign_dir)
+            except Exception as e:
+                print("WARNING", e)
+                continue
+
+       
+        for molset in molsets:
+            for space in "ABCDE":
+                for num in "12345":
+                    signature= 'sign'+sign
+                    data_code= space+num+'.001'
+
+                    if copy_backup:
+                        fichero= os.path.join(pathrepo,version,molset,space,space+num, data_code, signature, signature+'_BACKUP.h5')
+                    else:
+                        fichero= os.path.join(pathrepo,version,molset,space,space+num, data_code, signature, signature+'.h5')
+                        
+                    target_file  = os.path.join(sign_dir, cctype+'_'+space+num+'_'+molset+'.h5')
+
+                    if os.path.exists(fichero):
+                        if not os.path.exists(target_file):
+                            print("Copying",fichero,"to",target_file)
+                            shutil.copyfile(fichero,target_file)
+
+                            # Adding metadata
+                            if add_metadata:
+                                print("Adding metadata to", backup_file)
+                                dico= dict(cctype=signature, dataset_code=data_code, molset=molset)
+
+                                with h5py.File(target_file,'a') as f:
+                                    for k,v in dico.items():
+                                        if k not in f.attrs:
+                                            f.attrs.create(name=k,data=v)
+                                        else:
+                                            print(k,"already in attrs")
+                        else:
+                            print("WARNING: file",target_file,"already exists!")
+                            print("Please delete it first")
+                    else:
+                        print(fichero,"does not exist, skipping!")
+
+if __name__=='__main__':
+
     current_version="2020_01"
-    add_metadata(version=current_version)
+    target_directory= "/aloy/scratch/nsoler/CC_related/EXPORT_SIGN"
+
+    # Backup all h5 files and add metadata to the backups:
+    #add_metadata(version=current_version)
+
+    
+    export_sign(target_directory,version="2020_01", signatures='2',molsets=('full',), copy_backup=True)
+
+
