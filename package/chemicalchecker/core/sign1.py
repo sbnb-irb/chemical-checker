56356dfdc66338f020ce220283a9c232a5867c2f
nsoler <nicolas.soler@irbbarcelona.org>
2021-02-03 21:24:23 +0100

    trying to predict sign1

--- package/chemicalchecker/core/sign1.py
+++ package/chemicalchecker/core/sign1.py
@@ -120,6 +120,9 @@ class sign1(BaseSignature, DataSignature):
     def load_model(self, name):
         fn = os.path.join(self.get_molset(
             "reference").model_path, "%s.pkl" % name)
+        #debug
+        print("\nSHERLOCK fn",fn)
+
         with open(fn, "rb") as f:
             mod = pickle.load(f)
         return mod
@@ -236,56 +239,59 @@ class sign1(BaseSignature, DataSignature):
     def predict(self, sign0, destination=None):
         """Predict sign1 from sign0"""
 
-        if type(sign0) is not DataSignature:
-            raise Exception(
-                "Predict requires a DataSignature as input parameter")
+        # #if type(sign0) is not DataSignature:
+        # if type(sign0) != type(DataSignature):
+        #     print("type(sign0)", type(sign0))
+        #     print("type(DataSignature)",type(DataSignature))
+        #     raise Exception(
+        #         "Predict requires a DataSignature as input parameter")
         if not os.path.isfile(sign0.data_path):
             raise Exception("The file " + sign0.data_path + " does not exist")
         tag = str(uuid.uuid4())
         tmp_path = os.path.join(self.model_path, tag)
-        try:
-            cc = ChemicalChecker(tmp_path)
-            s1 = cc.signature(self.dataset, "sign1")
-            self.copy_sign0_to_sign1(sign0, s1, just_data=True)
-            self.__log.debug("Reading pipeline")
-            fn = self.pipeline_file()
-            with open(fn, "rb") as f:
-                pipeline = pickle.load(f)
-            self.__log.debug("Starting pipeline")
-            self.__log.debug("Scaling if necessary")
-            if not pipeline["sparse"] and pipeline["scale"]:
-                mod = self.load_model("scale")
-                mod.predict(s1)
-            self.__log.debug("Transformation")
-            if pipeline["metric_learning"]:
-                if pipeline["semisupervised"]:
-                    mod = self.load_model("semiml")
-                else:
-                    mod = self.load_model("unsupml")
+        #try:
+        cc = ChemicalChecker(tmp_path)
+        s1 = cc.signature(self.dataset, "sign1")
+        self.copy_sign0_to_sign1(sign0, s1, just_data=True)
+        self.__log.debug("Reading pipeline")
+        fn = self.pipeline_file()
+        with open(fn, "rb") as f:
+            pipeline = pickle.load(f)
+        self.__log.debug("Starting pipeline")
+        self.__log.debug("Scaling if necessary")
+        if not pipeline["sparse"] and pipeline["scale"]:
+            mod = self.load_model("scale")
+            mod.predict(s1)
+        self.__log.debug("Transformation")
+        if pipeline["metric_learning"]:
+            if pipeline["semisupervised"]:
+                mod = self.load_model("semiml")
             else:
-                if pipeline["latent"]:
-                    if pipeline["sparse"]:
-                        mod = self.load_model("lsi")
-                    else:
-                        mod = self.load_model("pca")
+                mod = self.load_model("unsupml")
+        else:
+            if pipeline["latent"]:
+                if pipeline["sparse"]:
+                    mod = self.load_model("lsi")
                 else:
-                    mod = None
-            if mod is not None:
-                mod.predict(s1)
-            self.__log.debug("Prediction done!")
-            if destination is None:
-                self.__log.debug("Returning a V, keys dictionary")
-                results = {
-                    "V": s1[:],
-                    "keys": s1.keys
-                }
+                    mod = self.load_model("pca")
             else:
-                self.__log.debug("Saving H5 file in %s" % destination)
-                shutil.copyfile(s1.data_path, destination)
-                results = None
-        except Exception as e:
-            shutil.rmtree(tmp_path)
-            raise Exception(e)
+                mod = None
+        if mod is not None:
+            mod.predict(s1)
+        self.__log.debug("Prediction done!")
+        if destination is None:
+            self.__log.debug("Returning a V, keys dictionary")
+            results = {
+                "V": s1[:],
+                "keys": s1.keys
+            }
+        else:
+            self.__log.debug("Saving H5 file in %s" % destination)
+            shutil.copyfile(s1.data_path, destination)
+            results = None
+        # except Exception as e:
+        #     shutil.rmtree(tmp_path)
+        #     raise Exception(e)
 
         self.__log.debug("Deleting tmp folder")
         shutil.rmtree(tmp_path)
