4b752d445046f060b9acd1222dc9b9fca5ee3a31
nsoler <nicocubi@gmail.com>
2021-02-11 14:42:37 +0100

    admin cc scripts

--- package/scripts/export_features_sign0.py
+++ package/scripts/export_features_sign0.py
@@ -6,7 +6,7 @@ from chemicalchecker.core.chemcheck import ChemicalChecker
 from get_repo_version import cc_repo_version
 #VERSION= "2020_02"
 
-def export_features_sign0(cc_repo=None, outDir="/aloy/scratch/nsoler/CC_related/EXPORT_SIGN/sign0"):
+def export_features_sign0(destination="/aloy/scratch/nsoler/CC_related/EXPORT_SIGN/sign0_features", cc_repo=None):
 
     if cc_repo is None:
     	cc_repo = cc_repo_version()
@@ -19,10 +19,23 @@ def export_features_sign0(cc_repo=None, outDir="/aloy/scratch/nsoler/CC_related/
 	    else:
 	    	print("Working with cc_repo:",cc_repo)
 
+	if not os.path.exists(destination):
+		try:
+			os.makedirs(destination)
+		except Exception as e:
+			print("ERROR while attempting to create destination folder", destination)
+			print(e)
+		else:
+			print("Created directory", destination)
+
 	cc = ChemicalChecker(cc_repo)
 
 	for space in "ABCDE":
 		for num in (1, 2, 3, 4, 5):
 			ds= space+str(num)+'.001'
 			sign0tmp = cc.get_signature('sign0', 'full', ds)
-			sign0tmp.export_features(outDir)
+			sign0tmp.export_features(destination)
+
+if __name__== '__main__':
+	destination="/aloy/scratch/nsoler/CC_related/EXPORT_SIGN/sign0_features"
+	export_features_sign0(destination=destination)
