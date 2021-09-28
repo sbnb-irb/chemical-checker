64f0f8e2617a061652980dbbb2ab77e902bf61df
Martino Bertoni <martino.bertoni@irbbarcelona.org>
2021-09-28 18:03:58 +0200

    feature should be decoded and passed as a list

--- package/scripts/preprocess/A1.001/run.py
+++ package/scripts/preprocess/A1.001/run.py
@@ -31,8 +31,9 @@ def main(args):
 
     args = Preprocess.get_parser().parse_args(args)
 
-    main._log.debug(
-        "Running preprocess for dataset " + dataset_code + ". Saving output in " + args.output_file)
+    main._log.info("Running preprocess for dataset " + dataset_code +
+                   ". Saving output in " + args.output_file)
+    main._log.debug("ARGS: %s" % args)
 
     if args.entry_point is None:
         args.entry_point = entry_point_keys
@@ -63,57 +64,47 @@ def main(args):
     if args.method == "predict":
 
         ACTS = []
-
         data = []
 
         with h5py.File(os.path.join(args.models_path, features_file)) as hf:
             features_list = hf["features"][:]
-            features = set(features_list)
+            features = list(set([f.decode() for f in features_list]))
+        main._log.debug("Features: %s" % len(features))
 
-        with open(args.input_file) as f:
-
-            for l in f:
-                items = l.rstrip().split("\t")
+        with open(args.input_file) as fh:
+            for line in fh:
+                items = line.rstrip().split("\t")
                 data.append(items)
 
+        # input is SMILES
         if args.entry_point == entry_point_smiles:
-
             inchikey_inchi = {}
-
             converter = Converter()
-
             for d in data:
-
                 try:
                     inchikey, inchi = converter.smiles_to_inchi(d[1])
                 except Exception:
                     continue
-
                 inchikey_inchi[d[0]] = inchi
 
+        # input is InChIKey InChI
         if args.entry_point == entry_point_inchi:
-
             inchikey_inchi = dict(data)
 
         if args.entry_point != entry_point_keys:
-
             parse_fn = DataCalculator.calc_fn(name)
-
             for chunk in parse_fn(inchikey_inchi, 1000):
-
                 for prop in chunk:
                     ACTS.append((prop["inchikey"], prop["raw"]))
-
         else:
-
             molprop = Calcdata(name)
             props = molprop.get_properties_from_list([i[0] for i in data])
             ACTS.extend(props)
 
-    main._log.info("Saving raws")
+    main._log.info("Saving raws: %s molecules" % len(ACTS))
 
     RAW = collections.defaultdict(list)
-    # NS: ACTS contains the dense format for raw data
+    # ACTS contains the dense format for raw data
     for k in ACTS:
         if features is None:
             vals = [str(t) for t in k[1].split(",")]
@@ -121,9 +112,10 @@ def main(args):
             vals = [str(t) for t in k[1].split(",") if str(t) in features]
         RAW[str(k[0])] = vals
 
-    # NS: Preprocess.save_output will then convert the dense format into binary data
+    # Preprocess.save_output will then convert the dense format into binary
+    main._log.debug(RAW)
     Preprocess.save_output(args.output_file, RAW, args.method,
-                args.models_path, True, features, features_int=True)
+                           args.models_path, True, features, features_int=True)
 
 
 if __name__ == '__main__':
