67aedf8594c1f50cff65d1b9f911e403ec4130d5
Martino Bertoni <martino.bertoni@irbbarcelona.org>
2021-09-30 16:14:26 +0200

    fixed all chemistry predict methods which had a problem with features, harmonized scripts

--- package/scripts/preprocess/A3.001/run.py
+++ package/scripts/preprocess/A3.001/run.py
@@ -1,13 +1,11 @@
 import os
 import sys
-import argparse
 import collections
 import h5py
-import numpy as np
 import logging
 
 from chemicalchecker.util import logged
-from chemicalchecker.database import Calcdata, Datasource
+from chemicalchecker.database import Calcdata
 from chemicalchecker.database import Molrepo
 from chemicalchecker.util.parser import DataCalculator
 from chemicalchecker.util.parser import Converter
@@ -29,8 +27,9 @@ def main(args):
 
     args = Preprocess.get_parser().parse_args(args)
 
-    main._log.debug(
-        "Running preprocess for dataset " + dataset_code + ". Saving output in " + args.output_file)
+    main._log.info("Running %s preprocess. Saving output in %s" %
+                   (dataset_code, args.output_file))
+    main._log.debug("ARGS: %s" % args)
 
     if args.entry_point is None:
         args.entry_point = entry_point_keys
@@ -40,18 +39,12 @@ def main(args):
     if args.method == "fit":
 
         molrepos = Molrepo.get_universe_molrepos()
-
         main._log.info("Querying molrepos")
-
         ACTS = []
         inchikeys = set()
-
         molprop = Calcdata(name)
-
         for molrepo in molrepos:
-
             molrepo = str(molrepo[0])
-
             inchikeys.update(Molrepo.get_fields_by_molrepo_name(
                 molrepo, ["inchikey"]))
         props = molprop.get_properties_from_list([i[0] for i in inchikeys])
@@ -60,56 +53,50 @@ def main(args):
     if args.method == "predict":
 
         ACTS = []
-
         data = []
-
         with h5py.File(os.path.join(args.models_path, features_file)) as hf:
             features_list = hf["features"][:]
-            features = set(features_list)
+            features = list([f.decode() for f in features_list])
+        main._log.debug("Loaded %s features" % len(features))
 
-        with open(args.input_file) as f:
-
-            for l in f:
-                items = l.rstrip().split("\t")
+        with open(args.input_file) as fh:
+            for line in fh:
+                items = line.rstrip().split("\t")
                 data.append(items)
+        main._log.debug("Predicting %s molecules" % len(data))
 
+        # input is SMILES
         if args.entry_point == entry_point_smiles:
-
             inchikey_inchi = {}
-
             converter = Converter()
-
             for d in data:
-
                 try:
                     inchikey, inchi = converter.smiles_to_inchi(d[1])
                 except Exception:
                     continue
-
                 inchikey_inchi[d[0]] = inchi
 
+        # input is InChIKey InChI
         if args.entry_point == entry_point_inchi:
-
             inchikey_inchi = dict(data)
 
         if args.entry_point != entry_point_keys:
-
             parse_fn = DataCalculator.calc_fn(name)
-
             for chunk in parse_fn(inchikey_inchi, 1000):
-
                 for prop in chunk:
                     ACTS.append((prop["inchikey"], prop["raw"]))
-
         else:
-
             molprop = Calcdata(name)
             props = molprop.get_properties_from_list([i[0] for i in data])
             ACTS.extend(props)
 
-    main._log.info("Saving raws")
+    main._log.info("Saving raws for %s molecules" % len(ACTS))
     RAW = collections.defaultdict(list)
+    # ACTS contains the dense format for raw data
     for k in ACTS:
+        if k[1] == '' or k[1] is None or k[0] is None:
+            main._log.warning('Could not calculate signature for %s' % k[0])
+            continue
         if features is None:
             vals = [str(t) for t in k[1].split(",")]
         else:
@@ -117,7 +104,8 @@ def main(args):
         RAW[str(k[0])] = vals
 
     Preprocess.save_output(args.output_file, RAW, args.method,
-                args.models_path, True, features)
+                           args.models_path, True, features)
+
 
 if __name__ == '__main__':
     main(sys.argv[1:])
