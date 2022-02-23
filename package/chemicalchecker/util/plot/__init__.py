231882891db4cb9788921339e521c0bbaa4ba78e
Martino Bertoni <martino.bertoni@irbbarcelona.org>
2022-02-23 09:52:58 +0100

    added class that generates plots for the CC stats web page

--- package/chemicalchecker/util/plot/__init__.py
+++ package/chemicalchecker/util/plot/__init__.py
@@ -3,4 +3,5 @@ from .plot import Plot
 from .multiplot import MultiPlot
 from .multiccplot import MultiCCPlot
 from .diagnosticsplot import DiagnosisPlot
+from .ccstatsplot import CCStatsPlot
 from .util import *
