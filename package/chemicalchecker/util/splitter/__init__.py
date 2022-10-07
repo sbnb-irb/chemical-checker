5e17f5adb7844582387c4116e791b0453cb5ef5d
Martino Bertoni <martino.bertoni@irbbarcelona.org>
2022-10-07 15:28:54 +0200

    major refactoring of triplet sampling: separating iterator from sampling, made the sampling modular so that we can extend it, first extension is Adria approach which significantly improved B2 sign3-sign0 recapitulation

--- package/chemicalchecker/util/splitter/__init__.py
+++ package/chemicalchecker/util/splitter/__init__.py
@@ -4,8 +4,8 @@ Classes in here allow creation and access train-test sets splits and expose
 the generator functions which tensorflow likes.
 """
 from .traintest import Traintest
-from .pairtraintest import PairTraintest
-from .neighborpair import NeighborPairTraintest
-from .neighbortriplet import NeighborTripletTraintest
-from .neighborerror import NeighborErrorTraintest
-from .ae_siam_traintest import AE_SiameseTraintest
+#from .pairtraintest import PairTraintest
+#from .neighborpair import NeighborPairTraintest
+from .neighbortriplet import OldTripletSampler, TripletIterator, BaseTripletSampler, AdriaTripletSampler
+#from .neighborerror import NeighborErrorTraintest
+#from .ae_siam_traintest import AE_SiameseTraintest
