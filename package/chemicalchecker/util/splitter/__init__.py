b56ef42566bcd0404fd516dadcf2d4117a60c102
Martino Bertoni <martino.bertoni@irbbarcelona.org>
2022-10-17 17:19:21 +0200

    fixed import for precumputed triplets in metric learn

--- package/chemicalchecker/util/splitter/__init__.py
+++ package/chemicalchecker/util/splitter/__init__.py
@@ -6,6 +6,8 @@ the generator functions which tensorflow likes.
 from .traintest import Traintest
 #from .pairtraintest import PairTraintest
 #from .neighborpair import NeighborPairTraintest
-from .neighbortriplet import OldTripletSampler, TripletIterator, BaseTripletSampler, AdriaTripletSampler
+from .neighbortriplet import OldTripletSampler, TripletIterator
+from .neighbortriplet import BaseTripletSampler, AdriaTripletSampler
+from .neighbortriplet import PrecomputedTripletSampler
 #from .neighborerror import NeighborErrorTraintest
 #from .ae_siam_traintest import AE_SiameseTraintest
