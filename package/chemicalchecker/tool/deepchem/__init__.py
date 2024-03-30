992134ccaf546a56bad9bcae0ea544551ab3f5ea
yasmmin <ycfrenchgirl2@gmail.com>
2024-03-31 00:17:58 +0100

    test pipe

--- package/chemicalchecker/tool/deepchem/__init__.py
+++ package/chemicalchecker/tool/deepchem/__init__.py
@@ -1,5 +1,16 @@
-"""Deepchem featurizer wrapper.
-
-From `DeepChem <https://deepchem.io/>`_
 """
-import feat
+Making it easy to import in classes.
+"""
+from __future__ import print_function
+from __future__ import division
+from __future__ import unicode_literals
+
+__author__ = "Bharath Ramsundar"
+__copyright__ = "Copyright 2016, Stanford University"
+__license__ = "MIT"
+
+from .base_classes import Featurizer
+from .graph_features import ConvMolFeaturizer
+from .graph_features import WeaveFeaturizer
+from .mol_graphs import ConvMol
+from .mol_graphs import WeaveMol
