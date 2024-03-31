de8a0005b4a3851077886337402f0c5086841276
yasmmin <ycfrenchgirl2@gmail.com>
2024-03-31 16:07:01 +0200

    snap so lib

--- package/chemicalchecker/util/network/snap/snapx/algorithms/__init__.py
+++
@@ -1,4 +0,0 @@
-from snapx.algorithms.centrality import *
-from snapx.algorithms.community import *
-from snapx.algorithms.components import *
-from snapx.algorithms.shortest_paths import *
--- package/chemicalchecker/util/network/snap/snapx/algorithms/shortest_paths/__init__.py
+++
@@ -1,2 +0,0 @@
-from snapx.algorithms.shortest_paths.weighted import *
-from snapx.algorithms.shortest_paths.unweighted import *
