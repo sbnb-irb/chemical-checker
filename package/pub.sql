6c3ba2ddb42eb73ee856697a40886b9efb4e2549
ymartins <ycfrenchgirl2@gmail.com>
2024-05-17 21:17:27 +0200

    cc pipeline web changes

--- package/pub.sql
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:a4471748187c17954b8070950507baf63bec1f7d7b748567023d6b84fe82ccf8
-size 488787416
--- package/pubchem.sql
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:37162afbc72b90bc7eb100394e0f3b8a4ae4cb05d9ed8001364609eeaabcf9fc
-size 488787911
