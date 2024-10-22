cd91a8b778fa2d40d0feff8f31ac8e5b97662bea
ymartins <ycfrenchgirl2@gmail.com>
2024-10-22 17:57:17 +0200

    gpu conifg

--- setup/run_chemicalchecker.sh
+++ setup/run_chemicalchecker.sh
@@ -3,8 +3,8 @@
 LOCAL_CCREPO="$HOME/chemical_checker"
 LOCAL_IMAGE="$LOCAL_CCREPO/cc.simg"
 
-LOCAL_CCREPO="/home/ymartins/Documents/cc_update/chemical_checker"
-LOCAL_IMAGE="/aloy/home/ymartins/Documents/cc_update/cc_image/cc.simg"
+#LOCAL_CCREPO="/home/ymartins/Documents/cc_update/chemical_checker"
+#LOCAL_IMAGE="/aloy/home/ymartins/Documents/cc_update/cc_image/cc.simg"
 
 JUPYTER_DIR=$LOCAL_CCREPO/run_user_sing
 
