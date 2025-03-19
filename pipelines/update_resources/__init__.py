3a98084408e1e828e80f74fe5e63056683de9d9f
victor.martinez@irbbarcelona.org <vmartinezs@irbccn08.hpc.irbbarcelona.pcb.ub.es>
2025-03-19 11:22:42 +0100

    docs static files

--- pipelines/update_resources/__init__.py
+++ pipelines/update_resources/__init__.py
@@ -1,1 +1,3 @@
-from .generate_chembl_files import generate_chembl_files
+version https://git-lfs.github.com/spec/v1
+oid sha256:06ab725859f3ceebc11042fb9f3a66f3922bed5e7fced09c9b0a0e0af905cea6
+size 56
