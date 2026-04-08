8f4ada8dabc9c8ef21fcba23800017a36caf8086
aperalta <aperalta@irblogin02.sc.irbbarcelona.org>
2026-04-08 16:25:28 +0200

    fix: remove overriding config from harcoded path

--- pipelines/cc_update.py
+++ pipelines/cc_update.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cb896d0c3bed583ada312959d409e5c3c9f12a37925597833632fb58a8fd71b0
-size 26326
+oid sha256:5c707aebfecd0dc215760e8583bf1125a9bff37d69d06351f6aa2a1e4e76e7ab
+size 26203
